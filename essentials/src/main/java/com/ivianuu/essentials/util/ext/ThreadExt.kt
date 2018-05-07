bd192be25e0424f6821f414d510e2a71077286d8
IVIanuu <IVIanuu@gmail.com>
2018-05-07 15:35:37 +0200

    Fix is main thread detection

--- essentials/src/main/java/com/ivianuu/essentials/util/ext/ThreadExt.kt
+++ essentials/src/main/java/com/ivianuu/essentials/util/ext/ThreadExt.kt
@@ -18,4 +18,4 @@ package com.ivianuu.essentials.util.ext
 
 import android.os.Looper
 
-val isMainThread = Looper.myLooper() == Looper.getMainLooper()
+val isMainThread get() = Looper.myLooper() == Looper.getMainLooper()
