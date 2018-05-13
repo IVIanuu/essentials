819791dd6a193c4e1860b7c3f01a3d5da237b6cc
IVIanuu <IVIanuu@gmail.com>
2018-05-13 11:35:20 +0200

    Added preference fragment delegate

--- essentials/src/main/java/com/ivianuu/essentials/ui/preference/BasePreferenceFragment.kt
+++ essentials/src/main/java/com/ivianuu/essentials/ui/preference/BasePreferenceFragment.kt
@@ -16,110 +16,30 @@
 
 package com.ivianuu.essentials.ui.preference
 
-import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.preference.PreferenceFragmentCompat
-import android.support.v7.preference.PreferenceScreen
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import com.ivianuu.essentials.ui.common.BackListener
-import com.ivianuu.essentials.ui.common.CORRESPONDING_FRAGMENT_EVENTS
-import com.ivianuu.essentials.ui.common.FragmentEvent
-import com.ivianuu.essentials.ui.common.FragmentEvent.*
-import com.ivianuu.traveler.Router
-import com.uber.autodispose.LifecycleScopeProvider
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.HasSupportFragmentInjector
-import io.reactivex.subjects.BehaviorSubject
-import javax.inject.Inject
+import com.ivianuu.essentials.ui.base.BaseFragment
+import com.ivianuu.essentials.util.ext.unsafeLazy
 
 /**
  * Base preference fragment
  */
-abstract class BasePreferenceFragment : PreferenceFragmentCompat(), BackListener, HasSupportFragmentInjector,
-    LifecycleScopeProvider<FragmentEvent> {
+abstract class BasePreferenceFragment : BaseFragment() {
 
-    @Inject lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
-
-    @Inject lateinit var router: Router
-
+    abstract val prefsContainerId: Int
+    open val preferenceTag = "prefs"
     open val prefsRes = -1
 
-    private val lifecycleSubject = BehaviorSubject.create<FragmentEvent>()
-
-    override fun onAttach(context: Context?) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-        lifecycleSubject.onNext(ATTACH)
+    protected val delegate by unsafeLazy {
+        PreferenceFragmentDelegate(childFragmentManager, prefsContainerId, preferenceTag)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleSubject.onNext(CREATE)
-    }
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        delegate.onCreate()
         if (prefsRes != -1) {
-            addPreferencesFromResource(prefsRes)
+            delegate.addPreferencesFromResource(prefsRes)
         }
     }
 
-    override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
-        return EnabledAwarePreferenceAdapter(preferenceScreen)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        lifecycleSubject.onNext(CREATE_VIEW)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        lifecycleSubject.onNext(START)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        lifecycleSubject.onNext(RESUME)
-    }
-
-    override fun onPause() {
-        lifecycleSubject.onNext(PAUSE)
-        super.onPause()
-    }
-
-    override fun onStop() {
-        lifecycleSubject.onNext(STOP)
-        super.onStop()
-    }
-
-    override fun onDestroyView() {
-        lifecycleSubject.onNext(DESTROY_VIEW)
-        super.onDestroyView()
-    }
-
-    override fun onDestroy() {
-        lifecycleSubject.onNext(DESTROY)
-        super.onDestroy()
-    }
-
-    override fun onDetach() {
-        lifecycleSubject.onNext(DETACH)
-        super.onDetach()
-    }
-
-    override fun handleBack(): Boolean {
-        return false
-    }
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = supportFragmentInjector
-
-    override fun lifecycle() = lifecycleSubject
-
-    override fun correspondingEvents() = CORRESPONDING_FRAGMENT_EVENTS
-
-    override fun peekLifecycle() = lifecycleSubject.value
 }
---
+++ essentials/src/main/java/com/ivianuu/essentials/ui/preference/PreferenceFragmentDelegate.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2018 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.ui.preference
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v7.preference.Preference
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
+import android.support.v7.preference.PreferenceScreen
+import android.support.v7.widget.RecyclerView
+
+/**
+ * Preference fragment delegate
+ */
+class PreferenceFragmentDelegate(
+    private val fragmentManager: FragmentManager,
+    private val containerId: Int,
+    private val tag: String = "prefs"
+) {
+
+    lateinit var fragment: PrefDelegateFragment
+
+    var adapterFactory: AdapterFactory?
+        get() = fragment.adapterFactory
+        set(value) {
+            fragment.adapterFactory = value
+        }
+
+    val preferenceManager: PreferenceManager
+        get() = fragment.preferenceManager
+
+    var preferenceScreen: PreferenceScreen?
+        get() = fragment.preferenceScreen
+        set(value) {
+            fragment.preferenceScreen = value
+        }
+
+    fun onCreate() {
+        var fragment = fragmentManager
+            .findFragmentByTag(tag) as PrefDelegateFragment?
+
+        if (fragment == null) {
+            fragment = PrefDelegateFragment()
+            fragmentManager.beginTransaction()
+                .replace(containerId, fragment, tag)
+                .commitNow()
+        }
+
+        this.fragment = fragment
+    }
+
+    fun addPreferencesFromResource(res: Int) {
+        fragment.addPreferencesFromResource(res)
+    }
+
+    fun setPreferencesFromResource(res: Int, key: CharSequence? = null) {
+        fragment.setPreferencesFromResource(res, key.toString())
+    }
+
+    fun findPreference(key: CharSequence): Preference = fragment.findPreference(key)
+
+    fun scrollToPreference(preference: Preference) {
+        fragment.scrollToPreference(preference)
+    }
+
+    fun scrollToPreference(key: CharSequence) {
+        fragment.scrollToPreference(key.toString())
+    }
+
+    fun setDivider(divider: Drawable?) {
+        fragment.setDivider(divider)
+    }
+
+    fun setDividerHeight(height: Int) {
+        fragment.setDividerHeight(height)
+    }
+
+    interface AdapterFactory {
+        fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*>
+    }
+}
+
+/**
+ * Fragment for the [PreferenceFragmentDelegate]
+ */
+class PrefDelegateFragment : PreferenceFragmentCompat() {
+
+    var adapterFactory: PreferenceFragmentDelegate.AdapterFactory? = DefaultAdapterFactory()
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+    }
+
+    override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
+        return adapterFactory?.onCreateAdapter(preferenceScreen) ?: super.onCreateAdapter(preferenceScreen)
+    }
+
+    private class DefaultAdapterFactory : PreferenceFragmentDelegate.AdapterFactory {
+        override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
+            return EnabledAwarePreferenceAdapter(preferenceScreen)
+        }
+    }
+}
