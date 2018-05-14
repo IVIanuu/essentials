633a5b3ff76de47b2b784521434a392bcd60e755
IVIanuu <IVIanuu@gmail.com>
2018-05-14 12:12:47 +0200

    Fix find preference

--- essentials/src/main/java/com/ivianuu/essentials/ui/preference/PreferenceFragmentDelegate.kt
+++ essentials/src/main/java/com/ivianuu/essentials/ui/preference/PreferenceFragmentDelegate.kt
@@ -75,7 +75,7 @@ class PreferenceFragmentDelegate(
 
     @Suppress("UNCHECKED_CAST")
     fun <T : Preference> findPreference(key: CharSequence): T? =
-        fragment.findPreference(key) as T
+        fragment.findPreference(key) as T?
 
     fun <T : Preference> requirePreference(key: CharSequence): T =
         findPreference(key) ?: throw IllegalStateException("no preference found for $key")
