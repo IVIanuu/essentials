daf033a407f1e3f6d9e044cdd863b08b3ff2479b
IVIanuu <IVIanuu@gmail.com>
2018-05-31 22:54:02 +0200

    Added simple lifecycle observer

--- essentials/src/main/java/com/ivianuu/essentials/util/screenlogger/ScreenLogger.kt
+++ essentials/src/main/java/com/ivianuu/essentials/util/screenlogger/ScreenLogger.kt
@@ -56,7 +56,8 @@ class ScreenLogger @Inject constructor(application: Application) : EssentialsSer
         }
 
         if (activity is FragmentActivity) {
-            activity.supportFragmentManager.doOnFragmentCreated(true) { _: FragmentManager, fragment: Fragment, bundle: Bundle? ->
+            activity.supportFragmentManager.doOnFragmentCreated(true) {
+                    _: FragmentManager, fragment: Fragment, bundle: Bundle? ->
                 if (fragment is NamedScreen
                     && fragment !is IgnoreNamedScreen
                     && bundle == null) {
