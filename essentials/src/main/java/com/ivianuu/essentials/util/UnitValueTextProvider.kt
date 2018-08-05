2bd6825817a1f8ae2dab575c09469282bd8b7ef9
IVIanuu <IVIanuu@gmail.com>
2018-08-05 16:01:05 +0200

    Added unit value text provider

---
+++ essentials/src/main/java/com/ivianuu/essentials/util/UnitValueTextProvider.kt
@@ -0,0 +1,27 @@
+package com.ivianuu.essentials.util
+
+import android.content.Context
+import com.ivianuu.epoxyprefs.SeekBarPreferenceModel
+import com.ivianuu.essentials.R
+import com.ivianuu.essentials.util.ext.string
+
+class UnitValueTextProvider(
+    private val context: Context,
+    private val unit: Unit
+) : SeekBarPreferenceModel.ValueTextProvider {
+
+    override fun getText(value: Int) = when (unit) {
+        Unit.DP -> context.string(R.string.seek_bar_pref_format_dp, value)
+        Unit.MILLIS -> context.string(R.string.seek_bar_pref_format_millis, value)
+        Unit.PERCENTAGE -> context.string(R.string.seek_bar_pref_format_percentage, value)
+        Unit.PX -> context.string(R.string.seek_bar_pref_format_px, value)
+        Unit.SECONDS -> context.string(R.string.seek_bar_pref_format_seconds, value)
+    }
+
+    enum class Unit {
+        DP, MILLIS, PERCENTAGE, PX, SECONDS
+    }
+}
+
+inline fun SeekBarPreferenceModel.Builder.unitValueTextProvider(unit: UnitValueTextProvider.Unit) =
+    valueTextProvider(UnitValueTextProvider(context, unit))
