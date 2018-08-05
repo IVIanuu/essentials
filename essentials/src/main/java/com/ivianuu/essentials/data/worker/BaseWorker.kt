b64688c0c646a6e3db236916da107ddd504ea6ec
IVIanuu <IVIanuu@gmail.com>
2018-08-05 23:36:22 +0200

    Added scope provider to base worker

--- essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
+++ essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
@@ -3,6 +3,7 @@ package com.ivianuu.essentials.data.worker
 import android.content.Context
 import androidx.work.Worker
 import com.ivianuu.essentials.util.ContextAware
+import com.ivianuu.essentials.util.rx.DisposableScopeProvider
 
 /**
  * Base worker
@@ -11,5 +12,12 @@ abstract class BaseWorker : Worker(), ContextAware {
 
     override val providedContext: Context
         get() = applicationContext
+
+    val scopeProvider = DisposableScopeProvider()
+
+    override fun onStopped() {
+        scopeProvider.dispose()
+        super.onStopped()
+    }
 
 }
