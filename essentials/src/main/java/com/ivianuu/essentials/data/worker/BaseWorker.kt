9a7388430f8dc9a9c896f92870ce4261080f6d14
IVIanuu <IVIanuu@gmail.com>
2018-08-07 16:35:20 +0200

    Auto inject workers

--- essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
+++ essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
@@ -2,22 +2,34 @@ package com.ivianuu.essentials.data.worker
 
 import android.content.Context
 import androidx.work.Worker
+import com.ivianuu.daggerextensions.work.WorkerInjection
+import com.ivianuu.essentials.injection.AutoInjector
+import com.ivianuu.essentials.injection.Injectable
 import com.ivianuu.essentials.util.ContextAware
 import com.ivianuu.essentials.util.rx.DisposableScopeProvider
 
 /**
  * Base worker
  */
-abstract class BaseWorker : Worker(), ContextAware {
+abstract class BaseWorker : Worker(), ContextAware, Injectable {
 
     override val providedContext: Context
         get() = applicationContext
 
     val scopeProvider = DisposableScopeProvider()
 
+    final override fun doWork(): WorkerResult {
+        if (this !is AutoInjector.Ignore) {
+            WorkerInjection.inject(this)
+        }
+
+        return doRealWork()
+    }
+
     override fun onStopped() {
         scopeProvider.dispose()
         super.onStopped()
     }
 
+    protected abstract fun doRealWork(): WorkerResult
 }
