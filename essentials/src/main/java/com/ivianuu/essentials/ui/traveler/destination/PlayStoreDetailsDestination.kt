7ee0e3108ee67d2b4766344546e6e07fc594ba23
IVIanuu <IVIanuu@gmail.com>
2018-08-13 12:25:21 +0200

    Do not serialize the play store details destination

--- essentials/src/main/java/com/ivianuu/essentials/ui/traveler/destination/PlayStoreDetailsDestination.kt
+++ essentials/src/main/java/com/ivianuu/essentials/ui/traveler/destination/PlayStoreDetailsDestination.kt
@@ -5,16 +5,18 @@ import android.content.Intent
 import androidx.core.net.toUri
 import com.ivianuu.compass.ActivityRouteFactory
 import com.ivianuu.compass.Destination
+import com.ivianuu.compass.DoNotSerialize
 import com.ivianuu.compass.RouteFactory
 
+@DoNotSerialize
 @RouteFactory(PlayStoreDetailsDestination.RouteFactory::class)
 @Destination
 data class PlayStoreDetailsDestination(val packageName: String) {
 
     object RouteFactory : ActivityRouteFactory<PlayStoreDetailsDestination> {
         override fun createActivityIntent(
-                context: Context,
-                destination: PlayStoreDetailsDestination
+            context: Context,
+            destination: PlayStoreDetailsDestination
         ) = Intent(Intent.ACTION_VIEW,
             "https://play.google.com/store/apps/details?id=${destination.packageName}".toUri()
         )
