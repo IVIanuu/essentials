45ff5874af0de084ab6aacd42560ccf9e64fd24b
IVIanuu <IVIanuu@gmail.com>
2018-08-22 16:43:11 +0200

    Improved exit on navigation

--- essentials/src/main/java/com/ivianuu/essentials/ui/traveler/RouterHolder.kt
+++ essentials/src/main/java/com/ivianuu/essentials/ui/traveler/RouterHolder.kt
@@ -16,6 +16,8 @@
 
 package com.ivianuu.essentials.ui.traveler
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.isInBackstack
 import android.support.v7.widget.Toolbar
 import android.view.View
 import com.ivianuu.epoxyprefs.PreferenceModel
@@ -29,8 +31,10 @@ import com.ivianuu.traveler.Router
 interface RouterHolder {
     val router: Router
 
-    fun Toolbar.exitOnNavigationClick() {
-        exitOnNavigationClick(router)
+    fun Toolbar.exitOnNavigationClick(force: Boolean = false) {
+        if (force || (this is Fragment && isInBackstack)) {
+            exitOnNavigationClick(router)
+        }
     }
 
     fun View.navigateOnClick(key: () -> Any) {
