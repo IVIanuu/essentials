7667c4f3a00682ba9fdb8a153637f7da5fedc323
IVIanuu <IVIanuu@gmail.com>
2018-09-01 01:46:56 +0200

    Return the disposable in disposed with

--- essentials/src/main/java/com/ivianuu/essentials/util/ext/DisposableExt.kt
+++ essentials/src/main/java/com/ivianuu/essentials/util/ext/DisposableExt.kt
@@ -8,7 +8,7 @@ import io.reactivex.disposables.Disposable
 fun Disposable.disposedWith(
     owner: LifecycleOwner,
     event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY
-) {
+) = apply {
     owner.lifecycle.addObserver(object : SimpleLifecycleObserver() {
         override fun onAny(owner: LifecycleOwner, e: Lifecycle.Event) {
             if (event == e) {
