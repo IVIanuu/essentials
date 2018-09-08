61c0e49e9e38b667deb4e63ccfabe77603e43121
IVIanuu <IVIanuu@gmail.com>
2018-09-08 04:14:02 +0200

    Auto inject workers

--- essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
+++ essentials/src/main/java/com/ivianuu/essentials/data/worker/BaseWorker.kt
@@ -1,7 +1,11 @@
 package com.ivianuu.essentials.data.worker
 
 import android.content.Context
+import androidx.work.WorkFinishedCallback
 import androidx.work.Worker
+import com.ivianuu.essentials.injection.AutoInjector
+import com.ivianuu.essentials.injection.Injectable
+import com.ivianuu.essentials.injection.worker.WorkerInjection
 import com.ivianuu.essentials.util.ContextAware
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.experimental.Job
@@ -9,13 +13,20 @@ import kotlinx.coroutines.experimental.Job
 /**
  * Base worker
  */
-abstract class BaseWorker : Worker(), ContextAware {
+abstract class BaseWorker : Worker(), ContextAware, Injectable {
 
     override val providedContext: Context
         get() = applicationContext
 
     protected val disposables = CompositeDisposable()
     protected val job = Job()
+
+    override fun onStartWork(callback: WorkFinishedCallback) {
+        if (this !is AutoInjector.Ignore) {
+            WorkerInjection.inject(this)
+        }
+        super.onStartWork(callback)
+    }
 
     override fun onStopped(cancelled: Boolean) {
         disposables.clear()
