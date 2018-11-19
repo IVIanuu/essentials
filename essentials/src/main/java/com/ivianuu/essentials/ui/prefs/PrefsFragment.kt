9d418f55ea5e2f6b733683ce50943ecb37836552
Manu <IVIanuu@gmail.com>
2018-11-19 17:21:00 +0100

    Use the default prefs in prefs controller/fragment

--- essentials/src/main/java/com/ivianuu/essentials/ui/prefs/PrefsFragment.kt
+++ essentials/src/main/java/com/ivianuu/essentials/ui/prefs/PrefsFragment.kt
@@ -1,45 +1,19 @@
 package com.ivianuu.essentials.ui.prefs
 
-import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import com.airbnb.epoxy.EpoxyController
-import com.ivianuu.epoxyprefs.CategoryPreferenceModel
-import com.ivianuu.epoxyprefs.CheckboxPreferenceModel
-import com.ivianuu.epoxyprefs.EditTextPreferenceModel
-import com.ivianuu.epoxyprefs.EpoxyPrefsPlugins
-import com.ivianuu.epoxyprefs.MultiSelectListPreferenceModel
-import com.ivianuu.epoxyprefs.PreferenceDividerDecoration
-import com.ivianuu.epoxyprefs.PreferenceModel
-import com.ivianuu.epoxyprefs.RadioButtonPreferenceModel
-import com.ivianuu.epoxyprefs.SeekBarPreferenceModel
-import com.ivianuu.epoxyprefs.SingleItemListPreferenceModel
-import com.ivianuu.epoxyprefs.SwitchPreferenceModel
-import com.ivianuu.epoxyprefs.categoryPreference
-import com.ivianuu.epoxyprefs.checkboxPreference
-import com.ivianuu.epoxyprefs.editTextPreference
-import com.ivianuu.epoxyprefs.multiSelectListPreference
-import com.ivianuu.epoxyprefs.preference
-import com.ivianuu.epoxyprefs.radioButtonPreference
-import com.ivianuu.epoxyprefs.seekBarPreference
-import com.ivianuu.epoxyprefs.singleItemListPreference
-import com.ivianuu.epoxyprefs.switchPreference
+import com.ivianuu.epoxyprefs.*
 import com.ivianuu.essentials.ui.simple.SimpleFragment
-import com.ivianuu.essentials.util.ext.unsafeLazy
+import javax.inject.Inject
 
 /**
  * Prefs fragment
  */
 abstract class PrefsFragment : SimpleFragment() {
 
-    protected open val sharedPreferencesName
-        get() =
-            EpoxyPrefsPlugins.getDefaultSharedPreferencesName(requireContext())
-
-    protected val sharedPreferences: SharedPreferences by unsafeLazy {
-        requireContext().getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
-    }
+    @Inject lateinit var sharedPreferences: SharedPreferences
 
     protected open val usePreferenceDividerDecoration = true
 
