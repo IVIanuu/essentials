ee7a507cd3aa94b35529850498350b2240a8b45a
IVIanuu <IVIanuu@gmail.com>
2019-03-19 21:56:55 +0100

    Changed bg color

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev-39"
+    const val version = "${Build.versionName}-dev-40"
 }
 
 object Versions {
