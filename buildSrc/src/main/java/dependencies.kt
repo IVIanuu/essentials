c73578c9644ceb20c9a6b033f9e07711a7987a49
IVIanuu <IVIanuu@gmail.com>
2019-04-28 11:12:10 +0200

    Version bump

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev-79"
+    const val version = "${Build.versionName}-dev-80"
 }
 
 object Versions {
