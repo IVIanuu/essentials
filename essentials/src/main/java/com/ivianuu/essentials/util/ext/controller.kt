b3ce3729ab2199349f16723e9e3cf3ae3440c48f
IVIanuu <IVIanuu@gmail.com>
2019-05-25 11:46:24 +0200

    Added back controller extensions

--- essentials/src/main/java/com/ivianuu/essentials/util/ext/controller.kt
+++ essentials/src/main/java/com/ivianuu/essentials/util/ext/controller.kt
@@ -16,8 +16,22 @@
 
 package com.ivianuu.essentials.util.ext
 
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import com.ivianuu.director.Controller
+import com.ivianuu.kommon.core.app.hideInputMethod
+import com.ivianuu.kommon.core.app.showInputMethod
 
 val Controller.viewLifecycleScope
     get() = viewLifecycleOwner.lifecycleScope
+
+fun Controller.hideInputMethod() {
+    activity.hideInputMethod()
+}
+
+fun Controller.showInputMethod(view: View, flags: Int = 0) {
+    activity.showInputMethod(view, flags)
+}
+
+fun Controller.requireParentController(): Controller =
+    parentController ?: error("parent Controller is null")
