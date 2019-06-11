96ce28e88f85e77b5c63efccad02f23dc6700b9f
IVIanuu <IVIanuu@gmail.com>
2019-06-11 10:06:52 +0200

    Updated injekt

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev-112"
+    const val version = "${Build.versionName}-dev-113"
 }
 
 object Versions {
