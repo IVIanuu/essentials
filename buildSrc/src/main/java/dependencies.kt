fd6ffd014a8dca7e014df9da481b2b1a94d9fee1
IVIanuu <IVIanuu@gmail.com>
2019-06-11 15:53:42 +0200

    Updated injekt

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev-113"
+    const val version = "${Build.versionName}-dev-116"
 }
 
 object Versions {
@@ -57,7 +57,7 @@ object Versions {
     const val epoxy = "3.5.1"
     const val epoxyPrefs = "0.0.1-dev-13"
     const val glide = "4.9.0"
-    const val injekt = "0.0.1-dev-56"
+    const val injekt = "0.0.1-dev-59"
     const val junit = "4.12"
     const val kommon = "0.0.1-dev-12"
     const val kotlin = "1.3.40-eap-67"
