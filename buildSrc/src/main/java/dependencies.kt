18e517e567b93f95041ad1165aba9c46e36ff74e
IVIanuu <IVIanuu@gmail.com>
2019-06-25 15:18:02 +0200

    Updated injekt

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -58,7 +58,7 @@ object Versions {
     const val epoxy = "3.5.1"
     const val epoxyPrefs = "0.0.1-dev-20"
     const val glide = "4.9.0"
-    const val injekt = "0.0.1-dev-64"
+    const val injekt = "0.0.1-dev-66"
     const val junit = "4.12"
     const val kommon = "0.0.1-dev-12"
     const val kotlin = "1.3.40"
