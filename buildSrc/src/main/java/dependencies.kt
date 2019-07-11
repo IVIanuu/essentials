e0da997bdb1702283f7464b3f240d37b079be4b9
IVIanuu <IVIanuu@gmail.com>
2019-07-11 14:35:02 +0200

    Updated mdc

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -31,7 +31,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev-151"
+    const val version = "${Build.versionName}-dev-154"
 }
 
 object Versions {
@@ -57,16 +57,16 @@ object Versions {
     const val coroutines = "1.3.0-M2"
     const val director = "0.0.1-dev-35"
     const val epoxy = "3.7.0"
-    const val epoxyPrefs = "0.0.1-dev-21"
+    const val epoxyPrefs = "0.0.1-dev-22"
     const val glide = "4.9.0"
     const val injekt = "0.0.1-dev-66"
     const val junit = "4.12"
     const val kommon = "0.0.1-dev-12"
-    const val kotlin = "1.3.40"
+    const val kotlin = "1.3.41"
     const val kPrefs = "0.0.1-dev-9"
     const val kSettings = "0.0.1-dev-7"
     const val mavenGradle = "2.1"
-    const val materialComponents = "1.1.0-alpha07"
+    const val materialComponents = "1.1.0-alpha08"
     const val materialDialogs = "3.1.0"
     const val mockitoKotlin = "2.0.0"
     const val roboelectric = "4.0.2"
