1b2462a048c5d5f01a5fd3752954a5c37b20315c
IVIanuu <IVIanuu@gmail.com>
2019-07-23 23:11:52 +0200

    card

---
+++ sample/src/main/java/com/ivianuu/essentials/sample/ui/widget/layout/Card.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.sample.ui.widget.layout
+
+import com.google.android.material.card.MaterialCardView
+import com.ivianuu.essentials.sample.ui.widget.lib.BuildContext
+import com.ivianuu.essentials.sample.ui.widget.lib.ViewGroupWidget
+
+fun CardView(children: BuildContext.() -> Unit) = ViewGroupWidget<MaterialCardView>(
+    children = children,
+    updateView = null // todo
+)
