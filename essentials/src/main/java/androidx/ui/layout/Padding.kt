b7072132b4c4f5e87e4e3b1937de958ffb978268
IVIanuu <ivianuu@gmail.com>
2019-07-28 04:22:47 +0200

    Added androidx ui

---
+++ essentials/src/main/java/androidx/ui/layout/Padding.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.layout
+
+import androidx.ui.core.Dp
+import androidx.ui.core.dp
+
+/**
+ * Describes a set of offsets from each of the four sides of a box. For example,
+ * it is used to describe padding for the [Padding] widget.
+ */
+data class EdgeInsets(
+    val left: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val right: Dp = 0.dp,
+    val bottom: Dp = 0.dp
+) {
+    constructor(all: Dp) : this(all, all, all, all)
+}
