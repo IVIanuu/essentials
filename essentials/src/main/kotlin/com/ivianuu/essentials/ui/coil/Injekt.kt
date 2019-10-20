3360b40aa26b716215613b633f9df2cd9b6d1c4c
IVIanuu <ivianuu@gmail.com>
2019-10-20 21:55:06 +0200

    More coil injekt support

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/coil/Injekt.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/coil/Injekt.kt
@@ -4,20 +4,20 @@ import coil.CoilAccessor
 import coil.ImageLoader
 import coil.decode.Decoder
 import coil.fetch.Fetcher
-import com.github.ajalt.timberkt.d
+import coil.map.Mapper
+import coil.map.MeasuredMapper
 import com.ivianuu.injekt.*
 import kotlin.reflect.KClass
 
 val esCoilModule = module {
     set<Decoder>(Decoders)
     set<FetcherBinding<*>>(Fetchers)
-    /*set<Pair<KClass<*>, Mapper<*, *>>>(Mappers)
-    set<Pair<KClass<*>, MeasuredMapper<*, *>>>(MeasuredMappers)*/ // todo
+    set<MapperBinding<*>>(Mappers)
+    set<MeasuredMapperBinding<*>>(MeasuredMappers)
 
     single {
         ImageLoader(get()) {
             componentRegistry {
-                d { "init image loader" }
                 get<Set<Decoder>>(Decoders)
                     .forEach {
                         add(it)
@@ -25,19 +25,18 @@ val esCoilModule = module {
 
                 get<Set<FetcherBinding<*>>>(Fetchers)
                     .forEach { binding ->
-                        d { "add fetcher $binding" }
                         CoilAccessor.add(this, binding.type.java, binding.fetcher)
                     }
 
-                /*get<Set<Pair<*, Mapper<*, *>>>>(Mappers)
-                    .forEach { (type, mapper) ->
-                        CoilAccessor.add(this, (type as KClass<Any>).java, mapper)
+                get<Set<MapperBinding<*>>>(Mappers)
+                    .forEach { binding ->
+                        CoilAccessor.add(this, binding.type.java, binding.mapper)
                     }
 
-                get<Set<Pair<*, MeasuredMapper<*, *>>>>(MeasuredMappers)
-                    .forEach { (type, mapper) ->
-                        CoilAccessor.add(this, (type as KClass<Any>).java, mapper)
-                    }*/ // todo
+                get<Set<MeasuredMapperBinding<*>>>(MeasuredMappers)
+                    .forEach { binding ->
+                        CoilAccessor.add(this, binding.type.java, binding.mapper)
+                    }
             }
         }
     }
@@ -90,15 +89,88 @@ inline fun <reified F : Fetcher<T>, reified T : Any> Module.bindFetcher(
 }
 
 inline fun <reified F : Fetcher<T>, reified T : Any> BindingContext<F>.bindFetcher(type: KClass<T>): BindingContext<F> {
-    println("bind fetcher f ${F::class}, t ${T::class}")
     module.factory<FetcherBinding<T>> {
-        println("const f ${F::class}, t ${T::class}")
         FetcherBinding<T>(fetcher = get<F>(), type = type).also {
-            println("const done $it")
         }
     }.also { bbc ->
-        println("bbc key ${bbc.key}")
         module.set<FetcherBinding<*>>(Fetchers) {
+            add(bbc.key, false)
+        }
+    }
+
+    return this
+}
+
+@Name(Mappers.Companion::class)
+annotation class Mappers {
+    companion object
+}
+
+data class MapperBinding<T : Any>(
+    val mapper: Mapper<T, *>,
+    val type: KClass<T>
+)
+
+// todo remove type arg once fixed
+
+inline fun <reified M : Mapper<T, *>, reified T : Any> Module.mapper(
+    type: KClass<T>,
+    name: Any? = null,
+    noinline definition: Definition<M>
+): BindingContext<M> = factory(name = name, definition = definition).bindMapper<M, T>(type)
+
+inline fun <reified M : Mapper<T, *>, reified T : Any> Module.bindMapper(
+    type: KClass<T>,
+    name: Any? = null
+) {
+    withBinding<M>(name) { bindMapper<M, T>(type) }
+}
+
+inline fun <reified M : Mapper<T, *>, reified T : Any> BindingContext<M>.bindMapper(type: KClass<T>): BindingContext<M> {
+    module.factory<MapperBinding<T>> {
+        MapperBinding<T>(mapper = get<M>(), type = type)
+    }.also { bbc ->
+        module.set<MapperBinding<*>>(Mappers) {
+            add(bbc.key, false)
+        }
+
+        return this
+    }
+}
+
+
+@Name(MeasuredMappers.Companion::class)
+annotation class MeasuredMappers {
+    companion object
+}
+
+data class MeasuredMapperBinding<T : Any>(
+    val mapper: MeasuredMapper<T, *>,
+    val type: KClass<T>
+)
+
+// todo remove type arg once fixed
+
+inline fun <reified M : MeasuredMapper<T, *>, reified T : Any> Module.measuredMapper(
+    type: KClass<T>,
+    name: Any? = null,
+    noinline definition: Definition<M>
+): BindingContext<M> = factory(name = name, definition = definition).bindMeasuredMapper<M, T>(type)
+
+inline fun <reified M : MeasuredMapper<T, *>, reified T : Any> Module.bindMeasuredMapper(
+    type: KClass<T>,
+    name: Any? = null
+) {
+    withBinding<M>(name) { bindMeasuredMapper<M, T>(type) }
+}
+
+inline fun <reified M : MeasuredMapper<T, *>, reified T : Any> BindingContext<M>.bindMeasuredMapper(
+    type: KClass<T>
+): BindingContext<M> {
+    module.factory<MeasuredMapperBinding<T>> {
+        MeasuredMapperBinding<T>(mapper = get<M>(), type = type)
+    }.also { bbc ->
+        module.set<MeasuredMapperBinding<*>>(MeasuredMappers) {
             add(bbc.key, false)
         }
     }
