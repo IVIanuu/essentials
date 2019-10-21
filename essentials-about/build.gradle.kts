0a369ed055e99efc4d20bb3dae46c64cb590b3c1
IVIanuu <ivianuu@gmail.com>
2019-10-21 18:17:02 +0200

    192

---
+++ essentials-about/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/android-build-lib.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/java-8-android.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/kt-android-ext.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/kt-compiler-args.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/kt-kapt.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/kt-source-sets-android.gradle")
+apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/mvn-publish.gradle")
+
+dependencies {
+    api(project(":essentials"))
+}
