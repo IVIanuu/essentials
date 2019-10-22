fb56f540446c5209d01555c7a9c8c02e1355b87b
IVIanuu <ivianuu@gmail.com>
2019-10-22 22:07:43 +0200

    Added fab support in scaffold

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Scaffold.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Scaffold.kt
@@ -1,26 +1,57 @@
 package com.ivianuu.essentials.ui.compose.material
 
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
 import androidx.ui.core.dp
 import androidx.ui.layout.FlexColumn
+import androidx.ui.layout.Padding
+import androidx.ui.layout.Stack
 import androidx.ui.material.surface.Surface
 import com.ivianuu.essentials.ui.compose.core.composable
 
 fun Scaffold(
     appBar: @Composable() () -> Unit,
-    content: @Composable() () -> Unit
+    content: @Composable() () -> Unit,
+    fabConfiguration: Scaffold.FabConfiguration? = null
 ) = composable("Scaffold") {
-    FlexColumn {
-        inflexible {
-            Surface(elevation = 4.dp) {
-                appBar()
+    Stack {
+        FlexColumn {
+            inflexible {
+                Surface(elevation = 4.dp) {
+                    appBar()
+                }
+            }
+
+            flexible(1f) {
+                Surface {
+                    content()
+                }
             }
         }
 
-        flexible(1f) {
-            Surface {
-                content()
+        if (fabConfiguration != null) {
+            aligned(
+                when (fabConfiguration.position) {
+                    Scaffold.FabPosition.Center -> Alignment.BottomCenter
+                    Scaffold.FabPosition.End -> Alignment.BottomRight
+                }
+            ) {
+                Padding(padding = 16.dp) {
+                    fabConfiguration.fab()
+                }
             }
         }
+    }
+}
+
+object Scaffold {
+
+    data class FabConfiguration(
+        val position: FabPosition,
+        val fab: @Composable() () -> Unit
+    )
+
+    enum class FabPosition {
+        Center, End
     }
 }
