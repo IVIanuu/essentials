28117ff44baac7e5b09a08c041630362d3b9b066
IVIanuu <ivianuu@gmail.com>
2019-10-24 16:31:30 +0200

    Clean

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/HandleBack.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/HandleBack.kt
@@ -3,10 +3,16 @@ package com.ivianuu.essentials.ui.compose.common
 import android.app.Activity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcherOwner
+import androidx.compose.ambient
 import androidx.compose.effectOf
 import androidx.compose.onActive
+import androidx.compose.unaryPlus
+import com.ivianuu.essentials.ui.compose.core.ActivityAmbient
 
-fun handleBack(activity: Activity, callback: () -> Unit) = effectOf<Unit> {
+fun handleBack(
+    activity: Activity = +ambient(ActivityAmbient),
+    callback: () -> Unit
+) = effectOf<Unit> {
     +onActive {
         val backPressedDispatcher =
             (activity as OnBackPressedDispatcherOwner).onBackPressedDispatcher
