969bb8ad7121d05f124116234784d9fd92e9ec05
IVIanuu <ivianuu@gmail.com>
2019-10-24 12:40:01 +0200

    Popup improvements

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Popup.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Popup.kt
@@ -2,17 +2,23 @@ package com.ivianuu.essentials.ui.compose.material
 
 import androidx.animation.TweenBuilder
 import androidx.compose.Composable
+import androidx.compose.ambient
+import androidx.compose.memo
 import androidx.compose.onActive
 import androidx.compose.unaryPlus
 import androidx.ui.animation.animatedFloat
 import androidx.ui.core.Alignment
 import androidx.ui.core.Dp
+import androidx.ui.core.LayoutCoordinates
+import androidx.ui.core.OnPositioned
 import androidx.ui.core.Opacity
 import androidx.ui.core.WithDensity
 import androidx.ui.core.dp
 import androidx.ui.core.gesture.PressGestureDetector
+import androidx.ui.core.positionInRoot
 import androidx.ui.foundation.Clickable
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Image
 import androidx.ui.layout.Column
 import androidx.ui.layout.ConstrainedBox
 import androidx.ui.layout.DpConstraints
@@ -21,16 +27,21 @@ import androidx.ui.layout.Padding
 import androidx.ui.layout.Wrap
 import androidx.ui.material.ripple.Ripple
 import androidx.ui.material.surface.Card
+import com.github.ajalt.timberkt.d
+import com.ivianuu.essentials.R
 import com.ivianuu.essentials.ui.compose.core.composable
+import com.ivianuu.essentials.ui.compose.resources.drawableResource
 
 @Composable
-fun Popup(
+fun <T> PopupMenu(
     onCancel: () -> Unit,
     alignment: Alignment = Alignment.TopLeft,
     offsetX: Dp = 0.dp,
     offsetY: Dp = 0.dp,
-    content: @Composable() () -> Unit
-) = composable("Popup") {
+    items: List<T>,
+    onSelected: (T) -> Unit,
+    item: @Composable() (T) -> Unit
+) = composable("PopupMenu") {
     val alphaAnim = +animatedFloat(0f)
 
     +onActive {
@@ -75,42 +86,23 @@ fun Popup(
                         PressGestureDetector {
                             Card(
                                 elevation = 8.dp,
-                                shape = RoundedCornerShape(4.dp),
-                                children = content
-                            )
+                                shape = RoundedCornerShape(4.dp)
+                            ) {
+                                Padding(top = 8.dp, bottom = 8.dp) {
+                                    Column {
+                                        items.forEach { value ->
+                                            PopupMenuItem(
+                                                content = { item(value) },
+                                                onClick = {
+                                                    onSelected(value)
+                                                }
+                                            )
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun <T> PopupMenu(
-    onCancel: () -> Unit,
-    alignment: Alignment = Alignment.TopLeft,
-    offsetX: Dp = 0.dp,
-    offsetY: Dp = 0.dp,
-    items: List<T>,
-    onSelected: (T) -> Unit,
-    item: @Composable() (T) -> Unit
-) = composable("PopupMenu") {
-    Popup(
-        onCancel = onCancel,
-        alignment = alignment,
-        offsetX = offsetX,
-        offsetY = offsetY
-    ) {
-        Padding(top = 8.dp, bottom = 8.dp) {
-            Column {
-                items.forEach { value ->
-                    PopupMenuItem(
-                        content = { item(value) },
-                        onClick = {
-                            onSelected(value)
-                        }
-                    )
                 }
             }
         }
@@ -146,7 +138,7 @@ private fun PopupMenuItem(
     }
 }
 
-fun <T> Scaffold.showPopup(
+fun <T> Scaffold.showPopupMenu(
     alignment: Alignment = Alignment.TopLeft,
     offsetX: Dp = 0.dp,
     offsetY: Dp = 0.dp,
@@ -155,7 +147,7 @@ fun <T> Scaffold.showPopup(
     onSelected: (T) -> Unit,
     item: @Composable() (T) -> Unit
 ) {
-    showOverlay { dismiss ->
+    addOverlay { dismiss ->
         PopupMenu(
             onCancel = {
                 dismiss()
@@ -174,28 +166,129 @@ fun <T> Scaffold.showPopup(
     }
 }
 
-fun Scaffold.showPopup(
-    alignment: Alignment = Alignment.TopLeft,
-    offsetX: Dp = 0.dp,
-    offsetY: Dp = 0.dp,
+private class CoordinatesHolder(var coordinates: LayoutCoordinates?)
+
+// todo better name?
+@Composable
+fun <T> PopupMenuTrigger(
+    alignment: Alignment = Alignment.Center,
     onCancel: (() -> Unit)? = null,
-    content: @Composable() (dismiss: () -> Unit) -> Unit
-) {
-    showOverlay { dismiss ->
-        val internalOnCancel = {
-            dismiss()
-            onCancel?.invoke()
-        }
-        Popup(
-            onCancel = { internalOnCancel() },
-            alignment = alignment,
-            offsetX = offsetX,
-            offsetY = offsetY,
-            content = {
-                content {
-                    internalOnCancel()
+    items: List<T>,
+    onSelected: (T) -> Unit,
+    item: @Composable() (T) -> Unit,
+    child: @Composable() (showPopup: () -> Unit) -> Unit
+) = composable("PopupMenuTrigger") {
+    Wrap {
+        WithDensity {
+            // cache coordinates
+            val coordinatesHolder = +memo { CoordinatesHolder(null) }
+            OnPositioned { coordinatesHolder.coordinates = it }
+
+            val scaffold = +ambient(ScaffoldAmbient)
+
+            child {
+                val coordinates = coordinatesHolder.coordinates!!
+                val scaffoldCoordinates = scaffold.coordinates!!
+
+                val globalPosition = coordinates.positionInRoot
+                val scaffoldGlobalPosition = scaffoldCoordinates.positionInRoot
+
+                val width = coordinates.size.width.toDp()
+                val height = coordinates.size.height.toDp()
+                val halfWidth = width.div(2)
+                val halfHeight = height.div(2)
+                val left = globalPosition.x.minus(scaffoldGlobalPosition.x).toDp()
+                val top = globalPosition.y.minus(scaffoldGlobalPosition.y).toDp()
+                val right = left.plus(width)
+                val bottom = top.plus(height)
+                val centerX = left.plus(halfWidth)
+                val centerY = top.plus(halfHeight)
+
+                val isLeft = left.value < scaffoldCoordinates.position.x.plus(
+                    scaffoldCoordinates.size.width.div(2)
+                ).toDp().value
+                val isTop = top.value < scaffoldCoordinates.position.y.plus(
+                    scaffoldCoordinates.size.height.div(2)
+                ).toDp().value
+
+                val popupAlignment = if (isLeft) {
+                    if (isTop) {
+                        Alignment.TopLeft
+                    } else {
+                        Alignment.BottomLeft
+                    }
+                } else {
+                    if (isTop) {
+                        Alignment.TopRight
+                    } else {
+                        Alignment.BottomRight
+                    }
                 }
+
+                var (offsetX, offsetY) = when (alignment) {
+                    Alignment.TopLeft -> left to top
+                    Alignment.TopCenter -> centerX to top
+                    Alignment.TopRight -> right to top
+                    Alignment.CenterLeft -> left to centerY
+                    Alignment.Center -> centerX to centerY
+                    Alignment.CenterRight -> right to centerY
+                    Alignment.BottomLeft -> left to bottom
+                    Alignment.BottomCenter -> centerX to bottom
+                    Alignment.BottomRight -> right to bottom
+                }
+
+                if (!isLeft) {
+                    offsetX = scaffoldCoordinates.size.width.toDp().minus(offsetX)
+                }
+                if (!isTop) {
+                    offsetY = scaffoldCoordinates.size.height.toDp().minus(offsetY)
+                }
+
+                d {
+                    "show popup\n" +
+                            "is left $isLeft it top $isTop\n" +
+                            "popup alignment $popupAlignment\n " +
+                            "user alignment $alignment\n" +
+                            "width $width, height $height\n" +
+                            "left $left, top $top, right $right, bottom $bottom\n" +
+                            "offset x $offsetX, y $offsetY"
+                }
+
+                scaffold.showPopupMenu(
+                    alignment = popupAlignment,
+                    offsetX = offsetX,
+                    offsetY = offsetY,
+                    items = items,
+                    item = item,
+                    onCancel = onCancel,
+                    onSelected = onSelected
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun <T> PopupMenuButton(
+    //offsetX: Dp = 0.dp,
+    //offsetY: Dp = 0.dp,
+    alignment: Alignment = Alignment.Center,
+    onCancel: (() -> Unit)? = null,
+    items: List<T>,
+    onSelected: (T) -> Unit,
+    item: @Composable() (T) -> Unit,
+    icon: Image = +drawableResource(R.drawable.abc_ic_menu_overflow_material)
+) = composable("PopupMenuButton") {
+    PopupMenuTrigger(
+        alignment = alignment,
+        onCancel = onCancel,
+        items = items,
+        onSelected = onSelected,
+        item = item
+    ) { showPopup ->
+        ImageButton(
+            image = icon,
+            onClick = showPopup
         )
     }
 }
