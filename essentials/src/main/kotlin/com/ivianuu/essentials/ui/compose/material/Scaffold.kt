1c32b276ab41ebb02709c9df84c55c083da85f37
IVIanuu <ivianuu@gmail.com>
2019-10-24 11:38:46 +0200

    Fix scaffold coords

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Scaffold.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Scaffold.kt
@@ -10,6 +10,7 @@ import androidx.ui.core.Alignment
 import androidx.ui.core.LayoutCoordinates
 import androidx.ui.core.OnPositioned
 import androidx.ui.core.dp
+import androidx.ui.layout.Container
 import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.Padding
 import androidx.ui.layout.Stack
@@ -25,40 +26,42 @@ fun Scaffold(
     val overlays = +state { emptyList<Overlay>() }
     val scaffold = +memo { Scaffold(overlays) }
 
-    OnPositioned { scaffold.coordinates = it }
-
     ScaffoldAmbient.Provider(value = scaffold) {
-        Stack {
-            FlexColumn {
-                inflexible {
-                    Surface(elevation = 4.dp) {
-                        appBar()
+        Container {
+            OnPositioned { scaffold.coordinates = it }
+
+            Stack {
+                FlexColumn {
+                    inflexible {
+                        Surface(elevation = 4.dp) {
+                            appBar()
+                        }
+                    }
+
+                    flexible(1f) {
+                        Surface {
+                            content()
+                        }
                     }
                 }
 
-                flexible(1f) {
-                    Surface {
-                        content()
+                if (fabConfiguration != null) {
+                    aligned(
+                        when (fabConfiguration.position) {
+                            Scaffold.FabPosition.Center -> Alignment.BottomCenter
+                            Scaffold.FabPosition.End -> Alignment.BottomRight
+                        }
+                    ) {
+                        Padding(padding = 16.dp) {
+                            fabConfiguration.fab()
+                        }
                     }
                 }
-            }
 
-            if (fabConfiguration != null) {
-                aligned(
-                    when (fabConfiguration.position) {
-                        Scaffold.FabPosition.Center -> Alignment.BottomCenter
-                        Scaffold.FabPosition.End -> Alignment.BottomRight
+                overlays.value.forEach { overlay ->
+                    overlay.composable {
+                        scaffold.hideOverlay(overlay)
                     }
-                ) {
-                    Padding(padding = 16.dp) {
-                        fabConfiguration.fab()
-                    }
-                }
-            }
-
-            overlays.value.forEach { overlay ->
-                overlay.composable {
-                    scaffold.hideOverlay(overlay)
                 }
             }
         }
