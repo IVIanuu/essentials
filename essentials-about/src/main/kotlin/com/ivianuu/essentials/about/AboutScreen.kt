b88f864c46b3727ec7c1f07af78ae89919545241
IVIanuu <ivianuu@gmail.com>
2019-10-24 16:50:27 +0200

    Use compose for about screen

--- essentials-about/src/main/kotlin/com/ivianuu/essentials/about/AboutScreen.kt
+++ essentials-about/src/main/kotlin/com/ivianuu/essentials/about/AboutScreen.kt
@@ -1,138 +1,106 @@
 package com.ivianuu.essentials.about
 
-import com.airbnb.epoxy.EpoxyController
-import com.ivianuu.epoxyprefs.CategoryPreference
-import com.ivianuu.epoxyprefs.Preference
+import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.material.ListItem
+import androidx.ui.res.stringResource
 import com.ivianuu.essentials.ui.common.urlRoute
+import com.ivianuu.essentials.ui.compose.composeControllerRoute
+import com.ivianuu.essentials.ui.compose.core.composable
+import com.ivianuu.essentials.ui.compose.injekt.inject
+import com.ivianuu.essentials.ui.compose.material.EsTopAppBar
+import com.ivianuu.essentials.ui.compose.material.Subheader
+import com.ivianuu.essentials.ui.compose.prefs.PrefsScreen
 import com.ivianuu.essentials.ui.navigation.Navigator
-import com.ivianuu.essentials.ui.navigation.director.controllerRoute
+import com.ivianuu.essentials.ui.navigation.Route
 import com.ivianuu.essentials.ui.navigation.director.defaultControllerRouteOptionsOrNull
-import com.ivianuu.essentials.ui.prefs.PrefsController
 import com.ivianuu.essentials.util.BuildInfo
-import com.ivianuu.injekt.Inject
-import com.ivianuu.injekt.Param
-import com.ivianuu.injekt.parametersOf
 
 fun aboutRoute(
     hasDebugPackageName: Boolean = true,
     privacyPolicyUrl: String? = null
-) = controllerRoute<AboutController>(
+) = composeControllerRoute(
     options = defaultControllerRouteOptionsOrNull()
 ) {
-    parametersOf(hasDebugPackageName, privacyPolicyUrl)
+    PrefsScreen(
+        appBar = { EsTopAppBar(+stringResource(R.string.about_title)) },
+        prefs = {
+            AboutSection(
+                hasDebugPackageName = hasDebugPackageName,
+                showHeader = false,
+                privacyPolicyUrl = privacyPolicyUrl
+            )
+        }
+    )
 }
 
-@Inject
-internal class AboutController(
-    private val buildInfo: BuildInfo,
-    @Param private val hasDebugPackageName: Boolean,
-    @Param private val privacyPolicyUrl: String?
-) : PrefsController() {
-
-    override val toolbarTitleRes: Int?
-        get() = R.string.about_title
-
-    override fun epoxyController() = epoxyController {
-        AboutSection(
-            buildInfo = buildInfo,
-            navigator = navigator,
-            hasDebugPackageName = hasDebugPackageName,
-            showHeader = false,
-            privacyPolicyUrl = privacyPolicyUrl
-        )
-    }
-
-}
-
-fun EpoxyController.AboutSection(
-    buildInfo: BuildInfo,
-    navigator: Navigator,
+@Composable
+fun AboutSection(
+    buildInfo: BuildInfo = +inject<BuildInfo>(),
     showHeader: Boolean = false,
     hasDebugPackageName: Boolean = buildInfo.isDebug,
     privacyPolicyUrl: String? = null
-) {
+) = composable("AboutSection") {
     if (showHeader) {
-        CategoryPreference {
-            key("about_header")
-            titleRes(R.string.about_title)
-        }
+        Subheader(+stringResource(R.string.about_title))
     }
 
-    Preference {
-        key("about_rate")
-        titleRes(R.string.about_rate)
-        summaryRes(R.string.about_rate_summary)
-        onClick {
+    AboutItem(
+        titleRes = R.string.about_rate,
+        descRes = R.string.about_rate_desc,
+        route = {
             val packageName = if (hasDebugPackageName && buildInfo.isDebug) {
                 buildInfo.packageName.removeSuffix(".debug")
             } else {
                 buildInfo.packageName
             }
-            navigator.push(
-                urlRoute("https://play.google.com/store/apps/details?id=$packageName")
-            )
-            return@onClick true
-        }
-    }
 
-    Preference {
-        key("about_more_apps")
-        titleRes(R.string.about_more_apps)
-        summaryRes(R.string.about_more_apps_summary)
-        onClick {
-            navigator.push(
-                urlRoute("https://play.google.com/store/apps/developer?id=Manuel+Wrage")
-            )
-            return@onClick true
+            urlRoute("https://play.google.com/store/apps/details?id=$packageName")
         }
-    }
+    )
 
-    Preference {
-        key("about_reddit")
-        titleRes(R.string.about_reddit)
-        summaryRes(R.string.about_reddit_summary)
-        onClick {
-            navigator.push(
-                urlRoute("https://www.reddit.com/r/manuelwrageapps")
-            )
-            return@onClick true
-        }
-    }
+    AboutItem(
+        titleRes = R.string.about_more_apps,
+        descRes = R.string.about_more_apps_desc,
+        route = { urlRoute("https://play.google.com/store/apps/developer?id=Manuel+Wrage") }
+    )
 
-    Preference {
-        key("about_github")
-        titleRes(R.string.about_github)
-        summaryRes(R.string.about_github_summary)
-        onClick {
-            navigator.push(
-                urlRoute("https://github.com/IVIanuu")
-            )
-            return@onClick true
-        }
-    }
+    AboutItem(
+        titleRes = R.string.about_reddit,
+        descRes = R.string.about_reddit_desc,
+        route = { urlRoute("https://www.reddit.com/r/manuelwrageapps\"") }
+    )
 
-    Preference {
-        key("about_twitter")
-        titleRes(R.string.about_twitter)
-        summaryRes(R.string.about_twitter_summary)
-        onClick {
-            navigator.push(
-                urlRoute("https://twitter.com/IVIanuu")
-            )
-            return@onClick true
-        }
-    }
+    AboutItem(
+        titleRes = R.string.about_github,
+        descRes = R.string.about_github_desc,
+        route = { urlRoute("https://github.com/IVIanuu") }
+    )
+
+    AboutItem(
+        titleRes = R.string.about_twitter,
+        descRes = R.string.about_twitter_desc,
+        route = { urlRoute("https://twitter.com/IVIanuu") }
+    )
 
     if (privacyPolicyUrl != null) {
-        Preference {
-            key("privacy_policy")
-            titleRes(R.string.about_privacy_policy)
-            onClick {
-                navigator.push(
-                    urlRoute(privacyPolicyUrl)
-                )
-                return@onClick true
-            }
-        }
+        AboutItem(
+            titleRes = R.string.about_privacy_policy,
+            route = { urlRoute(privacyPolicyUrl) }
+        )
     }
+}
+
+@Composable
+fun AboutItem(
+    titleRes: Int,
+    descRes: Int? = null,
+    route: () -> Route
+) = composable(titleRes + (descRes ?: 0)) {
+    val navigator = +inject<Navigator>()
+    ListItem(
+        text = +stringResource(titleRes),
+        secondaryText = descRes?.let { +stringResource(it) },
+        onClick = { navigator.push(route()) }
+    )
 }
