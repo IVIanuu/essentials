f3e03352c8663a946f36cb334fd471f7dd516baa
IVIanuu <ivianuu@gmail.com>
2019-10-24 15:10:34 +0200

    Improve SliderPreference layout

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/prefs/Slider.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/prefs/Slider.kt
@@ -3,14 +3,15 @@ package com.ivianuu.essentials.ui.compose.prefs
 import androidx.compose.Composable
 import androidx.compose.state
 import androidx.compose.unaryPlus
+import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.dp
-import androidx.ui.layout.Column
 import androidx.ui.layout.ConstrainedBox
 import androidx.ui.layout.CrossAxisAlignment
 import androidx.ui.layout.DpConstraints
 import androidx.ui.layout.FlexRow
 import androidx.ui.layout.Padding
+import androidx.ui.layout.Stack
 import androidx.ui.material.themeTextStyle
 import com.ivianuu.essentials.ui.compose.core.composable
 import com.ivianuu.essentials.ui.compose.material.Slider
@@ -37,53 +38,59 @@ fun SliderPreference(
         }
     }
 
-    Column {
-        Preference(
-            pref = pref,
-            title = title,
-            summary = summary,
-            singleLineSummary = singleLineSummary,
-            leading = leading,
-            onChange = onChange,
-            enabled = enabled,
-            dependencies = dependencies
-        )
-
-        FlexRow(
-            crossAxisAlignment = CrossAxisAlignment.Center
-        ) {
-            val internalValue = +state { pref.get() }
-
-            val onChanged: ((Int) -> Unit)? = if (dependencies.checkAll()) {
-                { newValue ->
-                    if (onChange?.invoke(newValue) != false) {
-                        internalValue.value = newValue
-                    }
-                }
-            } else {
-                null
-            }
-
-            flexible(1f) {
-                Slider(
-                    value = pref.get(),
-                    min = min,
-                    max = max,
-                    divisions = divisions,
-                    onChanged = onChanged,
-                    onChangeEnd = { valueChanged(it) }
+    Stack {
+        aligned(Alignment.BottomCenter) {
+            Padding(bottom = 16.dp) {
+                Preference(
+                    pref = pref,
+                    title = title,
+                    summary = summary,
+                    singleLineSummary = singleLineSummary,
+                    leading = leading,
+                    onChange = onChange,
+                    enabled = enabled,
+                    dependencies = dependencies
                 )
             }
+        }
 
-            if (valueText != null) {
-                inflexible {
-                    Padding(right = 8.dp) {
-                        ConstrainedBox(
-                            constraints = DpConstraints(
-                                minWidth = 36.dp
-                            )
-                        ) {
-                            valueText(internalValue.value)
+        aligned(Alignment.BottomCenter) {
+            FlexRow(
+                crossAxisAlignment = CrossAxisAlignment.Center
+            ) {
+                val internalValue = +state { pref.get() }
+
+                val onChanged: ((Int) -> Unit)? = if (dependencies.checkAll()) {
+                    { newValue ->
+                        if (onChange?.invoke(newValue) != false) {
+                            internalValue.value = newValue
+                        }
+                    }
+                } else {
+                    null
+                }
+
+                flexible(1f) {
+                    Slider(
+                        value = pref.get(),
+                        min = min,
+                        max = max,
+                        divisions = divisions,
+                        onChanged = onChanged,
+                        onChangeEnd = { valueChanged(it) }
+                    )
+                }
+
+                if (valueText != null) {
+                    inflexible {
+                        Padding(right = 8.dp) {
+                            ConstrainedBox(
+                                constraints = DpConstraints(
+                                    minWidth = 36.dp
+                                )
+                            ) {
+                                valueText(internalValue.value)
+                            }
                         }
                     }
                 }
