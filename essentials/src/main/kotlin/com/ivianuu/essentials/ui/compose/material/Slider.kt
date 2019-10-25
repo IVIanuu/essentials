c63f1d75bfeee0d3dfe499a97112ce60ab1b75ff
IVIanuu <ivianuu@gmail.com>
2019-10-25 10:08:05 +0200

    Slider clean

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Slider.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/material/Slider.kt
@@ -1,7 +1,9 @@
 package com.ivianuu.essentials.ui.compose.material
 
+import androidx.animation.AnimatedFloat
 import androidx.animation.TweenBuilder
 import androidx.compose.Composable
+import androidx.compose.State
 import androidx.compose.memo
 import androidx.compose.state
 import androidx.compose.unaryPlus
@@ -19,11 +21,9 @@ import androidx.ui.engine.geometry.Rect
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.Paint
 import androidx.ui.layout.Container
-import androidx.ui.layout.Stack
 import androidx.ui.lerp
 import androidx.ui.material.themeColor
 import com.ivianuu.essentials.ui.compose.core.composable
-import com.ivianuu.essentials.ui.compose.core.ref
 
 @Composable
 fun Slider(
@@ -37,105 +37,173 @@ fun Slider(
     color: Color = +themeColor { secondary }
 ) = composable("Slider") {
     WithConstraints { constraints ->
-        val (internalValue, setInternalValue) = +state {
-            val fraction = unlerp(value.toFloat(), max.toFloat(), min.toFloat())
-            lerp(0f, constraints.maxWidth.value.toFloat(), fraction)
-        }
-
+        val internalValue = +state { 0f }
         val overlayAnim = +animatedFloat(0f)
+        val state = +memo { SliderState(internalValue, overlayAnim) }
 
-        fun getCurrentFraction(): Float =
-            unlerp(internalValue, constraints.maxWidth.value.toFloat(), 0f)
-                .coerceIn(0f, 1f)
-
-        fun getCurrentUserValue(): Int {
-            return lerp(min, max, getCurrentFraction())
-                .coerceIn(min, max)
-                .let { value ->
-                    discretize(value, divisions?.let { divisions ->
-                        max / divisions
-                    })
-                }
+        // update state
+        +memo(
+            value,
+            onChanged,
+            onChangeStart,
+            onChangeEnd,
+            min,
+            max,
+            divisions,
+            color,
+            constraints.maxWidth.value
+        ) {
+            state.update(
+                value = value,
+                onChangeStart = onChangeStart,
+                onChanged = onChanged,
+                onChangeEnd = onChangeEnd,
+                min = min,
+                max = max,
+                divisions = divisions,
+                maxWidth = constraints.maxWidth.value
+            )
         }
 
-        fun notifyChangeStart() {
-            onChangeStart?.invoke(getCurrentUserValue())
-        }
+        RawDragGestureDetector(
+            dragObserver = object : DragObserver {
+                override fun onStart(downPosition: PxPosition) {
+                    super.onStart(downPosition)
+                    state.handleDragStart()
+                }
 
-        val notifiedChangeValue = +ref { value }
-        fun notifyChange() {
-            val currentValue = getCurrentUserValue()
-            if (notifiedChangeValue.value != currentValue) {
-                notifiedChangeValue.value = currentValue
-                onChanged?.invoke(currentValue)
-            }
-        }
+                override fun onDrag(dragDistance: PxPosition): PxPosition {
+                    state.handleDrag(dragDistance)
+                    return dragDistance
+                }
 
-        fun notifyChangeEnd() {
-            onChangeEnd?.invoke(getCurrentUserValue())
-        }
-
-        notifyChange()
-
-        RawDragGestureDetector(dragObserver = object : DragObserver {
-            override fun onStart(downPosition: PxPosition) {
-                super.onStart(downPosition)
-                if (onChanged != null) {
-                    overlayAnim.animateTo(
-                        targetValue = 1f,
-                        anim = TweenBuilder<Float>().apply { duration = 250 }
-                    )
-
-                    notifyChangeStart()
+                override fun onStop(velocity: PxPosition) {
+                    super.onStop(velocity)
+                    state.handleDragEnd()
                 }
             }
-
-            override fun onDrag(dragDistance: PxPosition): PxPosition {
-                if (onChanged != null) {
-                    setInternalValue(internalValue + dragDistance.x.value)
-                    notifyChange()
-                }
-                return dragDistance
-            }
-
-            override fun onStop(velocity: PxPosition) {
-                super.onStop(velocity)
-                if (onChanged != null) {
-                    overlayAnim.animateTo(
-                        targetValue = 0f,
-                        anim = TweenBuilder<Float>().apply { duration = 250 }
-                    )
-
-                    notifyChangeEnd()
-                }
-            }
-        }) {
+        ) {
             PressGestureDetector(
-                onPress = { position ->
-                    if (onChanged != null) {
-                        notifyChangeStart()
-                        setInternalValue(position.x.value)
-                        notifyChange()
-                        notifyChangeEnd()
-                    }
-                }
+                onPress = { state.handlePress(it) }
             ) {
                 Container(height = ContainerHeight, expanded = true) {
-                    //ColoredRect(Color.Red.copy(alpha = 0.1f))
-                    Stack {
-                        DrawSlider(
-                            getCurrentFraction(),
-                            overlayAnim.value,
-                            color,
-                            +themeColor { surface },
-                            divisions,
-                            onChanged != null
-                        )
-                    }
+                    DrawSlider(
+                        state.getCurrentFraction(),
+                        overlayAnim.value,
+                        color,
+                        +themeColor { surface },
+                        divisions,
+                        state.isEnabled
+                    )
                 }
             }
         }
     }
+}
+
+private class SliderState(
+    private val internalValue: State<Float>,
+    private val overlayAnim: AnimatedFloat
+) {
+
+    private var onChangeStart: ((Int) -> Unit)? = null
+    private var onChanged: ((Int) -> Unit)? = null
+    private var onChangeEnd: ((Int) -> Unit)? = null
+    private var min = 0
+    private var max = 0
+    private var divisions: Int? = null
+
+    private var maxWidth = 0
+
+    val isEnabled get() = onChanged != null
+
+    private var lastNotifiedValue: Int? = null
+
+    fun update(
+        value: Int,
+        onChangeStart: ((Int) -> Unit)?,
+        onChanged: ((Int) -> Unit)?,
+        onChangeEnd: ((Int) -> Unit)?,
+        min: Int,
+        max: Int,
+        divisions: Int?,
+        maxWidth: Int
+    ) {
+        this.onChangeStart = onChangeStart
+        this.onChanged = onChanged
+        this.onChangeEnd = onChangeEnd
+        this.min = min
+        this.max = max
+        this.divisions = divisions
+        this.maxWidth = maxWidth
+
+        val fraction = unlerp(value.toFloat(), min.toFloat(), max.toFloat())
+            .coerceIn(min.toFloat(), max.toFloat())
+        val newValue = lerp(0f, maxWidth.toFloat(), fraction)
+        internalValue.value = newValue
+        lastNotifiedValue = getCurrentUserValue()
+    }
+
+    fun handleDragStart() {
+        if (isEnabled) {
+            overlayAnim.animateTo(
+                targetValue = 1f,
+                anim = TweenBuilder<Float>().apply { duration = OverlayAnimDuration }
+            )
+
+            onChangeStart?.invoke(getCurrentUserValue())
+        }
+    }
+
+    fun handleDrag(dragDistance: PxPosition) {
+        if (isEnabled) {
+            internalValue.value = internalValue.value + dragDistance.x.value
+            notifyChange()
+        }
+    }
+
+    fun handlePress(position: PxPosition) {
+        if (isEnabled) {
+            handleDragStart()
+            internalValue.value = position.x.value
+            notifyChange()
+            handleDragEnd()
+        }
+    }
+
+    fun handleDragEnd() {
+        if (isEnabled) {
+            overlayAnim.animateTo(
+                targetValue = 0f,
+                anim = TweenBuilder<Float>().apply { duration = OverlayAnimDuration }
+            )
+
+            onChangeEnd?.invoke(getCurrentUserValue())
+        }
+    }
+
+    private fun notifyChange() {
+        val currentValue = getCurrentUserValue()
+        if (lastNotifiedValue != currentValue) {
+            lastNotifiedValue = currentValue
+            onChanged?.invoke(currentValue)
+        }
+    }
+
+    fun getCurrentFraction(): Float =
+        unlerp(internalValue.value, 0f, maxWidth.toFloat())
+            .coerceIn(0f, 1f)
+
+    private fun getCurrentUserValue(): Int {
+        return lerp(min, max, getCurrentFraction())
+            .coerceIn(min, max)
+            .let { value ->
+                discretize(value, divisions?.let { divisions ->
+                    max / divisions
+                })
+            }
+    }
+
 }
 
 @Composable
@@ -145,11 +213,11 @@ private fun DrawSlider(
     color: Color,
     surfaceColor: Color,
     divisions: Int?,
-    enabled: Boolean
+    isEnabled: Boolean
 ) = composable("DrawSlider") {
     val paint = +memo { Paint() }
 
-    Opacity(opacity = if (enabled) 1f else 0.5f) {
+    Opacity(opacity = if (isEnabled) 1f else 0.5f) {
         Draw { canvas, parentSize ->
             val thumbOverlayRadius = OverlayRadius.toPx().value
 
@@ -164,14 +232,14 @@ private fun DrawSlider(
                     } else {
                         tmp
                     }
-                }
+                }.coerceIn(startX, endX)
 
 
             val trackHeight = TrackHeight.toPx().value
 
             // track
             paint.color =
-                color.copy(alpha = if (enabled) InactiveTrackAlpha else DisabledInactiveTrackAlpha)
+                color.copy(alpha = if (isEnabled) InactiveTrackAlpha else DisabledInactiveTrackAlpha)
             canvas.drawRect(
                 Rect(startX, centerY - trackHeight / 2, endX, centerY + trackHeight / 2),
                 paint
@@ -179,7 +247,7 @@ private fun DrawSlider(
 
             // active track
             paint.color =
-                color.copy(alpha = if (enabled) ActiveTrackAlpha else DisabledActiveTrackAlpha)
+                color.copy(alpha = if (isEnabled) ActiveTrackAlpha else DisabledActiveTrackAlpha)
             canvas.drawRect(
                 Rect(startX, centerY - trackHeight / 2, currentX, centerY + trackHeight / 2),
                 paint
@@ -192,11 +260,11 @@ private fun DrawSlider(
                     val tickMarkX = (step * division)
                     if (tickMarkX <= currentX) {
                         paint.color = surfaceColor.copy(
-                            alpha = if (enabled) ActiveTickMarkAlpha else DisabledActiveTickMarkAlpha
+                            alpha = if (isEnabled) ActiveTickMarkAlpha else DisabledActiveTickMarkAlpha
                         )
                     } else {
                         paint.color = color.copy(
-                            alpha = if (enabled) InactiveTickMarkAlpha else DisabledInactiveTickMarkAlpha
+                            alpha = if (isEnabled) InactiveTickMarkAlpha else DisabledInactiveTickMarkAlpha
                         )
                     }
                     canvas.drawCircle(
@@ -216,15 +284,15 @@ private fun DrawSlider(
             }
 
             // disabled outline
-            if (!enabled) {
+            if (!isEnabled) {
                 paint.color = surfaceColor
                 canvas.drawCircle(Offset(currentX, centerY), ThumbOutlineRadius.toPx().value, paint)
             }
 
             // thumb
             val thumbRadius =
-                if (enabled) ThumbRadius.toPx().value else DisabledThumbRadius.toPx().value
-            paint.color = color.copy(alpha = if (enabled) ThumbAlpha else DisabledThumbAlpha)
+                if (isEnabled) ThumbRadius.toPx().value else DisabledThumbRadius.toPx().value
+            paint.color = color.copy(alpha = if (isEnabled) ThumbAlpha else DisabledThumbAlpha)
             canvas.drawCircle(Offset(currentX, centerY), thumbRadius, paint)
         }
     }
@@ -249,7 +317,9 @@ private const val ThumbAlpha = 1f
 private const val DisabledThumbAlpha = 0.32f
 private const val OverlayAlpha = 0.12f
 
-private fun unlerp(value: Float, max: Float, min: Float): Float =
+private val OverlayAnimDuration = 250
+
+private fun unlerp(value: Float, min: Float, max: Float): Float =
     if (max > min) (value - min) / (max - min) else 0f
 
 private fun discretize(
