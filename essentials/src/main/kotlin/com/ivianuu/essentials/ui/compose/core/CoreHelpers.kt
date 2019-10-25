3eae4c277378cc0cdb296d4dc364dc290547d324
IVIanuu <ivianuu@gmail.com>
2019-10-25 14:19:18 +0200

    Follow compose api design

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/core/CoreHelpers.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/core/CoreHelpers.kt
@@ -5,14 +5,51 @@ import androidx.compose.composer
 @PublishedApi
 internal val invocation = Any()
 
-inline fun composable(
+inline fun <V1> composable(
     key: Any,
-    inputs: Array<Any?>? = null,
+    v1: V1,
     block: () -> Unit
 ) {
     with(composer.composer) {
         startGroup(key)
-        if (inserting || inputs == null || changed(inputs.contentHashCode())) {
+        if (inserting || changed(v1)) {
+            startGroup(invocation)
+            block()
+            endGroup()
+        } else {
+            skipCurrentGroup()
+        }
+        endGroup()
+    }
+}
+
+inline fun <V1, V2> composable(
+    key: Any,
+    v1: V1,
+    v2: V2,
+    block: () -> Unit
+) {
+    with(composer.composer) {
+        startGroup(key)
+        if (inserting || changed(v1) || changed(v2)) {
+            startGroup(invocation)
+            block()
+            endGroup()
+        } else {
+            skipCurrentGroup()
+        }
+        endGroup()
+    }
+}
+
+inline fun composable(
+    key: Any,
+    vararg inputs: Array<Any?>,
+    block: () -> Unit
+) {
+    with(composer.composer) {
+        startGroup(key)
+        if (inserting || inputs.any { changed(it) }) {
             startGroup(invocation)
             block()
             endGroup()
@@ -24,5 +61,5 @@ inline fun composable(
 }
 
 inline fun staticComposable(key: Any, block: () -> Unit) {
-    composable(key = key, inputs = arrayOf(), block = block)
+    composable(key = key, v1 = Unit, block = block)
 }
