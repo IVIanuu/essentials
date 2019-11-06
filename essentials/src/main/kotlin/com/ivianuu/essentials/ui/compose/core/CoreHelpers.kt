17d5a18eb6cbc83c9031aa32768755e5420a3c87
IVIanuu <ivianuu@gmail.com>
2019-11-06 15:12:05 +0100

    Restart scope support

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/core/CoreHelpers.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/core/CoreHelpers.kt
@@ -16,77 +16,91 @@
 
 package com.ivianuu.essentials.ui.compose.core
 
+import androidx.compose.Composer
 import androidx.compose.composer
 
-inline fun composable(
+fun composable(
     key: Any,
     block: () -> Unit
 ) {
     with(composer.composer) {
-        startGroup(key)
-        block()
-        endGroup()
+        wrapInRestartScope(key) {
+            startGroup(key)
+            block()
+            endGroup()
+        }
     }
 }
 
-inline fun <V1> composable(
+fun <V1> composable(
     key: Any,
     v1: V1,
     block: () -> Unit
 ) {
     with(composer.composer) {
-        startGroup(key)
-        if (inserting || changed(v1)) {
-            startGroup(invocation)
-            block()
+        wrapInRestartScope(key) {
+            startGroup(key)
+            if (inserting || changed(v1)) {
+                startGroup(invocation)
+                block()
+                endGroup()
+            } else {
+                skipCurrentGroup()
+            }
             endGroup()
-        } else {
-            skipCurrentGroup()
         }
-        endGroup()
     }
 }
 
-inline fun <V1, V2> composable(
+fun <V1, V2> composable(
     key: Any,
     v1: V1,
     v2: V2,
     block: () -> Unit
 ) {
     with(composer.composer) {
-        startGroup(key)
-        if (inserting || changed(v1) || changed(v2)) {
-            startGroup(invocation)
-            block()
+        wrapInRestartScope(key) {
+            startGroup(key)
+            if (inserting || changed(v1) || changed(v2)) {
+                startGroup(invocation)
+                block()
+                endGroup()
+            } else {
+                skipCurrentGroup()
+            }
             endGroup()
-        } else {
-            skipCurrentGroup()
         }
-        endGroup()
     }
 }
 
-inline fun composable(
+fun composable(
     key: Any,
     vararg inputs: Any?,
     block: () -> Unit
 ) {
     with(composer.composer) {
-        startGroup(key)
-        if (inserting || changed(inputs.contentHashCode())) {
-            startGroup(invocation)
-            block()
+        wrapInRestartScope(key) {
+            startGroup(key)
+            if (inserting || changed(inputs.contentHashCode())) {
+                startGroup(invocation)
+                block()
+                endGroup()
+            } else {
+                skipCurrentGroup()
+            }
             endGroup()
-        } else {
-            skipCurrentGroup()
         }
-        endGroup()
     }
 }
 
-inline fun staticComposable(key: Any, block: () -> Unit) {
+fun staticComposable(key: Any, block: () -> Unit) {
     composable(key = key, v1 = Unit, block = block)
 }
 
-@PublishedApi
-internal val invocation = Any()
+private fun Composer<*>.wrapInRestartScope(key: Any, block: () -> Unit) {
+    startRestartGroup(key)
+    block()
+    endRestartGroup()?.updateScope { wrapInRestartScope(key, block) }
+}
+
+private val invocation = Any()
