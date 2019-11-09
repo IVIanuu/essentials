bbd6d98219912a5eeb918f4bd6e1f9b565a24008
IVIanuu <ivianuu@gmail.com>
2019-11-09 17:37:26 +0100

    Added FramedValue

---
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/FramedValue.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2019 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.ui.compose.common
+
+import androidx.compose.annotations.Hide
+import androidx.compose.frames.AbstractRecord
+import androidx.compose.frames.Framed
+import androidx.compose.frames.Record
+import androidx.compose.frames._created
+import androidx.compose.frames.readable
+import androidx.compose.frames.writable
+import kotlin.reflect.KProperty
+
+// todo remove once we can use @Model
+
+class FramedValue<T>(value: T) : Framed {
+    /* NOTE(lmr): When this module is compiled with IR, we will need to remove the below Framed implementation */
+
+    @Suppress("UNCHECKED_CAST")
+    var value: T
+        get() = next.readable(this).value
+        set(value) {
+            next.writable(this).value = value
+        }
+
+    private var next: StateRecord<T> =
+        StateRecord(value)
+
+    init {
+        _created(this)
+    }
+
+    // NOTE(lmr): ideally we can compile `State` with our own compiler so that this is not visible
+    @Hide
+    override val firstFrameRecord: Record
+        get() = next
+
+    // NOTE(lmr): ideally we can compile `State` with our own compiler so that this is not visible
+    @Hide
+    override fun prependFrameRecord(value: Record) {
+        value.next = next
+        @Suppress("UNCHECKED_CAST")
+        next = value as StateRecord<T>
+    }
+
+    private class StateRecord<T>(myValue: T) : AbstractRecord() {
+        override fun assign(value: Record) {
+            @Suppress("UNCHECKED_CAST")
+            this.value = (value as StateRecord<T>).value
+        }
+
+        override fun create(): Record =
+            StateRecord(value)
+
+        var value: T = myValue
+    }
+
+    operator fun component1(): T = value
+
+    operator fun component2(): (T) -> Unit = { value = it }
+
+
+    operator fun getValue(thisObj: Any?, property: KProperty<*>): T = value
+
+    operator fun setValue(thisObj: Any?, property: KProperty<*>, next: T) {
+        value = next
+    }
+}
