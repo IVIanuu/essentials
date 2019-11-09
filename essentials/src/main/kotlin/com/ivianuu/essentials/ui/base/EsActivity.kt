c913d3e18b852d38d53442c27a0f93bc8566b555
IVIanuu <ivianuu@gmail.com>
2019-11-09 14:26:46 +0100

    Disable edge to edge by default

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/base/EsActivity.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/base/EsActivity.kt
@@ -30,6 +30,7 @@ import com.ivianuu.essentials.ui.navigation.director.ControllerRenderer
 import com.ivianuu.essentials.ui.navigation.director.ControllerRoute
 import com.ivianuu.essentials.util.coroutineScope
 import com.ivianuu.essentials.util.unsafeLazy
+import com.ivianuu.essentials.util.withAlpha
 import com.ivianuu.injekt.InjektTrait
 import com.ivianuu.injekt.Module
 import com.ivianuu.injekt.android.activityComponent
@@ -58,7 +59,7 @@ abstract class EsActivity : AppCompatActivity(), InjektTrait, MvRxView {
     private val controllerRenderer: ControllerRenderer by inject()
 
     protected open val layoutRes: Int get() = 0
-    protected open val drawEdgeToEdge: Boolean get() = true
+    protected open val drawEdgeToEdge: Boolean get() = false
 
     open val containerId: Int
         get() = android.R.id.content
@@ -79,7 +80,7 @@ abstract class EsActivity : AppCompatActivity(), InjektTrait, MvRxView {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            window.statusBarColor = Color.TRANSPARENT
+            window.statusBarColor = Color.BLACK.withAlpha(0.25f)
             window.navigationBarColor = Color.TRANSPARENT
         }
 
