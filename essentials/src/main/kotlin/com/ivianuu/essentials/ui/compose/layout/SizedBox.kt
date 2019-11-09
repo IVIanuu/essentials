60ce9eb9b52f3729ff34956fb302df8613736463
IVIanuu <ivianuu@gmail.com>
2019-11-09 12:15:43 +0100

    Added SizedBox

---
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/layout/SizedBox.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.ui.compose.layout
+
+import androidx.compose.Composable
+import androidx.ui.core.Constraints
+import androidx.ui.core.Dp
+import androidx.ui.core.Layout
+import androidx.ui.core.WithDensity
+import androidx.ui.core.coerceIn
+import androidx.ui.core.dp
+import androidx.ui.core.ipx
+import com.ivianuu.essentials.ui.compose.core.composable
+
+@Composable
+fun SizedBox(
+    size: Dp,
+    child: @Composable() () -> Unit
+) = composable("SizedBox") {
+    SizedBox(width = size, height = size, child = child)
+}
+
+@Composable
+fun SizedBox(
+    width: Dp = 0.dp,
+    height: Dp = 0.dp,
+    child: @Composable() () -> Unit
+) = composable("SizedBox") {
+    WithDensity {
+        Layout(children = child) { measureables, constraints ->
+            val realWidth = width.toIntPx().coerceIn(constraints.minWidth, constraints.maxWidth)
+            val realHeight = height.toIntPx().coerceIn(constraints.minHeight, constraints.maxHeight)
+            val childConstraints = Constraints(maxWidth = realWidth, maxHeight = realHeight)
+            val placeable = measureables.firstOrNull()?.measure(childConstraints)
+            layout(realWidth, realHeight) {
+                placeable?.place(0.ipx, 0.ipx)
+            }
+        }
+    }
+}
