489847f657c8c8de7b96c80934142e7e60c7e833
IVIanuu <ivianuu@gmail.com>
2019-11-10 12:07:54 +0100

    Added ParceledState (fixes #99)

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/ParceledState.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/ParceledState.kt
@@ -16,34 +16,45 @@
 
 package com.ivianuu.essentials.ui.compose.common
 
+import android.app.Application
+import android.os.Parcelable
 import androidx.compose.State
+import androidx.compose.ambient
 import androidx.compose.effectOf
 import androidx.compose.onDispose
 import androidx.compose.state
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import com.ivianuu.essentials.ui.compose.core.ActivityAmbient
 import com.ivianuu.essentials.ui.compose.core.composable
+import com.ivianuu.essentials.ui.compose.injekt.inject
 import com.ivianuu.essentials.ui.compose.viewmodel.viewModel
 
-fun <T> retained(
-    key: Any,
+fun <T : Parcelable> parceled(
+    key: String,
     keepAcrossCompositions: Boolean = false,
     init: () -> T
 ) = effectOf<T> {
-    (+retainedState(key, keepAcrossCompositions, init)).value
+    (+parceledState(key, keepAcrossCompositions, init)).value
 }
 
-fun <T> retainedState(
-    key: Any,
+fun <T : Parcelable> parceledState(
+    key: String,
     keepAcrossCompositions: Boolean = false,
     init: () -> T
 ) = effectOf<State<T>> {
-    val viewModel = +viewModel<RetainedStateViewModel>(
-        key = "RetainedState:${key.hashCode()}"
+    val viewModel = +viewModel<ParceledStateViewModel>(
+        factory = SavedStateViewModelFactory(
+            +inject<Application>(),
+            (+ambient(ActivityAmbient)) as SavedStateRegistryOwner
+        )
     )
 
     val state = +state {
-        if (viewModel.values.containsKey(key)) {
-            viewModel.values[key] as T
+        if (viewModel.handle.contains(key)) {
+            viewModel.handle.get(key)
         } else {
             init()
         }
@@ -52,17 +63,17 @@ fun <T> retainedState(
     if (!keepAcrossCompositions) {
         composable("clear value") {
             +onDispose {
-                viewModel.values.remove(key)
+                viewModel.handle.remove<T>(key)
             }
         }
     }
 
-    viewModel.values[key] = state.value
+    viewModel.handle.set(key, state.value)
 
     return@effectOf state
 }
 
 @PublishedApi
-internal class RetainedStateViewModel : ViewModel() {
-    val values = mutableMapOf<Any, Any?>()
-}
+internal class ParceledStateViewModel(
+    val handle: SavedStateHandle
+) : ViewModel()
