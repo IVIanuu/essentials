ea4939d7f079010d79a4d15c970a8fb3dc249119
IVIanuu <ivianuu@gmail.com>
2019-11-25 23:28:13 +0100

    Added scope

---
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/scope/Scope.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.ui.compose.scope
+
+import androidx.compose.Composable
+import com.ivianuu.essentials.ui.compose.core.effect
+import com.ivianuu.essentials.ui.compose.core.memo
+import com.ivianuu.essentials.ui.compose.core.onDispose
+import com.ivianuu.scopes.MutableScope
+import com.ivianuu.scopes.Scope
+
+@Composable
+fun scope(): Scope = effect {
+    val scope = memo { MutableScope() }
+    onDispose { scope.close() }
+    return@effect scope
+}
