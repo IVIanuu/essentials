c859e4c0bca084ce864bce884e86f39bc60218ac
IVIanuu <ivianuu@gmail.com>
2019-11-29 00:18:30 +0100

    Upadted injekt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -116,7 +116,7 @@ object Deps {
         "com.ivianuu.essentials:essentials-gradle-plugin:0.0.1-dev251"
 
     object Injekt {
-        private const val version = "0.0.1-dev78"
+        private const val version = "0.0.1-dev79"
         const val injekt = "com.ivianuu.injekt:injekt:$version"
         const val android = "com.ivianuu.injekt:injekt-android:$version"
         const val gradlePlugin = "com.ivianuu.injekt:injekt-gradle-plugin:$version"
