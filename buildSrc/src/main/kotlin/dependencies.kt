29c81f301179cf18603439abb1d646ea14ac85da
IVIanuu <ivianuu@gmail.com>
2019-12-05 14:10:53 +0100

    Remove unwrapped boxes

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev270"
+    const val version = "${Build.versionName}-dev271"
 }
 
 object Deps {
