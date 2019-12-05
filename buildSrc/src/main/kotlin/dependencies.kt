338fb8bbb114a15610a284cfc067d4e7d4071c37
IVIanuu <ivianuu@gmail.com>
2019-12-05 13:53:56 +0100

    Fix enum string set

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev268"
+    const val version = "${Build.versionName}-dev269"
 }
 
 object Deps {
