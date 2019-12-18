4c9bbe4da446899fe14b4293c7ba8686ebfb5f4b
IVIanuu <ivianuu@gmail.com>
2019-12-18 13:30:09 +0100

    Ref clean

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/Ref.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/compose/common/Ref.kt
@@ -21,30 +21,20 @@ import androidx.compose.remember
 import com.ivianuu.essentials.ui.compose.core.Stable
 
 @Composable
-fun <T> ref(init: () -> T): Ref<T> = remember {
-    Ref(
-        init()
-    )
-}
+inline fun <T> ref(crossinline init: () -> T): Ref<T> = remember { Ref(init()) }
 
 @Composable
-fun <T, V1> refFor(v1: V1, init: () -> T): Ref<T> = remember(v1) {
-    Ref(init())
-}
+inline fun <T, V1> refFor(v1: V1, crossinline init: () -> T): Ref<T> = remember(v1) { Ref(init()) }
 
 @Composable
-fun <T, V1, V2> refFor(
+inline fun <T, V1, V2> refFor(
     v1: V1,
     v2: V2,
-    init: () -> T
-): Ref<T> = remember(v1, v2) {
-    Ref(
-        init()
-    )
-}
+    crossinline init: () -> T
+): Ref<T> = remember(v1, v2) { Ref(init()) }
 
 @Composable
-fun <T> refFor(vararg inputs: Any?, init: () -> T): Ref<T> = remember(*inputs) {
+inline fun <T> refFor(vararg inputs: Any?, crossinline init: () -> T): Ref<T> = remember(*inputs) {
     Ref(init())
 }
 
