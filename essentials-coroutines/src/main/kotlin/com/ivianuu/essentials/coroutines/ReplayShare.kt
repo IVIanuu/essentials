9967e1a6dc9486dca79cdd8a8959dae37f3e1c5b
IVIanuu <ivianuu@gmail.com>
2019-12-31 16:07:03 +0100

    Cleaned replay share

--- essentials-coroutines/src/main/kotlin/com/ivianuu/essentials/coroutines/ReplayShare.kt
+++ essentials-coroutines/src/main/kotlin/com/ivianuu/essentials/coroutines/ReplayShare.kt
@@ -16,14 +16,12 @@
 
 package com.ivianuu.essentials.coroutines
 
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlin.time.Duration
 
 fun <T> Flow<T>.replayShareIn(
@@ -46,20 +44,12 @@ private fun <T> Flow<T>.replayShareImpl(
     tag: String?
 ): Flow<T> {
     var lastValue: Any? = defaultValue
-
-    val upstream = this
+    return this
         .onEach { lastValue = it }
         .onCompletion { lastValue = defaultValue }
         .catch { lastValue = defaultValue }
         .shareIn(scope = scope, cacheSize = 0, timeout = timeout, tag = tag)
-
-    return flow {
-        try {
-            if (lastValue !== Null) emit(lastValue as T)
-            emitAll(upstream)
-        } catch (e: CancellationException) {
-        }
-    }
+        .onStart { if (lastValue !== Null) emit(lastValue as T) }
 }
 
 internal object Null
