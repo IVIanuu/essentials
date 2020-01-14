0b96addf0a151687317ee7ef6508828deacef8d1
IVIanuu <ivianuu@gmail.com>
2020-01-14 11:32:05 +0100

    Use icon style in ActionIcon

--- essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ui/ActionIcon.kt
+++ essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ui/ActionIcon.kt
@@ -1,13 +1,14 @@
 package com.ivianuu.essentials.gestures.action.ui
 
 import androidx.compose.Composable
+import androidx.compose.ambient
 import com.ivianuu.essentials.gestures.action.Action
 import com.ivianuu.essentials.ui.coroutines.collect
 import com.ivianuu.essentials.ui.material.Icon
-import com.ivianuu.essentials.ui.material.IconStyle
+import com.ivianuu.essentials.ui.material.IconStyleAmbient
 
 @Composable
 fun ActionIcon(action: Action) {
     val icon = collect(action.iconProvider.icon) ?: return
-    Icon(image = icon.image, style = IconStyle(tint = icon.tint))
+    Icon(image = icon.image, style = ambient(IconStyleAmbient).copy(tint = icon.tint))
 }
