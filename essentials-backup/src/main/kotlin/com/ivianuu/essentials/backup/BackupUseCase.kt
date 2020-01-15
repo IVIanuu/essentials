c0be49b8d7f71d9f60e8d8426fdbad40108b3282
IVIanuu <ivianuu@gmail.com>
2020-01-15 16:26:31 +0100

    Backup improvements

--- essentials-backup/src/main/kotlin/com/ivianuu/essentials/backup/BackupUseCase.kt
+++ essentials-backup/src/main/kotlin/com/ivianuu/essentials/backup/BackupUseCase.kt
@@ -1,10 +1,13 @@
 package com.ivianuu.essentials.backup
 
 import android.annotation.SuppressLint
-import android.app.Activity
+import android.app.Application
 import android.content.Intent
 import androidx.core.content.FileProvider
+import com.ivianuu.essentials.ui.navigation.ActivityRoute
+import com.ivianuu.essentials.ui.navigation.NavigatorState
 import com.ivianuu.essentials.util.AppDispatchers
+import com.ivianuu.essentials.util.BuildInfo
 import com.ivianuu.essentials.util.Toaster
 import com.ivianuu.injekt.Factory
 import kotlinx.coroutines.withContext
@@ -18,8 +21,10 @@ import java.util.zip.ZipOutputStream
 
 @Factory
 class BackupUseCase(
-    private val activity: Activity,
+    private val application: Application,
+    private val buildInfo: BuildInfo,
     private val dispatchers: AppDispatchers,
+    private val navigator: NavigatorState,
     private val toaster: Toaster
 ) {
 
@@ -28,9 +33,9 @@ class BackupUseCase(
         try {
             val dateFormat = SimpleDateFormat("dd_MM_yyyy_HH_mm_ss")
             val backupFileName = "backup_${dateFormat.format(Date())}"
-            val dataPath = activity.applicationInfo.dataDir
+            val dataPath = application.applicationInfo.dataDir
 
-            val backupFile = File("$dataPath/files/backup/$backupFileName.zip")
+            val backupFile = File("$dataPath/backups/$backupFileName.zip")
             backupFile.mkdirs()
             backupFile.createNewFile()
 
@@ -55,8 +60,8 @@ class BackupUseCase(
             withContext(dispatchers.main) {
                 val uri =
                     FileProvider.getUriForFile(
-                        activity,
-                        "${activity.packageName}.files",
+                        application,
+                        buildInfo.packageName,
                         backupFile
                     )
                 val intent = Intent(Intent.ACTION_SEND)
@@ -64,7 +69,7 @@ class BackupUseCase(
                 intent.data = uri
                 intent.putExtra(Intent.EXTRA_STREAM, uri)
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                activity.startActivity(Intent.createChooser(intent, "Share File"))
+                navigator.push(ActivityRoute { Intent.createChooser(intent, "Share File") })
             }
         } catch (e: Exception) {
             e.printStackTrace()
