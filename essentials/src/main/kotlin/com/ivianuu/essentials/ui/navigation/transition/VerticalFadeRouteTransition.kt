9046a227f2ff430e372e97b38f2e6e17237b2218
IVIanuu <ivianuu@gmail.com>
2020-01-15 15:11:34 +0100

    Fix vertical fade

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/navigation/transition/VerticalFadeRouteTransition.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/navigation/transition/VerticalFadeRouteTransition.kt
@@ -22,7 +22,7 @@ fun VerticalFadeRouteTransition(duration: Duration = 300.milliseconds) = RouteTr
         opsOf(
             OpacityRouteTransitionType.Opacity with transitionState[Alpha],
             ModifierRouteTransitionType.Modifier with LayoutPercentOffset(
-                percentY = 1f - transitionState[VerticalOffset]
+                percentY = transitionState[VerticalOffset]
             )
         )
     }
@@ -33,23 +33,23 @@ private fun verticalFadeRouteTransitionDefinition(
 ) = transitionDefinition {
     state(RouteTransition.State.Init) {
         set(Alpha, 0f)
-        set(VerticalOffset, 0f)
+        set(VerticalOffset, 0.3f)
     }
     state(RouteTransition.State.EnterFromPush) {
         set(Alpha, 1f)
-        set(VerticalOffset, 0.3f)
+        set(VerticalOffset, 0f)
     }
     state(RouteTransition.State.ExitFromPush) {
         set(Alpha, 1f)
-        set(VerticalOffset, 0.3f)
+        set(VerticalOffset, 0f)
     }
     state(RouteTransition.State.EnterFromPop) {
         set(Alpha, 1f)
-        set(VerticalOffset, 0.3f)
+        set(VerticalOffset, 0f)
     }
     state(RouteTransition.State.ExitFromPop) {
         set(Alpha, 0f)
-        set(VerticalOffset, 0f)
+        set(VerticalOffset, 0.3f)
     }
 
     transition {
