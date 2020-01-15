7563a40b633989afe53485736fc46aa36042b5cf
IVIanuu <ivianuu@gmail.com>
2020-01-15 20:01:13 +0100

    Disable icons module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ include(
     ":essentials-gradle-plugin",
     ":essentials-hide-nav-bar",
     ":essentials-kotlin-compiler",
-    ":essentials-material-icons",
+    //":essentials-material-icons",
     ":essentials-mvrx",
     ":essentials-notification-listener",
     ":essentials-permission",
