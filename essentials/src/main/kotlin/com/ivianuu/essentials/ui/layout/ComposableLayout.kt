e8d1babf5296f97e8da2f1ca4d24925eb8fd24f3
IVIanuu <ivianuu@gmail.com>
2020-01-22 23:23:05 +0100

    Added ComposableLayout

---
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/layout/ComposableLayout.kt
@@ -0,0 +1,79 @@
+package com.ivianuu.essentials.ui.layout
+
+import android.content.Context
+import androidx.compose.Composable
+import androidx.compose.Compose
+import androidx.compose.CompositionReference
+import androidx.compose.composer
+import androidx.compose.compositionReference
+import androidx.compose.remember
+import androidx.ui.core.Constraints
+import androidx.ui.core.ContextAmbient
+import androidx.ui.core.LayoutNode
+import androidx.ui.core.Measurable
+import androidx.ui.core.MeasureScope
+import androidx.ui.core.Modifier
+import androidx.ui.core.Ref
+import com.ivianuu.essentials.ui.core.current
+
+typealias ComposableMeasureBlock = MeasureScope.(
+    constraints: Constraints,
+    getMeasureables: () -> List<Measurable>,
+    recompose: () -> Unit
+) -> MeasureScope.LayoutResult
+
+@Composable
+fun ComposableLayout(
+    modifier: Modifier = Modifier.None,
+    children: @Composable() () -> Unit,
+    measureBlock: ComposableMeasureBlock
+) {
+    val state = remember { ComposableLayoutState() }
+    state.children = children
+    state.context = ContextAmbient.current
+    state.compositionRef = compositionReference()
+    state.measureBlock = measureBlock
+
+    composer.emit<LayoutNode>(
+        key = 0,
+        ctor = { LayoutNode() },
+        update = {
+            node.modifier = modifier
+            node.ref = state.nodeRef
+            node.measureBlocks = state.measureBlocks
+        }
+    )
+
+    state.recompose()
+}
+
+private class ComposableLayoutState {
+    val nodeRef = Ref<LayoutNode>()
+
+    lateinit var compositionRef: CompositionReference
+    lateinit var context: Context
+    lateinit var children: @Composable() () -> Unit
+    lateinit var measureBlock: ComposableMeasureBlock
+
+    val measureBlocks = object : LayoutNode.NoIntristicsMeasureBlocks(
+        error = "Intrinsic measurements are not supported by ComposableLayout"
+    ) {
+        override fun measure(
+            measureScope: MeasureScope,
+            measurables: List<Measurable>,
+            constraints: Constraints
+        ): MeasureScope.LayoutResult {
+            val root = nodeRef.value!!
+            return measureBlock(
+                measureScope,
+                constraints,
+                { root.layoutChildren },
+                { recompose() }
+            )
+        }
+    }
+
+    fun recompose() {
+        Compose.subcomposeInto(nodeRef.value!!, context, compositionRef, children)
+    }
+}
