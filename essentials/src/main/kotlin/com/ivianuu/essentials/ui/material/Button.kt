c72fc66c4569bf7108f6e11bb68f992c95ee8c41
IVIanuu <ivianuu@gmail.com>
2020-01-31 19:07:43 +0100

    Prepare for button style removal

--- essentials/src/main/kotlin/com/ivianuu/essentials/ui/material/Button.kt
+++ essentials/src/main/kotlin/com/ivianuu/essentials/ui/material/Button.kt
@@ -17,11 +17,79 @@
 package com.ivianuu.essentials.ui.material
 
 import androidx.compose.Composable
+import androidx.compose.Immutable
+import androidx.compose.remember
 import androidx.compose.staticAmbientOf
 import androidx.ui.core.Modifier
-import androidx.ui.material.ButtonStyle
-import androidx.ui.material.ContainedButtonStyle
+import androidx.ui.graphics.Brush
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.Shape
+import androidx.ui.graphics.SolidColor
+import androidx.ui.layout.EdgeInsets
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.contentColorFor
+import androidx.ui.unit.Dp
+import androidx.ui.unit.dp
 import com.ivianuu.essentials.ui.core.currentOrElse
+
+@Immutable
+data class ButtonStyle(
+    val backgroundColor: Color,
+    val contentColor: Color,
+    val shape: Shape,
+    val borderWidth: Dp = 0.dp,
+    val borderBrush: Brush? = null,
+    val elevation: Dp = 0.dp,
+    val paddings: EdgeInsets = EdgeInsets(
+        left = 16.dp,
+        top = 8.dp,
+        right = 16.dp,
+        bottom = 8.dp
+    )
+)
+
+@Composable
+fun ContainedButtonStyle(
+    backgroundColor: Color = MaterialTheme.colors().primary,
+    contentColor: Color = contentColorFor(backgroundColor),
+    shape: Shape = MaterialTheme.shapes().button,
+    elevation: Dp = 2.dp
+) = ButtonStyle(
+    backgroundColor = backgroundColor,
+    shape = shape,
+    elevation = elevation,
+    contentColor = contentColor
+)
+
+@Composable
+fun OutlinedButtonStyle(
+    borderWidth: Dp = 1.dp,
+    borderBrush: Brush = SolidColor(
+        MaterialTheme.colors().onSurface.copy(alpha = 0.12f)
+    ),
+    backgroundColor: Color = MaterialTheme.colors().surface,
+    contentColor: Color = MaterialTheme.colors().primary,
+    shape: Shape = MaterialTheme.shapes().button,
+    elevation: Dp = 0.dp
+) = ButtonStyle(
+    backgroundColor = backgroundColor,
+    shape = shape,
+    borderBrush = borderBrush,
+    borderWidth = borderWidth,
+    elevation = elevation,
+    contentColor = contentColor
+)
+
+@Composable
+fun TextButtonStyle(
+    shape: Shape = MaterialTheme.shapes().button,
+    contentColor: Color = MaterialTheme.colors().primary
+) = ButtonStyle(
+    backgroundColor = Color.Transparent,
+    shape = shape,
+    paddings = EdgeInsets(all = 8.dp),
+    contentColor = contentColor
+)
 
 val ButtonStyleAmbient = staticAmbientOf<ButtonStyle>()
 
@@ -30,10 +98,13 @@ fun Button(
     modifier: Modifier = Modifier.None,
     onClick: (() -> Unit)? = null,
     style: ButtonStyle = ButtonStyleAmbient.currentOrElse { ContainedButtonStyle() },
-    children: @Composable () -> Unit,
+    children: @Composable () -> Unit
 ) {
     androidx.ui.material.Button(
-        modifier = modifier, onClick = onClick, style = style, children = children
+        modifier = modifier,
+        onClick = onClick,
+        style = style.toAndroidxButtonStyle(),
+        children = children
     )
 }
 
@@ -42,9 +113,25 @@ fun Button(
     text: String,
     modifier: Modifier = Modifier.None,
     onClick: (() -> Unit)? = null,
-    style: ButtonStyle = ButtonStyleAmbient.currentOrElse { ContainedButtonStyle() },
+    style: ButtonStyle = ButtonStyleAmbient.currentOrElse { ContainedButtonStyle() }
 ) {
     androidx.ui.material.Button(
-        modifier = modifier, onClick = onClick, style = style, text = text.toUpperCase()
+        modifier = modifier,
+        onClick = onClick,
+        style = style.toAndroidxButtonStyle(),
+        text = text.toUpperCase()
+    )
+}
+
+@Composable
+private fun ButtonStyle.toAndroidxButtonStyle() = remember(this) {
+    androidx.ui.material.ButtonStyle(
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        shape = shape,
+        borderWidth = borderWidth,
+        borderBrush = borderBrush,
+        elevation = elevation,
+        paddings = paddings
     )
 }
