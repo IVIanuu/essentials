c8c9bb33e0cccd4c95bdfd0d3c7ea7edce01845a
ivianuu <ivianuu@gmail.com>
2020-03-18 10:42:15 +0100

    Clean

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -30,7 +30,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev492"
+    const val version = "${Build.versionName}-dev493"
 }
 
 object Deps {
