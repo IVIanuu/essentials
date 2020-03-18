8807f6650f7b351f84391166d70e20dc2b4aa061
ivianuu <ivianuu@gmail.com>
2020-03-18 10:43:17 +0100

    Fix split screen icon

--- essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/actions/SplitScreen.kt
+++ essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/actions/SplitScreen.kt
@@ -4,6 +4,7 @@ import android.accessibilityservice.AccessibilityService
 import android.os.Build
 import androidx.ui.material.icons.Icons
 import androidx.ui.material.icons.filled.PowerSettingsNew
+import androidx.ui.material.icons.filled.ViewAgenda
 import com.ivianuu.essentials.gestures.R
 import com.ivianuu.essentials.ui.image.Icon
 import com.ivianuu.injekt.ComponentBuilder
@@ -14,7 +15,7 @@ internal fun ComponentBuilder.splitScreenAction() {
             key = "split_screen",
             accessibilityAction = AccessibilityService.GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN,
             titleRes = R.string.es_action_split_screen,
-            icon = { Icon(Icons.Default.PowerSettingsNew) }
+            icon = { Icon(Icons.Default.ViewAgenda) }
         )
     }
 }
