d74c29030b5129fe6e54769e02a19bf907142efc
ivianuu <ivianuu@gmail.com>
2020-06-10 13:06:30 +0200

    Better pop up animation

--- essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/popup/PopupRoute.kt
+++ essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/popup/PopupRoute.kt
@@ -16,18 +16,27 @@
 
 package com.ivianuu.essentials.ui.popup
 
+import androidx.animation.FloatPropKey
+import androidx.animation.LinearOutSlowInEasing
+import androidx.animation.transitionDefinition
 import androidx.compose.Composable
+import androidx.compose.onActive
 import androidx.compose.remember
 import androidx.ui.core.ConfigurationAmbient
 import androidx.ui.core.Layout
 import androidx.ui.core.Modifier
+import androidx.ui.core.TransformOrigin
 import androidx.ui.core.gesture.tapGestureFilter
 import androidx.ui.foundation.Box
 import androidx.ui.unit.IntPx
 import androidx.ui.unit.IntPxBounds
+import androidx.ui.unit.center
 import androidx.ui.unit.dp
 import androidx.ui.unit.ipx
-import com.ivianuu.essentials.ui.animatedstack.animation.FadeStackAnimation
+import com.ivianuu.essentials.ui.animatable.AnimatableElementsAmbient
+import com.ivianuu.essentials.ui.animatable.animatableElement
+import com.ivianuu.essentials.ui.animatedstack.StackTransition
+import com.ivianuu.essentials.ui.common.transition
 import com.ivianuu.essentials.ui.common.untrackedState
 import com.ivianuu.essentials.ui.navigation.NavigatorAmbient
 import com.ivianuu.essentials.ui.navigation.Route
@@ -38,8 +47,8 @@ fun PopupRoute(
     popup: @Composable () -> Unit
 ) = Route(
     opaque = true,
-    enterAnimation = FadeStackAnimation(),
-    exitAnimation = FadeStackAnimation()
+    enterTransition = PopupStackAnimation(position),
+    exitTransition = PopupStackAnimation(position)
 ) {
     val navigator = NavigatorAmbient.current
 
@@ -59,16 +68,21 @@ fun PopupRoute(
         }
     }
 
-
     PopupLayout(
         position = position,
         modifier = Modifier.tapGestureFilter(
             onTap = { dismiss(true) }
         )
     ) {
-        Box(modifier = Modifier.tapGestureFilter(onTap = {}), children = popup)
+        Box(
+            modifier = Modifier.tapGestureFilter(onTap = {})
+                .animatableElement(PopupTag),
+            children = popup
+        )
     }
 }
+
+private val PopupTag = Any()
 
 @Composable
 private fun PopupLayout(
@@ -101,6 +115,80 @@ private fun PopupLayout(
 
         layout(constraints.maxWidth, constraints.maxHeight) {
             placeable.place(x = x, y = y)
+        }
+    }
+}
+
+private fun PopupStackAnimation(
+    position: IntPxBounds
+): StackTransition = { context ->
+    val popupElement = AnimatableElementsAmbient.current.getElement(PopupTag).value
+
+    if (context.isPush) onActive { context.addTo() }
+
+    val transitionState = transition(
+        definition = PopupTransitionDefinition,
+        initState = !context.isPush,
+        toState = context.isPush,
+        onStateChangeFinished = {
+            if (!context.isPush) context.removeFrom()
+            context.onComplete()
+        }
+    )
+
+    val transformOrigin = if (context.containerBounds != null) {
+        TransformOrigin(
+            pivotFractionX = if (position.left.value.toFloat() < context.containerBounds.center().x / 2) 0f else 1f,
+            pivotFractionY = if (position.top.value.toFloat() < context.containerBounds.center().y / 2) 0f else 1f
+        )
+    } else TransformOrigin.Center
+
+    popupElement.drawLayerModifier.alpha = transitionState[Alpha]
+    popupElement.drawLayerModifier.scaleX = transitionState[ScaleX]
+    popupElement.drawLayerModifier.scaleY = transitionState[ScaleY]
+    popupElement.drawLayerModifier.transformOrigin = transformOrigin
+}
+
+private val ScaleY = FloatPropKey()
+private val ScaleX = FloatPropKey()
+private val Alpha = FloatPropKey()
+
+private val PopupTransitionDefinition = transitionDefinition {
+    state(false) {
+        this[ScaleX] = 0f
+        this[ScaleY] = 0f
+        this[Alpha] = 0f
+    }
+    state(true) {
+        this[ScaleX] = 1f
+        this[ScaleY] = 1f
+        this[Alpha] = 1f
+    }
+    transition(false, true) {
+        ScaleX using tween {
+            duration = 120
+            easing = LinearOutSlowInEasing
+        }
+        ScaleY using tween {
+            duration = 180
+            easing = LinearOutSlowInEasing
+            delay = 60
+        }
+        Alpha using tween {
+            duration = 30
+        }
+    }
+    transition(true, false) {
+        ScaleX using tween {
+            duration = 1
+            delay = 120 - 1
+        }
+        ScaleY using tween {
+            duration = 1
+            delay = 120 - 1
+        }
+        Alpha using tween {
+            duration = 120
         }
     }
 }
