ad482b9eb18a09eee8c3bfc4a2e890f9ea9d98cc
ivianuu <ivianuu@gmail.com>
2020-06-14 23:09:05 +0200

    Fix deprecation

--- essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/common/ViewGroupCompose.kt
+++ essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/common/ViewGroupCompose.kt
@@ -4,13 +4,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.Composable
 import androidx.compose.Composition
+import androidx.compose.Recomposer
 import androidx.ui.core.setContent
 
 fun ViewGroup.compose(content: @Composable () -> Unit) {
     addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
         private var composition: Composition? = null
         override fun onViewAttachedToWindow(v: View?) {
-            composition = setContent(content)
+            composition = setContent(Recomposer.current(), content)
         }
 
         override fun onViewDetachedFromWindow(v: View?) {
