d947ce4ad740fa1cf8ed3a826cdff58fa9af50f8
ivianuu <ivianuu@gmail.com>
2020-06-16 18:09:53 +0200

    Fix bg

--- essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/material/Surface.kt
+++ essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/material/Surface.kt
@@ -32,7 +32,6 @@ import androidx.ui.graphics.RectangleShape
 import androidx.ui.graphics.Shape
 import androidx.ui.graphics.compositeOver
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.contentColorFor
 import androidx.ui.unit.Dp
 import androidx.ui.unit.dp
 import androidx.ui.unit.ipx
@@ -43,7 +42,7 @@ fun Surface(
     modifier: Modifier = Modifier,
     shape: Shape = RectangleShape,
     color: Color = MaterialTheme.colors.surface,
-    contentColor: Color = contentColorFor(color),
+    contentColor: Color = guessingContentColorFor(color),
     border: Border? = null,
     elevation: Dp = 0.dp,
     content: @Composable () -> Unit
