24dbbe121cc0ed733e07f7d6ea53d2916ea9432e
ivianuu <ivianuu@gmail.com>
2020-06-19 23:42:19 +0200

    Refactor system bars

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/DynamicSystemBarsPage.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/DynamicSystemBarsPage.kt
@@ -2,15 +2,22 @@ package com.ivianuu.essentials.sample.ui
 
 import androidx.compose.Composable
 import androidx.ui.core.Modifier
+import androidx.ui.core.onPositioned
+import androidx.ui.core.tag
 import androidx.ui.foundation.Box
+import androidx.ui.foundation.ScrollerPosition
+import androidx.ui.foundation.Text
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.graphics.Color
+import androidx.ui.layout.Stack
 import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.height
 import androidx.ui.savedinstancestate.rememberSavedInstanceState
 import androidx.ui.unit.dp
+import com.ivianuu.essentials.ui.common.SafeArea
 import com.ivianuu.essentials.ui.core.systemBarOverlayStyle
 import com.ivianuu.essentials.ui.dialog.ColorPickerPalette
+import com.ivianuu.essentials.ui.material.TopAppBar
 import com.ivianuu.essentials.util.isLight
 import com.ivianuu.injekt.Transient
 
@@ -19,20 +26,37 @@ class DynamicSystemBarsPage {
 
     @Composable
     operator fun invoke() {
-        val colors: List<Color> = rememberSavedInstanceState {
-            ColorPickerPalette.values()
-                .filter { it != ColorPickerPalette.Black && it != ColorPickerPalette.White }
-                .flatMap { it.colors }
-                .shuffled()
-        }
+        Stack {
+            val scrollerPosition = ScrollerPosition()
 
-        VerticalScroller {
-            colors.forEach { color ->
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                        .height(300.dp)
-                        .systemBarOverlayStyle(color.isLight),
-                    backgroundColor = color
+            VerticalScroller(scrollerPosition = scrollerPosition) {
+                val colors: List<Color> = rememberSavedInstanceState {
+                    ColorPickerPalette.values()
+                        .filter { it != ColorPickerPalette.Black && it != ColorPickerPalette.White }
+                        .flatMap { it.colors }
+                        .shuffled()
+                }
+
+                colors.forEach { color ->
+                    Box(
+                        modifier = Modifier.fillMaxWidth()
+                            .height(300.dp)
+                            .tag(color)
+                            .systemBarOverlayStyle(color.isLight),
+                        backgroundColor = color
+                    )
+                }
+            }
+
+            SafeArea {
+                TopAppBar(
+                    modifier = Modifier
+                        .onPositioned {
+                            println("${it.size.width} ${it.size.height}")
+                        },
+                    backgroundColor = Color.Transparent,
+                    elevation = 0.dp,
+                    title = { Text("Dynamic system bars") }
                 )
             }
         }
