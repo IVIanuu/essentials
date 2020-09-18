28519e126260a0ec85425b18bc015e3ee218cabe
ivianuu <ivianuu@gmail.com>
2020-09-18 13:52:12 +0200

    Replaced Switch with checkbox for now

--- essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/material/TmpSwitch.kt
+++ essentials-android-core/src/main/kotlin/com/ivianuu/essentials/ui/material/TmpSwitch.kt
@@ -1,13 +1,10 @@
 package com.ivianuu.essentials.ui.material
 
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.ivianuu.essentials.ui.common.untrackedState
 
 @Composable
 fun TmpSwitch(
@@ -17,15 +14,11 @@ fun TmpSwitch(
     enabled: Boolean = true,
     color: Color = MaterialTheme.colors.secondaryVariant
 ) {
-    var initialValueChange by untrackedState { true }
-    Switch(
+    Checkbox(
         checked = checked,
-        onCheckedChange = {
-            if (initialValueChange) initialValueChange = false
-            else onCheckedChange(it)
-        },
+        onCheckedChange = onCheckedChange,
         modifier = modifier,
         enabled = enabled,
-        color = color
+        checkedColor = color
     )
 }
