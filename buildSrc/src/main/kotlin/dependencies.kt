6c7bb427e0b342348de0adf80256db514bd9a26e
ivianuu <ivianuu@gmail.com>
2020-10-07 17:22:53 +0200

    Fix

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -28,7 +28,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev622"
+    const val version = "${Build.versionName}-dev624"
 }
 
 object Deps {
