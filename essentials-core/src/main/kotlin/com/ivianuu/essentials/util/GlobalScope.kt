e8f1427d645d760a8039d0242d01816d3dc94dda
ivianuu <ivianuu@gmail.com>
2020-10-12 10:36:23 +0200

    Added globalTask { ... } api

--- essentials-core/src/main/kotlin/com/ivianuu/essentials/util/GlobalScope.kt
+++ essentials-core/src/main/kotlin/com/ivianuu/essentials/util/GlobalScope.kt
@@ -1,11 +1,22 @@
 package com.ivianuu.essentials.util
 
+import com.ivianuu.injekt.Assisted
 import com.ivianuu.injekt.Binding
+import com.ivianuu.injekt.FunBinding
 import com.ivianuu.injekt.merge.ApplicationComponent
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 typealias GlobalScope = CoroutineScope
 
 @Binding(ApplicationComponent::class)
 fun globalScope(defaultDispatcher: DefaultDispatcher): GlobalScope =
     CoroutineScope(defaultDispatcher)
+
+@FunBinding
+suspend fun globalTask(
+    globalScope: GlobalScope,
+    block: @Assisted suspend CoroutineScope.() -> Unit,
+) {
+    globalScope.launch(block = block).join()
+}
