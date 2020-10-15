b6c8b5b67fbb017088469b33148c5783b376946f
ivianuu <ivianuu@gmail.com>
2020-10-15 23:38:04 +0200

    Fix absorb pointer (fixes #204)

--- essentials-ui-core/src/main/kotlin/com/ivianuu/essentials/ui/common/AbsorbPointer.kt
+++ essentials-ui-core/src/main/kotlin/com/ivianuu/essentials/ui/common/AbsorbPointer.kt
@@ -17,67 +17,18 @@
 package com.ivianuu.essentials.ui.common
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.gesture.rawPressStartGestureFilter
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.PointerId
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.PointerInputFilter
-import androidx.compose.ui.input.pointer.PointerInputModifier
-import androidx.compose.ui.unit.IntSize
 
 @Composable
 fun Modifier.absorbPointer(
     absorb: Boolean = true,
 ): Modifier = composed {
-    remember { AbsorbPointerGestureFilter() }
-        .also { it.absorb = absorb }
-}
-
-private class AbsorbPointerGestureFilter : PointerInputFilter(), PointerInputModifier {
-
-    var absorb = true
-
-    override val pointerInputFilter: PointerInputFilter
-        get() = this
-
-    private val consumedIds = mutableSetOf<PointerId>()
-
-    override fun onPointerEvent(
-        pointerEvent: PointerEvent,
-        pass: PointerEventPass,
-        bounds: IntSize,
-    ): List<PointerInputChange> {
-        // todo
-        return pointerEvent.changes
-        /*return if (absorb && (pass == PointerEventPass.Initial ||
-                    pass == PointerEventPass.PreDown ||
-                    pass == PointerEventPass.PostDown)
-        ) {
-            changes.map { change ->
-                if (change.changedToDown()) {
-                    change.consumeDownChange().also { consumedChange ->
-                        consumedIds += consumedChange.id
-                    }
-                } else {
-                    change
-                }
-            }
-        } else {
-            changes.map { change ->
-                if (change.id in consumedIds) {
-                    change.copy(consumed = change.consumed.copy(downChange = false))
-                        .also { consumedIds -= change.id }
-                } else {
-                    change
-                }
-            }
-        }*/
-    }
-
-    override fun onCancel() {
-        consumedIds.clear()
-    }
+    rawPressStartGestureFilter(
+        onPressStart = {},
+        enabled = absorb,
+        executionPass = PointerEventPass.Initial
+    )
 }
