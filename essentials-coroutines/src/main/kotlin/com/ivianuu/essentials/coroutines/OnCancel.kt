ffcea541514dd9e6355ba3e5dec5e52b3b96ef0c
ivianuu <ivianuu@gmail.com>
2020-11-21 18:31:40 +0100

    Added Flow.onCancel which allows the emission of additional items

---
+++ essentials-coroutines/src/main/kotlin/com/ivianuu/essentials/coroutines/OnCancel.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.essentials.coroutines
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.withContext
+
+fun <T> Flow<T>.onCancel(
+    action: suspend FlowCollector<T>.() -> Unit
+) = object : Flow<T> {
+    override suspend fun collect(collector: FlowCollector<T>) {
+        var fromCollector = false
+        try {
+            this@onCancel.collect { value ->
+                try {
+                    collector.emit(value)
+                } catch (e: CancellationException) {
+                    fromCollector = true
+                }
+            }
+            collector.emitAll(this@onCancel)
+        } catch (e: CancellationException) {
+            if (fromCollector) {
+                throw e
+            } else {
+                withContext(NonCancellable) {
+                    action(collector)
+                }
+            }
+        }
+    }
+}
