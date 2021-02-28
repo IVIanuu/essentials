f15b1169962eeecf54a607a8e26be6c58cf08445
manu <ivianuu@gmail.com>
2021-02-28 09:57:28 +0100

    Version bump

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -28,7 +28,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.essentials"
     const val vcsUrl = "https://github.com/IVIanuu/essentials"
-    const val version = "${Build.versionName}-dev757"
+    const val version = "${Build.versionName}-dev758"
 }
 
 object Deps {
