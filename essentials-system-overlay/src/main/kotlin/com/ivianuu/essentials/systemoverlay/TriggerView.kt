ea8ba00326d68b19d60bf11d9c4935c19b6274df
manu <ivianuu@gmail.com>
2021-03-28 15:25:51 +0200

    Fix crash

--- essentials-system-overlay/src/main/kotlin/com/ivianuu/essentials/systemoverlay/TriggerView.kt
+++ essentials-system-overlay/src/main/kotlin/com/ivianuu/essentials/systemoverlay/TriggerView.kt
@@ -4,10 +4,13 @@ import android.annotation.SuppressLint
 import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
+import com.github.michaelbull.result.getOrElse
+import com.github.michaelbull.result.runCatching
 
 @SuppressLint("ViewConstructor")
 class TriggerView(private val delegate: View) : FrameLayout(delegate.context) {
     @SuppressLint("ClickableViewAccessibility")
     override fun dispatchTouchEvent(event: MotionEvent): Boolean =
-        delegate.dispatchTouchEvent(event)
+        runCatching { delegate.dispatchTouchEvent(event) }
+            .getOrElse { false }
 }
