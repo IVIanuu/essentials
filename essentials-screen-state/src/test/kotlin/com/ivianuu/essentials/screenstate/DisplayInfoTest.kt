fc79be8f1274251147f550725882ad37a43f1f60
manu <ivianuu@gmail.com>
2021-03-30 12:51:34 +0200

    Clean

--- essentials-screen-state/src/test/kotlin/com/ivianuu/essentials/screenstate/DisplayInfoTest.kt
+++ essentials-screen-state/src/test/kotlin/com/ivianuu/essentials/screenstate/DisplayInfoTest.kt
@@ -1,12 +1,10 @@
 package com.ivianuu.essentials.screenstate
 
 import android.util.DisplayMetrics
-import android.view.Display
 import android.view.WindowManager
 import com.ivianuu.essentials.coroutines.EventFlow
 import com.ivianuu.essentials.test.runCancellingBlockingTest
 import com.ivianuu.essentials.test.testCollect
-import com.ivianuu.injekt.Given
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.mockk.every
