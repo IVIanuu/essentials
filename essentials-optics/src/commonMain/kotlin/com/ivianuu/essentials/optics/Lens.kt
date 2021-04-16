46bd9e97b0778714e2995e75a4a95be925778522
manu <ivianuu@gmail.com>
2021-04-16 12:45:33 +0200

    Lens tests

--- essentials-optics/src/commonMain/kotlin/com/ivianuu/essentials/optics/Lens.kt
+++ essentials-optics/src/commonMain/kotlin/com/ivianuu/essentials/optics/Lens.kt
@@ -9,3 +9,10 @@ fun <T, V> Lens(get: (T) -> V, set: (T, V) -> T): Lens<T, V> = object : Lens<T, 
     override fun get(t: T): V = get.invoke(t)
     override fun set(t: T, v: V): T = set.invoke(t, v)
 }
+
+inline fun <T, V> Lens<T, V>.update(t: T, transform: V.() -> V): T = set(t, transform(get(t)))
+
+operator fun <A, B, C> Lens<A, B>.plus(other: Lens<B, C>): Lens<A, C> = Lens(
+    { a -> other.get(get(a)) },
+    { b, c -> set(b, other.set(get(b), c)) }
+)
---
+++ essentials-optics/src/jvmTest/kotlin/com/ivianuu/essentials/optics/LensTest.kt
@@ -0,0 +1,33 @@
+package com.ivianuu.essentials.optics
+
+import io.kotest.matchers.*
+import org.junit.*
+
+class LensTest {
+    private data class Foo(val value: Int)
+    private data class Bar(val foo: Foo)
+    private val fooLens = Lens<Foo, Int>(get = { it.value }, set = { a, b -> a.copy(value = b) })
+    private val barLens = Lens<Bar, Foo>(get = { it.foo }, set = { a, b -> a.copy(foo = b) })
+
+    @Test
+    fun testLens() {
+        var foo = Foo(0)
+        fooLens.get(foo) shouldBe 0
+        foo = fooLens.set(foo, 1)
+        foo.value shouldBe 1
+    }
+
+    @Test
+    fun testUpdate() {
+        var foo = Foo(0)
+        foo = fooLens.update(foo) { inc() }
+        foo.value shouldBe 1
+    }
+
+    @Test
+    fun testPlus() {
+        var bar = Bar(Foo(0))
+        bar = barLens.set(bar, Foo(1))
+        barLens.get(bar).value shouldBe 1
+    }
+}
