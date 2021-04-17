30194768cb57138b2cd2801a53ebf099de4600dd
manu <ivianuu@gmail.com>
2021-04-17 16:28:43 +0200

    Clean

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,6 @@ include(
     ":essentials-gradle-plugin",
     ":essentials-hide-nav-bar",
     ":essentials-ide-plugin",
-    ":essentials-lint",
     ":essentials-notification-listener",
     ":essentials-optics",
     ":essentials-permission",
