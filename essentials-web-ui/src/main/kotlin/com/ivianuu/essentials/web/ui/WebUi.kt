28e7c7581e69d2613832618586251bfbd4596407
manu <ivianuu@gmail.com>
2021-04-23 17:54:54 +0200

    Fix web ui has not respected insets

--- essentials-web-ui/src/main/kotlin/com/ivianuu/essentials/web/ui/WebUi.kt
+++ essentials-web-ui/src/main/kotlin/com/ivianuu/essentials/web/ui/WebUi.kt
@@ -1,14 +1,17 @@
 package com.ivianuu.essentials.web.ui
 
 import android.webkit.*
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.res.*
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.viewinterop.*
 import androidx.compose.ui.window.*
 import com.ivianuu.essentials.ui.common.*
+import com.ivianuu.essentials.ui.core.*
 import com.ivianuu.essentials.ui.material.Scaffold
 import com.ivianuu.essentials.ui.material.TopAppBar
 import com.ivianuu.essentials.ui.navigation.*
@@ -30,15 +33,19 @@ fun webUi(
     Scaffold(
         topBar = { TopAppBar(title = { Text(key.title) }) },
         bottomBar = {
-            BottomAppBar {
-                IconButton(onClick = { webViewRef!!.goBack() }) {
-                    Icon(painterResource(R.drawable.es_ic_arrow_back_ios_new), null)
-                }
-                IconButton(onClick = { webViewRef!!.reload() }) {
-                    Icon(painterResource(R.drawable.es_ic_refresh), null)
-                }
-                IconButton(onClick = { navigator.push(UrlKey(webViewRef!!.url)) }) {
-                    Icon(painterResource(R.drawable.es_ic_open_in_browser), null)
+            Surface(color = MaterialTheme.colors.primary, elevation = 8.dp) {
+                InsetsPadding(left = false, top = false, right = false) {
+                    BottomAppBar(elevation = 0.dp) {
+                        IconButton(onClick = { webViewRef!!.goBack() }) {
+                            Icon(painterResource(R.drawable.es_ic_arrow_back_ios_new), null)
+                        }
+                        IconButton(onClick = { webViewRef!!.reload() }) {
+                            Icon(painterResource(R.drawable.es_ic_refresh), null)
+                        }
+                        IconButton(onClick = { navigator.push(UrlKey(webViewRef!!.url)) }) {
+                            Icon(painterResource(R.drawable.es_ic_open_in_browser), null)
+                        }
+                    }
                 }
             }
         }
