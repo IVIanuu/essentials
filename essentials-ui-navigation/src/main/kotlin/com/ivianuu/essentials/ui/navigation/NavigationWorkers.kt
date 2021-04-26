3fce947b5826a2a34194714e18f25d44524724f7
manu <ivianuu@gmail.com>
2021-04-26 13:36:59 +0200

    Moved declarations

--- essentials-ui-navigation/src/main/kotlin/com/ivianuu/essentials/ui/navigation/NavigationWorkers.kt
+++ essentials-ui-navigation/src/main/kotlin/com/ivianuu/essentials/ui/navigation/NavigationWorkers.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 Manuel Wrage
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ivianuu.essentials.ui.navigation
 
 import androidx.activity.*
@@ -24,6 +8,7 @@ import com.ivianuu.injekt.*
 import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.*
 
+/* Pops the top key on back presses */
 @Given
 fun androidBackPressHandler(
     @Given activity: ComponentActivity,
@@ -44,4 +29,17 @@ private fun OnBackPressedDispatcherOwner.backPresses() = callbackFlow<Unit> {
     }
     onBackPressedDispatcher.addCallback(callback)
     awaitClose { callback.remove() }
+}
+
+/* Pops to root on activity destroy */
+@Given
+fun popToRootOnActivityDestroyWorker(@Given navigator: Navigator): ScopeWorker<UiGivenScope> = {
+    runOnCancellation {
+        navigator.state
+            .first()
+            .backStack
+            .drop(1)
+            .filterIsInstance<Key<Any>>()
+            .forEach { navigator.pop(it) }
+    }
 }
