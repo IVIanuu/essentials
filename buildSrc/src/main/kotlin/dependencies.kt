4b23d3df58baa30931b89bb87b568e93d0c30d7d
manu <ivianuu@gmail.com>
2021-06-16 21:39:32 +0200

    Bumped compile and target sdks

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -18,9 +18,9 @@
 
 object Build {
   const val applicationId = "com.ivianuu.essentials.sample"
-  const val compileSdk = 29
+  const val compileSdk = 30
   const val minSdk = 24
-  const val targetSdk = 29
+  const val targetSdk = 30
   const val versionCode = 1
   const val versionName = "0.0.1"
 }
