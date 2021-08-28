058e4a9e689ad2c7b38115e137cc667e0e809384
manu <IVIanuu@gmail.com>
2021-08-28 22:58:00 +0200

    Added a option to allow the TriggerView to be used in the halo app

--- essentials-system-overlay/src/main/kotlin/com/ivianuu/essentials/systemoverlay/TriggerView.kt
+++ essentials-system-overlay/src/main/kotlin/com/ivianuu/essentials/systemoverlay/TriggerView.kt
@@ -26,21 +26,40 @@ import com.ivianuu.essentials.*
 class TriggerView(private val delegate: View) : FrameLayout(delegate.context) {
   var useDownTouchOffset = true
 
-  private val delegateLocation = intArrayOf(0, 0)
+  private val thisDownLocation = intArrayOf(0, 0)
   private val thisLocation = intArrayOf(0, 0)
+  private val delegateLocation = intArrayOf(0, 0)
 
   @SuppressLint("ClickableViewAccessibility")
   override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-    if (!useDownTouchOffset || ev.action == MotionEvent.ACTION_DOWN) {
-      getLocationOnScreen(thisLocation)
-    }
-
+    getLocationOnScreen(thisLocation)
     delegate.getLocationOnScreen(delegateLocation)
 
-    ev.offsetLocation(
-      (thisLocation[0] - delegateLocation[0]).toFloat(),
-      (thisLocation[1] - delegateLocation[1]).toFloat()
-    )
+    if (useDownTouchOffset) {
+      if (ev.action == MotionEvent.ACTION_DOWN) {
+        // capture down location
+        thisDownLocation[0] = thisLocation[0]
+        thisDownLocation[1] = thisLocation[1]
+      } else {
+        // translate to down location view
+        ev.offsetLocation(
+          (thisLocation[0] - thisDownLocation[0]).toFloat(),
+          (thisLocation[1] - thisDownLocation[1]).toFloat()
+        )
+      }
+
+      // translate to delegate view
+      ev.offsetLocation(
+        (thisDownLocation[0] - delegateLocation[0]).toFloat(),
+        (thisDownLocation[1] - delegateLocation[1]).toFloat()
+      )
+    } else {
+      // translate to delegate view
+      ev.offsetLocation(
+        (thisLocation[0] - delegateLocation[0]).toFloat(),
+        (thisLocation[1] - delegateLocation[1]).toFloat()
+      )
+    }
 
     // compose crashes in some situations
     return catch { delegate.dispatchTouchEvent(ev) }
