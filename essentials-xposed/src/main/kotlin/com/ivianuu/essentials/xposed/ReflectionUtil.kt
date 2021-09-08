2faf17b0b249b1fa7d3a5dece824f947bed7c15e
manu <IVIanuu@gmail.com>
2021-09-09 01:34:19 +0200

    Removed reflection util overloads lol

--- essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/ReflectionUtil.kt
+++ essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/ReflectionUtil.kt
@@ -1,6 +1,5 @@
 package com.ivianuu.essentials.xposed
 
-import com.ivianuu.essentials.cast
 import de.robv.android.xposed.XposedHelpers
 import kotlin.reflect.KClass
 
@@ -15,32 +14,21 @@ fun <T : Any> KClass<T>.newInstance(
   parameterTypes: Array<KClass<*>>? = null
 ): T = XposedHelpers.newInstance(java, parameterTypes, *args) as T
 
-@JvmName("getFieldT")
-inline fun <reified T> Any.getField(fieldName: String): T = getField(fieldName).cast()
-
-fun Any.getField(fieldName: String): Any? = XposedHelpers.getObjectField(this, fieldName)
+fun <T> Any.getField(fieldName: String): T = XposedHelpers.getObjectField(this, fieldName) as T
 
 fun Any.setField(fieldName: String, value: Any?) {
   XposedHelpers.setObjectField(this, fieldName, value)
 }
 
-@JvmName("getStaticFieldT")
-inline fun <reified T> KClass<*>.getStaticField(fieldName: String): T =
-  getStaticField(fieldName).cast()
-
-fun KClass<*>.getStaticField(fieldName: String): Any? =
-  XposedHelpers.getStaticObjectField(java, fieldName)
+fun <T> KClass<*>.getStaticField(fieldName: String): T =
+  XposedHelpers.getStaticObjectField(java, fieldName) as T
 
 fun KClass<*>.setStaticField(fieldName: String, value: Any?) {
   XposedHelpers.setStaticObjectField(java, fieldName, value)
 }
 
-@JvmName("getAdditionalFieldT")
-inline fun <reified T> Any.getAdditionalField(fieldName: String): T =
-  getAdditionalField(fieldName).cast()
-
-fun Any.getAdditionalField(fieldName: String): Any? =
-  XposedHelpers.getAdditionalInstanceField(this, fieldName)
+fun <T> Any.getAdditionalField(fieldName: String): T =
+  XposedHelpers.getAdditionalInstanceField(this, fieldName) as T
 
 fun Any.setAdditionalField(fieldName: String, value: Any?) {
   XposedHelpers.setAdditionalInstanceField(this, fieldName, value)
@@ -50,12 +38,8 @@ fun Any.removeAdditionalField(fieldName: String) {
   XposedHelpers.removeAdditionalInstanceField(this, fieldName)
 }
 
-@JvmName("getAdditionalStaticFieldT")
-inline fun <reified T> KClass<*>.getAdditionalStaticField(fieldName: String): T =
-  getAdditionalStaticField(fieldName).cast()
-
-fun KClass<*>.getAdditionalStaticField(fieldName: String): Any? =
-  XposedHelpers.getAdditionalStaticField(java, fieldName)
+fun <T> KClass<*>.getAdditionalStaticField(fieldName: String): T =
+  XposedHelpers.getAdditionalStaticField(java, fieldName) as T
 
 fun KClass<*>.setAdditionalStaticField(fieldName: String, value: Any?) {
   XposedHelpers.setAdditionalStaticField(java, fieldName, value)
@@ -65,30 +49,18 @@ fun KClass<*>.removeAdditionalStaticField(fieldName: String) {
   XposedHelpers.removeAdditionalStaticField(java, fieldName)
 }
 
-@JvmName("callMethodT")
-inline fun <reified T> Any.callMethod(
+fun <T> Any.callMethod(
   methodName: String,
   vararg args: Any?,
   parameterTypes: Array<KClass<*>>? = null
-): T = callMethod(methodName, *args, parameterTypes = parameterTypes).cast()
+): T =
+  (if (parameterTypes == null) XposedHelpers.callMethod(this, methodName, *args)
+  else XposedHelpers.callMethod(this, methodName, parameterTypes, *args)) as T
 
-fun Any.callMethod(
+fun <T> KClass<*>.callStaticMethod(
   methodName: String,
   vararg args: Any?,
   parameterTypes: Array<KClass<*>>? = null
-): Any? = if (parameterTypes == null) XposedHelpers.callMethod(this, methodName, *args)
-else XposedHelpers.callMethod(this, methodName, parameterTypes, *args)
-
-@JvmName("callStaticMethodT")
-inline fun <reified T> KClass<*>.callStaticMethod(
-  methodName: String,
-  vararg args: Any?,
-  parameterTypes: Array<KClass<*>>? = null
-): T = callStaticMethod(methodName, *args, parameterTypes = parameterTypes).cast()
-
-fun KClass<*>.callStaticMethod(
-  methodName: String,
-  vararg args: Any?,
-  parameterTypes: Array<KClass<*>>? = null
-): Any? = if (parameterTypes == null) XposedHelpers.callStaticMethod(java, methodName, *args)
-else XposedHelpers.callStaticMethod(java, methodName, parameterTypes, *args)
+): T =
+  (if (parameterTypes == null) XposedHelpers.callStaticMethod(java, methodName, *args)
+  else XposedHelpers.callStaticMethod(java, methodName, parameterTypes, *args)) as T
