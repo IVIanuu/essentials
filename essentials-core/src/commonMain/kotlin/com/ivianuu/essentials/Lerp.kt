bfe68d0d7222c94248ccc8a31218c204468637da
manu <IVIanuu@gmail.com>
2021-09-16 13:22:21 +0200

    ....

--- essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
+++ essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
@@ -16,5 +16,14 @@
 
 package com.ivianuu.essentials
 
-fun lerp(start: Float, stop: Float, fraction: Float) =
-  (start * (1f - fraction) + stop * fraction)
+fun lerp(start: Float, stop: Float, fraction: Float): Float =
+  start * (1f - fraction) + stop * fraction
+
+fun lerp(start: Double, stop: Double, fraction: Float): Double =
+  start * (1f - fraction) + stop * fraction
+
+fun lerp(start: Int, stop: Int, fraction: Float): Int =
+  (start * (1f - fraction) + stop * fraction).toInt()
+
+fun lerp(start: Long, stop: Long, fraction: Float): Long =
+  (start * (1f - fraction) + stop * fraction).toLong()
