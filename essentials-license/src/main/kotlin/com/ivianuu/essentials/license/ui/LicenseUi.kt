c8b8e5cf445bde8e7f9f036524a9668884163925
manu <IVIanuu@gmail.com>
2021-09-23 22:33:01 +0200

    Renamed func

--- essentials-license/src/main/kotlin/com/ivianuu/essentials/license/ui/LicenseUi.kt
+++ essentials-license/src/main/kotlin/com/ivianuu/essentials/license/ui/LicenseUi.kt
@@ -24,13 +24,17 @@ import com.ivianuu.essentials.license.domain.GetLicenseProjectsUseCase
 import com.ivianuu.essentials.optics.Optics
 import com.ivianuu.essentials.resource.Idle
 import com.ivianuu.essentials.resource.Resource
-import com.ivianuu.essentials.resource.flowResultAsResource
+import com.ivianuu.essentials.resource.flowAsResource
 import com.ivianuu.essentials.store.action
 import com.ivianuu.essentials.store.state
 import com.ivianuu.essentials.ui.material.ListItem
 import com.ivianuu.essentials.ui.material.Scaffold
 import com.ivianuu.essentials.ui.material.TopAppBar
-import com.ivianuu.essentials.ui.navigation.*
+import com.ivianuu.essentials.ui.navigation.Key
+import com.ivianuu.essentials.ui.navigation.KeyUiScope
+import com.ivianuu.essentials.ui.navigation.ModelKeyUi
+import com.ivianuu.essentials.ui.navigation.Navigator
+import com.ivianuu.essentials.ui.navigation.UrlKey
 import com.ivianuu.essentials.ui.resource.ResourceLazyColumnFor
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.coroutines.NamedCoroutineScope
@@ -68,7 +72,7 @@ object LicenseKey : Key<Unit>
   scope: NamedCoroutineScope<KeyUiScope>
 ): StateFlow<LicenseModel> = scope.state(LicenseModel()) {
   flow { emit(getProjects()) }
-    .flowResultAsResource()
+    .flowAsResource()
     .update { copy(projects = it) }
 
   action(LicenseModel.openProject()) { project ->
