128a12569fbd55fb5e9e3eb7aa372e7cabaa7b82
manu <IVIanuu@gmail.com>
2021-10-02 11:52:51 +0200

    Lazy evaluate action collections

--- essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ActionRepository.kt
+++ essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ActionRepository.kt
@@ -5,21 +5,21 @@ import com.ivianuu.injekt.coroutines.DefaultDispatcher
 import kotlinx.coroutines.withContext
 
 @Provide class ActionRepository(
-  private val actions: Map<String, () -> Action<*>> = emptyMap(),
-  private val actionFactories: Set<() -> ActionFactory> = emptySet(),
-  private val actionsExecutors: Map<String, () -> ActionExecutor<*>> = emptyMap(),
-  private val actionSettings: Map<String, () -> ActionSettingsKey<*>> = emptyMap(),
-  private val actionPickerDelegates: Set<() -> ActionPickerDelegate> = emptySet(),
+  private val actions: () -> Map<String, () -> Action<*>> = { emptyMap() },
+  private val actionFactories: () -> Set<() -> ActionFactory> = { emptySet() },
+  private val actionsExecutors: () -> Map<String, () -> ActionExecutor<*>> = { emptyMap() },
+  private val actionSettings: () -> Map<String, () -> ActionSettingsKey<*>> = { emptyMap() },
+  private val actionPickerDelegates: () -> Set<() -> ActionPickerDelegate> = { emptySet() },
   private val dispatcher: DefaultDispatcher
 ) {
   suspend fun getAllActions(): List<Action<*>> = withContext(dispatcher) {
-    actions.values.map { it() }
+    actions().values.map { it() }
   }
 
   suspend fun getAction(id: String): Action<*>? = withContext(dispatcher) {
-    actions[id]
+    actions()[id]
       ?.invoke()
-      ?: actionFactories
+      ?: actionFactories()
         .asSequence()
         .map { it() }
         .firstOrNull { it.handles(id) }
@@ -27,9 +27,9 @@ import kotlinx.coroutines.withContext
   }
 
   suspend fun getActionExecutor(id: String): ActionExecutor<*>? = withContext(dispatcher) {
-    actionsExecutors[id]
+    actionsExecutors()[id]
       ?.invoke()
-      ?: actionFactories
+      ?: actionFactories()
         .asSequence()
         .map { it() }
         .firstOrNull { it.handles(id) }
@@ -37,8 +37,8 @@ import kotlinx.coroutines.withContext
   }
 
   suspend fun getActionSettingsKey(id: String): ActionSettingsKey<*>? =
-    withContext(dispatcher) { actionSettings[id]?.invoke() }
+    withContext(dispatcher) { actionSettings()[id]?.invoke() }
 
   suspend fun getActionPickerDelegates(): List<ActionPickerDelegate> =
-    withContext(dispatcher) { actionPickerDelegates.map { it() } }
+    withContext(dispatcher) { actionPickerDelegates().map { it() } }
 }
