30c6e23bd3f312d6dd75978a0e5f764c78ebcb1f
manu <IVIanuu@gmail.com>
2021-10-05 02:12:51 +0200

    Added toggle behavior to quick settings action

--- essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/actions/QuickSettings.kt
+++ essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/actions/QuickSettings.kt
@@ -17,16 +17,26 @@
 package com.ivianuu.essentials.gestures.action.actions
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Intent
+import android.view.accessibility.AccessibilityNodeInfo
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import com.ivianuu.essentials.AppContext
 import com.ivianuu.essentials.ResourceProvider
+import com.ivianuu.essentials.accessibility.AccessibilityConfig
+import com.ivianuu.essentials.accessibility.EsAccessibilityService
 import com.ivianuu.essentials.accessibility.GlobalActionExecutor
+import com.ivianuu.essentials.catch
 import com.ivianuu.essentials.gestures.R
 import com.ivianuu.essentials.gestures.action.Action
 import com.ivianuu.essentials.gestures.action.ActionExecutor
 import com.ivianuu.essentials.gestures.action.ActionId
+import com.ivianuu.essentials.getOrElse
 import com.ivianuu.essentials.loadResource
 import com.ivianuu.injekt.Provide
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
 @Provide object QuickSettingsActionId : ActionId("quick_settings")
 
@@ -38,7 +48,40 @@ import com.ivianuu.injekt.Provide
 )
 
 @Provide fun quickSettingsActionExecutor(
-  globalActionExecutor: GlobalActionExecutor
+  context: AppContext,
+  globalActionExecutor: GlobalActionExecutor,
+  service: Flow<EsAccessibilityService?>
 ): ActionExecutor<QuickSettingsActionId> = {
-  globalActionExecutor(AccessibilityService.GLOBAL_ACTION_QUICK_SETTINGS)
+  val targetState = catch {
+    val service = service.first()!!
+
+    val systemUiContext = context.createPackageContext(
+      "com.android.systemui", 0
+    )
+
+    val id = systemUiContext.resources.getIdentifier(
+      "accessibility_desc_quick_settings", "string", "com.android.systemui"
+    )
+
+    val notificationShadeDesc = systemUiContext.resources.getString(id)
+
+    fun AccessibilityNodeInfo.isQuickSettingsPanelVisibleRecursive(): Boolean {
+      if (packageName != "com.android.systemui") return false
+      if (paneTitle == notificationShadeDesc) return true
+      return (0 until childCount)
+        .any { getChild(it).isQuickSettingsPanelVisibleRecursive() }
+    }
+
+    return@catch !service.rootInActiveWindow.isQuickSettingsPanelVisibleRecursive()
+  }.getOrElse { true }
+
+  if (targetState)
+    globalActionExecutor(AccessibilityService.GLOBAL_ACTION_QUICK_SETTINGS)
+  else
+    context.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
 }
+
+@Provide val quickSettingsActionAccessibilityConfig = AccessibilityConfig(
+  flags = AccessibilityServiceInfo.CAPABILITY_CAN_RETRIEVE_WINDOW_CONTENT or
+      AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS
+)
