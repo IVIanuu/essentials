f3923132a57db25df1f5e5761656ab9d855374b4
manu <IVIanuu@gmail.com>
2021-10-05 13:16:31 +0200

    Removed useless extension

--- essentials-data/src/commonMain/kotlin/com/ivianuu/essentials/data/DataStore.kt
+++ essentials-data/src/commonMain/kotlin/com/ivianuu/essentials/data/DataStore.kt
@@ -17,8 +17,6 @@
 package com.ivianuu.essentials.data
 
 import com.ivianuu.essentials.optics.Lens
-import com.ivianuu.injekt.Provide
-import com.ivianuu.injekt.Spread
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
@@ -42,5 +40,3 @@ fun <T, S> DataStore<T>.lens(lens: Lens<T, S>): DataStore<S> = object : DataStor
   override suspend fun updateData(transform: S.() -> S): S =
     lens.get(this@lens.updateData { lens.set(this, transform(lens.get(this))) })
 }
-
-@Provide fun <@Spread T : DataStore<D>, D> dataStoreFlow(dataStore: T): Flow<D> = dataStore.data
