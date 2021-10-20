97e764e27421c0f38d37ff6057b5f8d18e2aeabf
manu <IVIanuu@gmail.com>
2021-10-20 20:08:11 +0200

    Updated injekt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -97,12 +97,12 @@ object Deps {
   const val dokkaGradlePlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.20"
 
   const val essentialsGradlePlugin =
-    "com.ivianuu.essentials:essentials-gradle-plugin:0.0.1-dev994"
+    "com.ivianuu.essentials:essentials-gradle-plugin:0.0.1-dev997"
 
   const val firebaseCrashlytics = "com.google.firebase:firebase-crashlytics:17.2.2"
 
   object Injekt {
-    private const val version = "0.0.1-dev624"
+    private const val version = "0.0.1-dev625"
     const val android = "com.ivianuu.injekt:injekt-android:$version"
     const val androidWork = "com.ivianuu.injekt:injekt-android-work:$version"
     const val core = "com.ivianuu.injekt:injekt-core:$version"
