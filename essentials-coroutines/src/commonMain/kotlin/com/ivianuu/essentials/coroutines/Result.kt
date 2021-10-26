025e6a5714b77558dc5fd1d06d3e617cd816979c
manu <IVIanuu@gmail.com>
2021-10-26 15:55:29 +0200

    ...

--- essentials-coroutines/src/commonMain/kotlin/com/ivianuu/essentials/coroutines/Result.kt
+++ essentials-coroutines/src/commonMain/kotlin/com/ivianuu/essentials/coroutines/Result.kt
@@ -3,11 +3,22 @@ package com.ivianuu.essentials.coroutines
 import com.ivianuu.essentials.Err
 import com.ivianuu.essentials.Ok
 import com.ivianuu.essentials.Result
+import com.ivianuu.essentials.err
+import com.ivianuu.essentials.nonFatalOrThrow
+import com.ivianuu.essentials.ok
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
-fun <T> Flow<Result<T, Throwable>>.unwrapResult() = flow {
+fun <T> Flow<T>.catchResult(): Flow<Result<T, Throwable>> = flow {
+  try {
+    this@catchResult.collect { emit(it.ok()) }
+  } catch (e: Throwable) {
+    emit(e.nonFatalOrThrow().err())
+  }
+}
+
+fun <T> Flow<Result<T, Throwable>>.unwrapResult(): Flow<T> = flow {
   this@unwrapResult.collect {
     when (it) {
       is Ok -> emit(it.value)
