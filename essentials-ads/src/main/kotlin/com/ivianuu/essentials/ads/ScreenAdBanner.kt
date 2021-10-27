7cd40cdbf260f1505262af906d0154bf2ee81d50
manu <IVIanuu@gmail.com>
2021-10-27 20:20:53 +0200

    Reduce content jumps caused by the ScreenAdBannerKeyUiDecorator

--- essentials-ads/src/main/kotlin/com/ivianuu/essentials/ads/ScreenAdBanner.kt
+++ essentials-ads/src/main/kotlin/com/ivianuu/essentials/ads/ScreenAdBanner.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Surface
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ivianuu.essentials.ui.core.InsetsPadding
 import com.ivianuu.essentials.ui.core.LocalInsets
+import com.ivianuu.essentials.ui.navigation.Key
+import com.ivianuu.essentials.ui.navigation.KeyUiComponent
 import com.ivianuu.essentials.ui.navigation.KeyUiDecorator
 import com.ivianuu.essentials.ui.navigation.LocalKeyUiComponent
 import com.ivianuu.injekt.Provide
+import com.ivianuu.injekt.common.Scoped
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 @Provide object ScreenAdBannerFeature : AdFeature
 
@@ -24,36 +30,42 @@ typealias ScreenAdBannerKeyUiDecorator = KeyUiDecorator
 @Provide fun adBannerKeyUiDecorator(
   isFeatureEnabled: IsAdFeatureEnabledUseCase,
   config: ScreenAdBannerConfig? = null,
-  showAdsFlow: Flow<ShowAds>
-): ScreenAdBannerKeyUiDecorator = decorator@ { content ->
-  if (config == null) {
-    content()
-    return@decorator
-  }
-
-  if (!isFeatureEnabled(LocalKeyUiComponent.current.key::class, ScreenAdBannerFeature)) {
-    content()
-    return@decorator
-  }
-
-  val showAds by showAdsFlow.collectAsState(null)
-
-  Column {
-    Box(modifier = Modifier.weight(1f)) {
-      val currentInsets = LocalInsets.current
-      CompositionLocalProvider(
-        LocalInsets provides if (showAds == true) currentInsets
-        else currentInsets.copy(bottom = 0.dp),
-        content = content
-      )
+  showAdsFlow: Flow<ShowAds>,
+  key: Key<*>
+): @Scoped<KeyUiComponent>  ScreenAdBannerKeyUiDecorator {
+  var showAds by mutableStateOf<Boolean?>(null)
+  return (decorator@ { content ->
+    if (config == null) {
+      content()
+      return@decorator
     }
 
-    if (showAds == true) {
-      Surface(elevation = 8.dp) {
-        InsetsPadding(top = false) {
-          AdBanner(config)
+    if (!isFeatureEnabled(LocalKeyUiComponent.current.key::class, ScreenAdBannerFeature)) {
+      content()
+      return@decorator
+    }
+
+    LaunchedEffect(true) {
+      showAdsFlow.collect { showAds = it }
+    }
+
+    Column {
+      Box(modifier = Modifier.weight(1f)) {
+        val currentInsets = LocalInsets.current
+        CompositionLocalProvider(
+          LocalInsets provides if (showAds == true) currentInsets
+          else currentInsets.copy(bottom = 0.dp),
+          content = content
+        )
+      }
+
+      if (showAds == true) {
+        Surface(elevation = 8.dp) {
+          InsetsPadding(top = false) {
+            AdBanner(config)
+          }
         }
       }
     }
-  }
+  })
 }
