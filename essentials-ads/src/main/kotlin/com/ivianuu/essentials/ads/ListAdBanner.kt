8d55082fd3c348157497eff1c7b0e3ed3df87a15
manu <IVIanuu@gmail.com>
2021-10-27 20:37:57 +0200

    Avoid list jumps

--- essentials-ads/src/main/kotlin/com/ivianuu/essentials/ads/ListAdBanner.kt
+++ essentials-ads/src/main/kotlin/com/ivianuu/essentials/ads/ListAdBanner.kt
@@ -1,13 +1,19 @@
 package com.ivianuu.essentials.ads
 
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.ivianuu.essentials.catch
 import com.ivianuu.essentials.getOrNull
+import com.ivianuu.essentials.scoped
+import com.ivianuu.essentials.storage
+import com.ivianuu.essentials.ui.LocalComponent
 import com.ivianuu.essentials.ui.common.ListDecorator
 import com.ivianuu.essentials.ui.navigation.LocalKeyUiComponent
 import com.ivianuu.injekt.Provide
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 @Provide object ListAdBannerFeature : AdFeature
 
@@ -24,7 +30,10 @@ typealias ListAdBannerListDecorator = ListDecorator
     item(null) {
       val key = catch { LocalKeyUiComponent.current.key::class }.getOrNull()
       if (key == null || isFeatureEnabled(key, ListAdBannerFeature)) {
-        val showAds by showAdsFlow.collectAsState(null)
+        var showAds by LocalComponent.current.storage.scoped { mutableStateOf<Boolean?>(null) }
+        LaunchedEffect(true) {
+          showAdsFlow.collect { showAds = it }
+        }
         if (showAds == true) AdBanner(config)
       }
     }
