93cef81fd23cfab2d273ba5a2d64d0e4975d5b2e
manu <IVIanuu@gmail.com>
2021-10-27 19:22:09 +0200

    Added component access apis

--- essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/ComponentStorage.kt
+++ essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/ComponentStorage.kt
@@ -3,9 +3,12 @@ package com.ivianuu.essentials
 import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.common.Component
+import com.ivianuu.injekt.common.ComponentObserver
 import com.ivianuu.injekt.common.Disposable
+import com.ivianuu.injekt.common.EntryPoint
 import com.ivianuu.injekt.common.Scoped
 import com.ivianuu.injekt.common.TypeKey
+import com.ivianuu.injekt.common.entryPoint
 import com.ivianuu.injekt.common.synchronized
 
 interface ComponentStorage<C : @Component Any> {
@@ -16,19 +19,12 @@ interface ComponentStorage<C : @Component Any> {
   fun remove(key: Any)
 }
 
-class ComponentStorageImpl<C : @Component Any> @Provide @Scoped<C> constructor() : ComponentStorage<C> {
-  private val values = mutableMapOf<Any, Any?>()
-
-  override operator fun <T> get(key: Any): T? = values[key] as? T
-
-  override operator fun <T> set(key: Any, value: T) {
-    values[key] = value
-  }
-
-  override fun remove(key: Any) {
-    values.remove(key)
-  }
+@EntryPoint<Any> interface ComponentStorageComponent<C : @Component Any> {
+  val componentStorage: ComponentStorage<C>
 }
+
+val <C : @Component Any> C.componentStorage: ComponentStorage<C>
+  get() = entryPoint<ComponentStorageComponent<C>>(this).componentStorage
 
 inline fun <T> ComponentStorage<*>.scoped(key: Any, computation: () -> T): T {
   synchronized(this) {
@@ -54,11 +50,25 @@ inline fun <T> ComponentStorage<*>.scoped(
       (holder.value as? Disposable)?.dispose()
       holder.value = computation()
       holder.args = args
-      //(holder.value as? ScopeObserver)?.init()
+      (holder.value as? ComponentObserver<*>)?.init()
     }
   }
 
   return holder.value as T
+}
+
+class ComponentStorageImpl<C : @Component Any> @Provide @Scoped<C> constructor() : ComponentStorage<C> {
+  private val values = mutableMapOf<Any, Any?>()
+
+  override operator fun <T> get(key: Any): T? = values[key] as? T
+
+  override operator fun <T> set(key: Any, value: T) {
+    values[key] = value
+  }
+
+  override fun remove(key: Any) {
+    values.remove(key)
+  }
 }
 
 @PublishedApi internal class ScopedValueHolder : Disposable {
