005e8ac4907b65e0f749797a649e818b071e5f0a
manu <IVIanuu@gmail.com>
2021-11-25 17:53:49 +0100

    Updated injekt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -103,7 +103,7 @@ object Deps {
   }
 
   object Injekt {
-    private const val version = "0.0.1-dev666"
+    private const val version = "0.0.1-dev667"
     const val android = "com.ivianuu.injekt:injekt-android:$version"
     const val androidWork = "com.ivianuu.injekt:injekt-android-work:$version"
     const val core = "com.ivianuu.injekt:injekt-core:$version"
