106341b192d53634f425fa163d95a37e4fcba925
manu <IVIanuu@gmail.com>
2021-11-26 18:32:32 +0100

    Fix

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/DynamicSystemBarsUi.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/DynamicSystemBarsUi.kt
@@ -16,13 +16,23 @@
 
 package com.ivianuu.essentials.sample.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.ivianuu.essentials.colorpicker.ColorPickerPalette
+import com.ivianuu.essentials.ui.common.VerticalList
 import com.ivianuu.essentials.ui.material.TopAppBar
 import com.ivianuu.essentials.ui.navigation.Key
 import com.ivianuu.essentials.ui.navigation.KeyUi
+import com.ivianuu.essentials.ui.systembars.systemBarStyle
+import com.ivianuu.essentials.ui.util.isLight
 import com.ivianuu.injekt.Provide
 
 @Provide val dynamicSystemBarsHomeItem = HomeItem("Dynamic system bars") { DynamicSystemBarsKey }
@@ -31,26 +41,29 @@ object DynamicSystemBarsKey : Key<Unit>
 
 @Provide val dynamicSystemBarsUi = KeyUi<DynamicSystemBarsKey> {
   Box {
-    /*val colors = rememberSaveable {
+    val colors = rememberSaveable {
       ColorPickerPalette.values()
         .filter { it != ColorPickerPalette.BLACK && it != ColorPickerPalette.WHITE }
         .flatMap { it.colors }
         .shuffled()
     }
     VerticalList(contentPadding = PaddingValues(0.dp)) {
-      items(colors) { color ->
-        Box(
-          modifier = Modifier
-            .fillMaxWidth()
-            .height(300.dp)
-            .background(color)
-            .systemBarStyle(
-              bgColor = Color.Black.copy(alpha = 0.2f),
-              lightIcons = color.isLight
-            )
-        )
+      // use items once fixed
+      for (color in colors) {
+        item {
+          Box(
+            modifier = Modifier
+              .fillMaxWidth()
+              .height(300.dp)
+              .background(color)
+              .systemBarStyle(
+                bgColor = Color.Black.copy(alpha = 0.2f),
+                lightIcons = color.isLight
+              )
+          )
+        }
       }
-    }*/
+    }
 
     TopAppBar(
       backgroundColor = Color.Transparent,
