24393349c591c39013b23876d0a81f6a25ee3a53
manu <IVIanuu@gmail.com>
2022-03-01 10:23:58 +0100

    Fixed ide plugin

---
+++ essentials-ide-plugin/src/main/kotlin/com/ivianuu/essentials/ide/EssentialsProjectInitalizer.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.ivianuu.essentials.ide
+
+import com.intellij.openapi.project.*
+import com.ivianuu.essentials.kotlin.compiler.experimental.*
+import com.ivianuu.essentials.kotlin.compiler.serializationfix.*
+import org.jetbrains.kotlin.utils.addToStdlib.*
+
+class EssentialsProjectInitializer : ProjectManagerListener {
+  override fun projectOpened(project: Project) {
+    experimental(project.cast())
+    serializationFix(project.cast())
+  }
+}
