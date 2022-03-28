bf8292d65239c601abec3b1222a04c35ff341a67
manu <IVIanuu@gmail.com>
2022-03-28 10:19:25 +0200

    Support days in iso8601 conversions

--- essentials-billing/src/main/kotlin/com/ivianuu/essentials/billing/Iso8601Duration.kt
+++ essentials-billing/src/main/kotlin/com/ivianuu/essentials/billing/Iso8601Duration.kt
@@ -10,13 +10,14 @@ fun String.toIso8601Duration(): Iso8601Duration = Iso8601Duration(
     'Y' -> Iso8601Duration.Unit.YEAR
     'M' -> Iso8601Duration.Unit.MONTH
     'W' -> Iso8601Duration.Unit.WEEK
+    'D' -> Iso8601Duration.Unit.DAY
     else -> throw IllegalArgumentException("Unexpected iso string $this")
   }
 )
 
 data class Iso8601Duration(val amount: Int, val unit: Unit) {
   enum class Unit {
-    YEAR, MONTH, WEEK
+    YEAR, MONTH, WEEK, DAY
   }
 }
 
@@ -24,4 +25,5 @@ fun Iso8601Duration.toReadableString() = "$amount " + when (unit) {
   Iso8601Duration.Unit.YEAR -> "year"
   Iso8601Duration.Unit.MONTH -> "month"
   Iso8601Duration.Unit.WEEK -> "week"
+  Iso8601Duration.Unit.DAY -> "day"
 } + " ${if (amount > 1) "s" else ""}"
