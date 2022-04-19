a53c874b3bcb6c055dc1074d3185573395e0f061
manu <IVIanuu@gmail.com>
2022-04-19 15:19:52 +0200

    Make PopupMenu scrollable lol

--- essentials-ui-popup/src/commonMain/kotlin/com/ivianuu/essentials/ui/popup/PopupMenu.kt
+++ essentials-ui-popup/src/commonMain/kotlin/com/ivianuu/essentials/ui/popup/PopupMenu.kt
@@ -6,12 +6,11 @@ package com.ivianuu.essentials.ui.popup
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,11 +33,13 @@ object PopupMenu {
 
 @Composable fun PopupMenu(items: List<PopupMenu.Item>) {
   Popup {
-    Column {
-      val component = LocalKeyUiElements.current<PopupMenuComponent>()
-      val scope = rememberCoroutineScope()
+    val component = LocalKeyUiElements.current<PopupMenuComponent>()
+    val scope = rememberCoroutineScope()
+
+    LazyColumn {
+      // todo use items(items) { } once fixed
       items.forEach { item ->
-        key(item) {
+        item {
           PopupMenuItem(
             onSelected = {
               scope.launch { component.navigator.pop(component.key) }
