73b43c0bd8448d14c459d9409832ada3b07228ea
manu <IVIanuu@gmail.com>
2022-10-30 17:49:51 +0100

    ...

--- essentials-state/src/commonMain/kotlin/com/ivianuu/essentials/state/State.kt
+++ essentials-state/src/commonMain/kotlin/com/ivianuu/essentials/state/State.kt
@@ -10,9 +10,11 @@ import androidx.compose.runtime.Composition
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MonotonicFrameClock
 import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.Snapshot
 import com.ivianuu.essentials.resource.Idle
 import com.ivianuu.essentials.resource.Resource
@@ -116,11 +118,7 @@ interface ProduceScope<T> : CoroutineScope {
   LaunchedEffect(state) {
     block(
       object : ProduceScope<T>, CoroutineScope by this {
-        override var value
-          get() = state.value
-          set(value) {
-            state.value = value
-          }
+        override var value by state
       }
     )
   }
