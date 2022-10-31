e8e8be072f8c8fb77cfc723974c2d1b80874bcc0
manu <IVIanuu@gmail.com>
2022-10-31 11:42:40 +0100

    ...

---
+++ essentials-permission/src/main/kotlin/com/ivianuu/essentials/permission/ignorebatteryoptimizations/IgnoreBatteryOptimizationsPermission.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.ivianuu.essentials.permission.ignorebatteryoptimizations
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.PowerManager
+import android.provider.Settings
+import androidx.core.net.toUri
+import com.ivianuu.essentials.BuildInfo
+import com.ivianuu.essentials.permission.Permission
+import com.ivianuu.essentials.permission.PermissionStateProvider
+import com.ivianuu.essentials.permission.intent.PermissionIntentFactory
+import com.ivianuu.injekt.Provide
+import com.ivianuu.injekt.android.SystemService
+
+interface IgnoreBatteryOptimizationsPermission : Permission
+
+@Provide
+fun <P : IgnoreBatteryOptimizationsPermission> ignoreBatteryOptimizationsPermissionStateProvider(
+  buildInfo: BuildInfo,
+  powerManager: @SystemService PowerManager
+) = PermissionStateProvider<P> {
+  powerManager.isIgnoringBatteryOptimizations(buildInfo.packageName)
+}
+
+@SuppressLint("BatteryLife")
+@Provide
+fun <P : IgnoreBatteryOptimizationsPermission> ignoreBatteryOptimizationsPermissionIntentFactory(
+  buildInfo: BuildInfo
+) = PermissionIntentFactory<P> {
+  Intent(
+    Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+    "package:${buildInfo.packageName}".toUri()
+  )
+}
---
+++ essentials-permission/src/main/kotlin/com/ivianuu/essentials/permission/packageusagestats/PackageUsageStatsPermission.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.ivianuu.essentials.permission.packageusagestats
+
+import android.app.AppOpsManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Process
+import android.provider.Settings
+import com.ivianuu.essentials.BuildInfo
+import com.ivianuu.essentials.permission.Permission
+import com.ivianuu.essentials.permission.PermissionStateProvider
+import com.ivianuu.essentials.permission.intent.PermissionIntentFactory
+import com.ivianuu.essentials.permission.intent.ShowFindPermissionHint
+import com.ivianuu.injekt.Provide
+import com.ivianuu.injekt.android.SystemService
+
+interface PackageUsageStatsPermission : Permission
+
+@Provide fun <P : PackageUsageStatsPermission> packageUsageStatsShowFindPermissionHint(
+) = ShowFindPermissionHint<P>(true)
+
+@Suppress("DEPRECATION")
+@Provide
+fun <P : PackageUsageStatsPermission> packageUsageStatsPermissionStateProvider(
+  appOpsManager: @SystemService AppOpsManager,
+  buildInfo: BuildInfo,
+) = PermissionStateProvider<P> {
+  appOpsManager.checkOpNoThrow(
+    AppOpsManager.OPSTR_GET_USAGE_STATS,
+    Process.myUid(),
+    buildInfo.packageName
+  ) == AppOpsManager.MODE_ALLOWED
+}
+
+@Provide fun <P : PackageUsageStatsPermission> notificationListenerPermissionIntentFactory(
+  buildInfo: BuildInfo
+) = PermissionIntentFactory<P> {
+  Intent(
+    Settings.ACTION_USAGE_ACCESS_SETTINGS,
+    Uri.parse("package:${buildInfo.packageName}")
+  )
+}
