77d8246510eeb1e5b29a09876f4922f1a5fc1ab2
manu <IVIanuu@gmail.com>
2022-10-31 15:01:44 +0100

    Revert "Revert "Simplified display rotation""
    This reverts commit 28e6066ff2bcbd8144f47824c0d9bdcb3db48c94.

--- essentials-screen-state/src/main/kotlin/com/ivianuu/essentials/screenstate/DisplayRotation.kt
+++ essentials-screen-state/src/main/kotlin/com/ivianuu/essentials/screenstate/DisplayRotation.kt
@@ -4,25 +4,27 @@
 
 package com.ivianuu.essentials.screenstate
 
+import android.hardware.SensorManager
+import android.view.OrientationEventListener
 import android.view.Surface
 import android.view.WindowManager
+import com.ivianuu.essentials.AppContext
+import com.ivianuu.essentials.coroutines.infiniteEmptyFlow
 import com.ivianuu.essentials.logging.Logger
 import com.ivianuu.essentials.logging.log
 import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.android.SystemService
-import com.ivianuu.injekt.coroutines.IOContext
+import com.ivianuu.injekt.coroutines.MainContext
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.withContext
 
 enum class DisplayRotation(val isPortrait: Boolean) {
   // 0 degrees
@@ -39,38 +41,41 @@ enum class DisplayRotation(val isPortrait: Boolean) {
 }
 
 @Provide fun displayRotation(
-  configChanges: () -> Flow<ConfigChange>,
-  rotationChanges: () -> Flow<RotationChange>,
+  context: AppContext,
+  coroutineContext: MainContext,
   screenState: () -> Flow<ScreenState>,
-  WM: @SystemService WindowManager,
-  L: Logger
-): Flow<DisplayRotation> = flow {
-  screenState()
-    .flatMapLatest { currentScreenState ->
-      if (currentScreenState.isOn) {
-        merge(rotationChanges(), configChanges())
-          .onStart { log { "sub for rotation" } }
-          .onCompletion { log { "dispose rotation" } }
-      } else {
-        log { "do not observe rotation while screen is off" }
-        emptyFlow()
+  L: Logger,
+  windowManager: @SystemService WindowManager
+): Flow<DisplayRotation> = screenState()
+  .flatMapLatest { currentScreenState ->
+    if (currentScreenState.isOn) {
+      callbackFlow {
+        val listener = object :
+          OrientationEventListener(context, SensorManager.SENSOR_DELAY_NORMAL) {
+          override fun onOrientationChanged(orientation: Int) {
+            trySend(orientation)
+          }
+        }
+        listener.enable()
+        awaitClose { listener.disable() }
       }
+        .flowOn(coroutineContext)
+        .map { getCurrentDisplayRotation() }
+        .onStart { log { "sub for rotation" } }
+        .onCompletion { log { "dispose rotation" } }
+    } else {
+      log { "do not observe rotation while screen is off" }
+      infiniteEmptyFlow()
     }
-    .onStart { emit(Unit) }
-    .map { getCurrentDisplayRotation() }
-    .distinctUntilChanged()
-    .let { emitAll(it) }
-}
-
-private suspend fun getCurrentDisplayRotation(
-  @Inject context: IOContext,
-  @Inject windowManager: @SystemService WindowManager,
-) = withContext(context) {
-  when (windowManager.defaultDisplay.rotation) {
-    Surface.ROTATION_0 -> DisplayRotation.PORTRAIT_UP
-    Surface.ROTATION_90 -> DisplayRotation.LANDSCAPE_LEFT
-    Surface.ROTATION_180 -> DisplayRotation.PORTRAIT_DOWN
-    Surface.ROTATION_270 -> DisplayRotation.LANDSCAPE_RIGHT
-    else -> error("Unexpected rotation")
   }
+  .distinctUntilChanged()
+
+private fun getCurrentDisplayRotation(
+  @Inject windowManager: @SystemService WindowManager,
+) = when (windowManager.defaultDisplay.rotation) {
+  Surface.ROTATION_0 -> DisplayRotation.PORTRAIT_UP
+  Surface.ROTATION_90 -> DisplayRotation.LANDSCAPE_LEFT
+  Surface.ROTATION_180 -> DisplayRotation.PORTRAIT_DOWN
+  Surface.ROTATION_270 -> DisplayRotation.LANDSCAPE_RIGHT
+  else -> error("Unexpected rotation")
 }
