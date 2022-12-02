c3ced28d4336943545ee20febdfcd687c07c3b4d
manu <IVIanuu@gmail.com>
2022-12-02 14:02:10 +0100

    Fix log auto completion

--- essentials-logging/src/commonMain/kotlin/com/ivianuu/essentials/logging/Logger.kt
+++ essentials-logging/src/commonMain/kotlin/com/ivianuu/essentials/logging/Logger.kt
@@ -21,8 +21,8 @@ interface Logger {
 
 inline fun log(
   priority: Logger.Priority = DEBUG,
-  @Inject tag: LoggingTag,
-  @Inject logger: Logger,
+  @Inject tag: LoggingTag = throw AssertionError(),
+  @Inject logger: Logger = throw AssertionError(),
   message: () -> String
 ) {
   if (logger.isEnabled.value) logger.log(message(), priority, tag)
