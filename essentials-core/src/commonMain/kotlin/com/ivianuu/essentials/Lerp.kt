5a376e2dfbb2289e6deb472a59ac0fd8eda85f06
manu <IVIanuu@gmail.com>
2022-12-03 12:12:45 +0100

    Added unlerp

--- essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
+++ essentials-core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
@@ -15,3 +15,15 @@ fun lerp(start: Int, stop: Int, fraction: Float): Int =
 
 fun lerp(start: Long, stop: Long, fraction: Float): Long =
   (start * (1f - fraction) + stop * fraction).toLong()
+
+fun unlerp(start: Float, stop: Float, value: Float): Float =
+  (if (stop - start == 0f) 0f else (value - start) / (stop - start)).coerceIn(0f, 1f)
+
+fun unlerp(start: Double, stop: Double, value: Double): Float =
+  unlerp(start.toFloat(), stop.toFloat(), value.toFloat())
+
+fun unlerp(start: Int, stop: Int, value: Int): Float =
+  unlerp(start.toFloat(), stop.toFloat(), value.toFloat())
+
+fun unlerp(start: Long, stop: Long, value: Long): Float =
+  unlerp(start.toFloat(), stop.toFloat(), value.toFloat())
