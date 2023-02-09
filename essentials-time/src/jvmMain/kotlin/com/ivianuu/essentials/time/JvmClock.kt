b4bf2ff42dd061062d825df5e1dbd1a6c8c70679
manu <IVIanuu@gmail.com>
2023-02-09 11:35:09 +0100

    ...

--- essentials-time/src/jvmMain/kotlin/com/ivianuu/essentials/time/JvmClock.kt
+++ essentials-time/src/jvmMain/kotlin/com/ivianuu/essentials/time/JvmClock.kt
@@ -6,4 +6,4 @@ package com.ivianuu.essentials.time
 
 import com.ivianuu.injekt.Provide
 
-@Provide actual val DefaultClock = Clock { System.nanoTime().nanoseconds }
+@Provide actual val DefaultClock = Clock { System.currentTimeMillis().milliseconds }
