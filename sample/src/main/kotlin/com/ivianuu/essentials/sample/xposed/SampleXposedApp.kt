b43dc8e283f38550d31c0cf74ea8535dd129c3ea
manu <IVIanuu@gmail.com>
2023-06-07 00:03:09 +0200

    Revert "Removed XposedScope"
    This reverts commit 3ef1daf62a654c142ad620131f03978b74f0b509.

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/xposed/SampleXposedApp.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/xposed/SampleXposedApp.kt
@@ -4,11 +4,11 @@
 
 package com.ivianuu.essentials.sample.xposed
 
-import com.ivianuu.essentials.AppScope
 import com.ivianuu.essentials.logging.XposedLogTag
 import com.ivianuu.essentials.xposed.EsXposedApp
 import com.ivianuu.essentials.xposed.ModulePackageName
 import com.ivianuu.essentials.xposed.XposedContext
+import com.ivianuu.essentials.xposed.XposedScope
 import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.Providers
@@ -19,8 +19,8 @@ import com.ivianuu.injekt.inject
 class SampleXposedApp : EsXposedApp() {
   @Providers(".**") override fun buildXposedElements(
     @Inject ctx: XposedContext,
-    @Inject scope: Scope<AppScope>
-  ): Elements<AppScope> {
+    @Inject scope: Scope<XposedScope>
+  ): Elements<XposedScope> {
     @Provide val logTag = XposedLogTag("EssentialsSample")
     @Provide val modulePackageName = ModulePackageName("com.ivianuu.essentials.sample")
     return inject()
