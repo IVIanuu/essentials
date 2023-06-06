c1cb89ec2993c2847a55035569e753c79d82326d
manu <IVIanuu@gmail.com>
2023-06-07 00:03:09 +0200

    Revert "Removed XposedScope"
    This reverts commit c31cb9a47e7c6686f64b8d2081b8f150f1e647c6.

--- essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/EsXposedApp.kt
+++ essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/EsXposedApp.kt
@@ -4,7 +4,6 @@
 
 package com.ivianuu.essentials.xposed
 
-import com.ivianuu.essentials.AppScope
 import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.common.Elements
@@ -12,12 +11,12 @@ import com.ivianuu.injekt.common.Scope
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-private lateinit var appScope: Scope<AppScope>
+private lateinit var appScope: Scope<XposedScope>
 
 abstract class EsXposedApp : IXposedHookLoadPackage {
   override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
     @Provide val context = XposedContextImpl(lpparam)
-    @Provide val scope = Scope<AppScope>()
+    @Provide val scope = Scope<XposedScope>()
       .also { appScope = it }
     @Provide val elements = buildXposedElements()
     elements.element<XposedHooksComponent>().hooks().forEach { hooks ->
@@ -31,6 +30,6 @@ abstract class EsXposedApp : IXposedHookLoadPackage {
 
   protected abstract fun buildXposedElements(
     @Inject ctx: XposedContext,
-    @Inject scope: Scope<AppScope>
-  ): Elements<AppScope>
+    @Inject scope: Scope<XposedScope>
+  ): Elements<XposedScope>
 }
--- essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/XposedScope.kt
+++ essentials-xposed/src/main/kotlin/com/ivianuu/essentials/xposed/XposedScope.kt
@@ -4,9 +4,10 @@
 
 package com.ivianuu.essentials.xposed
 
-import com.ivianuu.essentials.AppScope
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.common.Element
 
-@Provide @Element<AppScope>
+object XposedScope
+
+@Provide @Element<XposedScope>
 data class XposedHooksComponent(val hooks: () -> List<Hooks>)
