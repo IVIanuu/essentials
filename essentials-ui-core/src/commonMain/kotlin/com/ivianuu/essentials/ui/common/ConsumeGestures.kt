a19abeaac07b03076d0ce21cfe26e0f6eb76fb03
manu <IVIanuu@gmail.com>
2023-06-13 22:20:44 +0200

    Replaced Modifier.absorbPointer with Modifier.consumeGestures

---
+++ essentials-ui-core/src/commonMain/kotlin/com/ivianuu/essentials/ui/common/ConsumeGestures.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.ivianuu.essentials.ui.common
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+
+fun Modifier.consumeGestures(consume: Boolean = true): Modifier {
+  return if (!consume) this
+  else pointerInput(Unit) {
+    awaitPointerEventScope {
+      // we should wait for all new pointer events
+      while (true) {
+        awaitPointerEvent(pass = PointerEventPass.Initial)
+          .changes
+          .forEach { it.consume() }
+      }
+    }
+  }
+}
