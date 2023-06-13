b446b85dbf4b9413e2f31fff1a1451133ebf4dcd
manu <IVIanuu@gmail.com>
2023-06-13 12:50:54 +0200

    Added rememberScoped

--- essentials-compose/src/commonMain/kotlin/com/ivianuu/essentials/compose/LocalScope.kt
+++ essentials-compose/src/commonMain/kotlin/com/ivianuu/essentials/compose/LocalScope.kt
@@ -4,7 +4,38 @@
 
 package com.ivianuu.essentials.compose
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.currentCompositeKeyHash
+import androidx.compose.runtime.remember
 import com.ivianuu.essentials.Scope
 
 val LocalScope = compositionLocalOf<Scope<*>> { error("No scope provided") }
+
+@Composable fun <T : Any> rememberScoped(vararg inputs: Any?, key: Any? = null, init: () -> T): T {
+  val scope = LocalScope.current
+
+  val finalKey = key ?: currentCompositeKeyHash
+
+  val valueHolder = remember { scope.scoped(finalKey) { ScopedValueHolder() } }
+
+  val value = remember(*inputs) {
+    valueHolder.value
+      .takeIf {
+        it !== valueHolder &&
+            inputs.contentEquals(valueHolder.inputs)
+      }
+      ?: init()
+        .also {
+          valueHolder.value = it
+          valueHolder.inputs = inputs
+        }
+  }
+
+  return value as T
+}
+
+private class ScopedValueHolder {
+  var value: Any? = this
+  var inputs: Array<out Any?> = emptyArray()
+}
