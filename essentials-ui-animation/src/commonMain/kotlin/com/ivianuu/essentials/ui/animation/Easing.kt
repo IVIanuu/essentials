e9efb25b66ed47f9b9e7dfa6a1bd55f14be6a952
manu <IVIanuu@gmail.com>
2023-06-15 15:39:14 +0200

    Moved easings

--- essentials-ui-animation/src/commonMain/kotlin/com/ivianuu/essentials/ui/animation/Easing.kt
+++ essentials-ui-animation/src/commonMain/kotlin/com/ivianuu/essentials/ui/animation/Easing.kt
@@ -2,7 +2,7 @@
  * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package com.ivianuu.essentials.ui.animation.util
+package com.ivianuu.essentials.ui.animation
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.lerp
