0615223cd84d21f796afd5438f6a37250e5e3909
manu <ivianuu@gmail.com>
2023-06-26 21:33:09 +0200

    ...

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/NotificationsScreen.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/NotificationsScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
