8db1ac8ba4c4300d8b41b085724711877e3929d1
manu <ivianuu@gmail.com>
2023-06-29 12:11:38 +0200

    Added enabled parameter to PopupMenuItem

--- essentials-ui-popup/src/commonMain/kotlin/com/ivianuu/essentials/ui/popup/PopupMenu.kt
+++ essentials-ui-popup/src/commonMain/kotlin/com/ivianuu/essentials/ui/popup/PopupMenu.kt
@@ -9,17 +9,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import com.ivianuu.essentials.compose.LocalScope
 import com.ivianuu.essentials.compose.action
+import com.ivianuu.essentials.ui.common.interactive
 import com.ivianuu.essentials.ui.navigation.navigator
 import com.ivianuu.essentials.ui.navigation.pop
 import com.ivianuu.essentials.ui.navigation.screen
 
-@Composable fun PopupMenuItem(onSelected: () -> Unit, content: @Composable () -> Unit) {
+@Composable fun PopupMenuItem(
+  onSelected: () -> Unit,
+  enabled: Boolean = true,
+  content: @Composable () -> Unit,
+) {
   val navigator = LocalScope.current.navigator
   val screen = LocalScope.current.screen
 
@@ -27,6 +34,8 @@ import com.ivianuu.essentials.ui.navigation.screen
     modifier = Modifier
       .fillMaxWidth()
       .height(48.dp)
+      .interactive(enabled)
+      .alpha(if (enabled) 1f else ContentAlpha.disabled)
       .clickable(onClick = action {
         navigator.pop(screen)
         onSelected()
