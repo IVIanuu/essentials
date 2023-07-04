e7b05824ac51c25564d17baf92cbfbb213178656
manu <ivianuu@gmail.com>
2023-07-04 17:01:29 +0200

    Removed unused permissions

--- essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ActionPermissions.kt
+++ essentials-gestures/src/main/kotlin/com/ivianuu/essentials/gestures/action/ActionPermissions.kt
@@ -10,10 +10,7 @@ import com.ivianuu.essentials.accessibility.EsAccessibilityService
 import com.ivianuu.essentials.gestures.R
 import com.ivianuu.essentials.permission.Permission
 import com.ivianuu.essentials.permission.accessibility.AccessibilityServicePermission
-import com.ivianuu.essentials.permission.notificationpolicy.NotificationPolicyPermission
-import com.ivianuu.essentials.permission.root.RootPermission
 import com.ivianuu.essentials.permission.systemoverlay.SystemOverlayPermission
-import com.ivianuu.essentials.permission.writesecuresettings.WriteSecureSettingsPermission
 import com.ivianuu.essentials.permission.writesettings.WriteSettingsPermission
 import com.ivianuu.injekt.Provide
 
@@ -25,30 +22,11 @@ import com.ivianuu.injekt.Provide
     icon = Permission.Icon { Icon(R.drawable.es_ic_accessibility) }
   )
 
-@Provide class ActionNotificationPolicyPermission(resources: Resources) :
-  NotificationPolicyPermission(
-    title = resources(R.string.es_action_notification_policy_permission_title),
-    desc = resources(R.string.es_action_notification_policy_permission_desc),
-    icon = Permission.Icon { Icon(R.drawable.es_ic_notifications) }
-  )
-
-@Provide class ActionRootPermission(resources: Resources) : RootPermission(
-  title = resources(R.string.es_action_root_permission_title),
-  icon = Permission.Icon { Icon(R.drawable.es_ic_adb) }
-)
-
 @Provide class ActionSystemOverlayPermission(resources: Resources) :
   SystemOverlayPermission(
     title = resources(R.string.es_action_system_overlay_permission_title),
     desc = resources(R.string.es_action_system_overlay_permission_desc),
     icon = Permission.Icon { Icon(R.drawable.es_ic_adb) }
-  )
-
-@Provide class ActionWriteSecureSettingsPermission(resources: Resources) :
-  WriteSecureSettingsPermission(
-    title = resources(R.string.es_action_write_secure_settings_permission_title),
-    desc = resources(R.string.es_action_write_secure_settings_permission_desc),
-    icon = Permission.Icon { Icon(R.drawable.es_ic_settings) }
   )
 
 @Provide class ActionWriteSettingsPermission(resources: Resources) :
