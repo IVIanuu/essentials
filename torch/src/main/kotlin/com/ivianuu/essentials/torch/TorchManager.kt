651755899b7b9b8a9435ed884d76a2052a29f007
manu <ivianuu@gmail.com>
2024-02-15 14:37:59 +0100

    Removed flashlight notification

--- torch/src/main/kotlin/com/ivianuu/essentials/torch/TorchManager.kt
+++ torch/src/main/kotlin/com/ivianuu/essentials/torch/TorchManager.kt
@@ -4,7 +4,6 @@
 
 package com.ivianuu.essentials.torch
 
-import android.app.NotificationManager
 import android.hardware.camera2.CameraManager
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -12,26 +11,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import arrow.core.Either
 import com.ivianuu.essentials.AppScope
-import com.ivianuu.essentials.Resources
 import com.ivianuu.essentials.Scoped
 import com.ivianuu.essentials.SystemService
 import com.ivianuu.essentials.compose.compositionStateFlow
 import com.ivianuu.essentials.coroutines.ScopedCoroutineScope
 import com.ivianuu.essentials.coroutines.onCancel
-import com.ivianuu.essentials.foreground.ForegroundManager
 import com.ivianuu.essentials.logging.Logger
 import com.ivianuu.essentials.logging.asLog
 import com.ivianuu.essentials.logging.log
-import com.ivianuu.essentials.torch.TorchManagerImpl.DisableTorchAction
-import com.ivianuu.essentials.util.NotificationFactory
-import com.ivianuu.essentials.util.RemoteAction
 import com.ivianuu.essentials.util.Toaster
-import com.ivianuu.essentials.util.context
-import com.ivianuu.essentials.util.remoteActionOf
-import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.serialization.json.Json
 
 interface TorchManager {
   val torchEnabled: StateFlow<Boolean>
@@ -41,32 +31,13 @@ interface TorchManager {
 
 @Provide @Scoped<AppScope> class TorchManagerImpl(
   private val cameraManager: @SystemService CameraManager,
-  private val foregroundManager: ForegroundManager,
-  @Inject private val json: Json,
   private val logger: Logger,
-  private val notificationFactory: NotificationFactory,
-  private val resources: Resources,
   scope: ScopedCoroutineScope<AppScope>,
   private val toaster: Toaster
 ) : TorchManager {
   private var _torchEnabled by mutableStateOf(false)
   override val torchEnabled = scope.compositionStateFlow {
     if (!_torchEnabled) return@compositionStateFlow _torchEnabled
-
-    LaunchedEffect(true) {
-      foregroundManager.startForeground {
-        notificationFactory(
-          "torch",
-          resources(R.string.es_notif_channel_torch),
-          NotificationManager.IMPORTANCE_LOW
-        ) {
-          setSmallIcon(R.drawable.es_ic_flashlight_on)
-          setContentTitle(resources(R.string.es_notif_title_torch))
-          setContentText(resources(R.string.es_notif_text_torch))
-          setContentIntent(remoteActionOf<DisableTorchAction>(context))
-        }
-      }
-    }
 
     LaunchedEffect(true) {
       Either.catch {
@@ -91,8 +62,4 @@ interface TorchManager {
   override suspend fun updateTorchState(value: Boolean) {
     _torchEnabled = value
   }
-
-  fun interface DisableTorchAction : RemoteAction<Any?>
-
-  @Provide fun disableTorchAction() = DisableTorchAction { _torchEnabled = false }
 }
