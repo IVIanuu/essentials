065c4bf98ba51a7b1e6701939c9a301ffe836aed
manu <ivianuu@gmail.com>
2024-02-18 12:21:22 +0100

    Changed action

--- core/src/commonMain/kotlin/com/ivianuu/essentials/compose/Action.kt
+++ core/src/commonMain/kotlin/com/ivianuu/essentials/compose/Action.kt
@@ -1,113 +1,36 @@
 package com.ivianuu.essentials.compose
 
 import androidx.compose.runtime.*
-import com.ivianuu.essentials.*
 import kotlinx.coroutines.*
 
-@Composable fun action(block: suspend () -> Unit): () -> Unit {
-  val scope = rememberCoroutineScope()
-  return {
-    scope.launch {
-      block()
-    }
-  }
-}
+@Composable inline fun action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend () -> Unit
+): () -> Unit = { scope.launch { block() } }
 
-@Composable fun <P1> action(block: suspend (P1) -> Unit): (P1) -> Unit {
-  val scope = rememberCoroutineScope()
-  return { p1 ->
-    scope.launch {
-      block(p1)
-    }
-  }
-}
+@Composable inline fun <P1> action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend (P1) -> Unit
+): (P1) -> Unit = { p1 -> scope.launch { block(p1) } }
 
-@Composable fun <P1, P2> action(block: suspend (P1, P2) -> Unit): (P1, P2) -> Unit {
-  val scope = rememberCoroutineScope()
-  return { p1, p2 ->
-    scope.launch {
-      block(p1, p2)
-    }
-  }
-}
+@Composable inline fun <P1, P2> action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend (P1, P2) -> Unit
+): (P1, P2) -> Unit = { p1, p2 -> scope.launch { block(p1, p2) } }
 
-@Composable fun <P1, P2, P3> action(block: suspend (P1, P2, P3) -> Unit): (P1, P2, P3) -> Unit {
-  val scope = rememberCoroutineScope()
-  return { p1, p2, p3 ->
-    scope.launch {
-      block(p1, p2, p3)
-    }
-  }
-}
+@Composable inline fun <P1, P2, P3> action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend (P1, P2, P3) -> Unit
+): (P1, P2, P3) -> Unit = { p1, p2, p3 -> scope.launch { block(p1, p2, p3) } }
 
-@Composable fun <P1, P2, P3, P4> action(block: suspend (P1, P2, P3, P4) -> Unit): (P1, P2, P3, P4) -> Unit {
-  val scope = rememberCoroutineScope()
-  return { p1, p2, p3, p4 ->
-    scope.launch {
-      block(p1, p2, p3, p4)
-    }
-  }
-}
+@Composable inline fun <P1, P2, P3, P4> action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend (P1, P2, P3, P4) -> Unit
+): (P1, P2, P3, P4) -> Unit = { p1, p2, p3, p4 -> scope.launch { block(p1, p2, p3, p4) } }
 
-@Composable fun <P1, P2, P3, P4, P5> action(block: suspend (P1, P2, P3, P4, P5) -> Unit): (P1, P2, P3, P4, P5) -> Unit {
-  val scope = rememberCoroutineScope()
-  return { p1, p2, p3, p4, p5 ->
-    scope.launch {
-      block(p1, p2, p3, p4, p5)
-    }
-  }
-}
-
-@Composable fun scopedAction(block: suspend () -> Unit): () -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return {
-    scope.launch {
-      block()
-    }
-  }
-}
-
-@Composable fun <P1> scopedAction(block: suspend (P1) -> Unit): (P1) -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return { p1 ->
-    scope.launch {
-      block(p1)
-    }
-  }
-}
-
-@Composable fun <P1, P2> scopedAction(block: suspend (P1, P2) -> Unit): (P1, P2) -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return { p1, p2 ->
-    scope.launch {
-      block(p1, p2)
-    }
-  }
-}
-
-@Composable fun <P1, P2, P3> scopedAction(block: suspend (P1, P2, P3) -> Unit): (P1, P2, P3) -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return { p1, p2, p3 ->
-    scope.launch {
-      block(p1, p2, p3)
-    }
-  }
-}
-
-@Composable fun <P1, P2, P3, P4> scopedAction(block: suspend (P1, P2, P3, P4) -> Unit): (P1, P2, P3, P4) -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return { p1, p2, p3, p4 ->
-    scope.launch {
-      block(p1, p2, p3, p4)
-    }
-  }
-}
-
-@Composable fun <P1, P2, P3, P4, P5> scopedAction(block: suspend (P1, P2, P3, P4, P5) -> Unit): (P1, P2, P3, P4, P5) -> Unit {
-  val scope = LocalScope.current.coroutineScope
-  return { p1, p2, p3, p4, p5 ->
-    scope.launch {
-      block(p1, p2, p3, p4, p5)
-    }
-  }
+@Composable inline fun <P1, P2, P3, P4, P5> action(
+  scope: CoroutineScope = rememberCoroutineScope(),
+  crossinline block: suspend (P1, P2, P3, P4, P5) -> Unit
+): (P1, P2, P3, P4, P5) -> Unit = { p1, p2, p3, p4, p5 ->
+  scope.launch { block(p1, p2, p3, p4, p5) }
 }
