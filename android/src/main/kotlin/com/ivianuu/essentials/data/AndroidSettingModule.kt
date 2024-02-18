e554b063833e6e61e8b6152b90f032b41790947e
manu <ivianuu@gmail.com>
2024-02-18 23:30:31 +0100

    Simplified AndroidSettingModule

--- android/src/main/kotlin/com/ivianuu/essentials/data/AndroidSettingModule.kt
+++ android/src/main/kotlin/com/ivianuu/essentials/data/AndroidSettingModule.kt
@@ -13,19 +13,41 @@ import com.ivianuu.injekt.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
-class AndroidSettingModule<T : S, S>(
+class AndroidSettingModule<T : S, S : Any>(
   private val name: String,
   private val type: AndroidSettingsType,
   private val defaultValue: T
 ) {
-  @Suppress("UNCHECKED_CAST")
   @Provide fun dataStore(
-    adapter: AndroidSettingAdapter<S>,
     contentChangesFactory: ContentChangesFactory,
     contentResolver: ContentResolver,
     coroutineContexts: CoroutineContexts,
     scope: ScopedCoroutineScope<AppScope>
   ): DataStore<T> = object : DataStore<T> {
+    private fun get(): T = when (defaultValue) {
+      is Float -> when (type) {
+        AndroidSettingsType.GLOBAL -> Settings.Global.getFloat(contentResolver, name, defaultValue)
+        AndroidSettingsType.SECURE -> Settings.Secure.getFloat(contentResolver, name, defaultValue)
+        AndroidSettingsType.SYSTEM -> Settings.System.getFloat(contentResolver, name, defaultValue)
+      }
+      is Int -> when (type) {
+        AndroidSettingsType.GLOBAL -> Settings.Global.getInt(contentResolver, name, defaultValue)
+        AndroidSettingsType.SECURE -> Settings.Secure.getInt(contentResolver, name, defaultValue)
+        AndroidSettingsType.SYSTEM -> Settings.System.getInt(contentResolver, name, defaultValue)
+      }
+      is Long -> when (type) {
+        AndroidSettingsType.GLOBAL -> Settings.Global.getLong(contentResolver, name, defaultValue)
+        AndroidSettingsType.SECURE -> Settings.Secure.getLong(contentResolver, name, defaultValue)
+        AndroidSettingsType.SYSTEM -> Settings.System.getLong(contentResolver, name, defaultValue)
+      }
+      is String -> when (type) {
+        AndroidSettingsType.GLOBAL -> Settings.Global.getString(contentResolver, name) ?: defaultValue
+        AndroidSettingsType.SECURE -> Settings.Secure.getString(contentResolver, name) ?: defaultValue
+        AndroidSettingsType.SYSTEM -> Settings.System.getString(contentResolver, name) ?: defaultValue
+      }
+      else -> throw AssertionError("Unsupported type ${defaultValue::class}")
+    }.unsafeCast()
+
     override val data: Flow<T> = contentChangesFactory(
       when (type) {
         AndroidSettingsType.GLOBAL -> Settings.Global.getUriFor(name)
@@ -34,20 +56,38 @@ class AndroidSettingModule<T : S, S>(
       }
     )
       .onStart { emit(Unit) }
-      .map {
-        withContext(coroutineContexts.io) {
-          adapter.get(contentResolver, name, type, defaultValue) as T
-        }
-      }
+      .map { withContext(coroutineContexts.io) { get() } }
       .shareIn(scope, SharingStarted.WhileSubscribed(), 1)
       .distinctUntilChanged()
 
     override suspend fun updateData(transform: T.() -> T): T {
-      val currentValue = adapter.get(contentResolver, name, type, defaultValue) as T
+      val currentValue = get()
       val newValue = transform(currentValue)
       if (currentValue != newValue)
         withContext(coroutineContexts.io) {
-          adapter.set(contentResolver, name, type, newValue)
+          when (newValue) {
+            is Float -> when (type) {
+              AndroidSettingsType.GLOBAL -> Settings.Global.putFloat(contentResolver, name, newValue)
+              AndroidSettingsType.SECURE -> Settings.Secure.putFloat(contentResolver, name, newValue)
+              AndroidSettingsType.SYSTEM -> Settings.System.putFloat(contentResolver, name, newValue)
+            }
+            is Int -> when (type) {
+              AndroidSettingsType.GLOBAL -> Settings.Global.putInt(contentResolver, name, newValue)
+              AndroidSettingsType.SECURE -> Settings.Secure.putInt(contentResolver, name, newValue)
+              AndroidSettingsType.SYSTEM -> Settings.System.putInt(contentResolver, name, newValue)
+            }
+            is Long -> when (type) {
+              AndroidSettingsType.GLOBAL -> Settings.Global.putLong(contentResolver, name, newValue)
+              AndroidSettingsType.SECURE -> Settings.Secure.putLong(contentResolver, name, newValue)
+              AndroidSettingsType.SYSTEM -> Settings.System.putLong(contentResolver, name, newValue)
+            }
+            is String -> when (type) {
+              AndroidSettingsType.GLOBAL -> Settings.Global.putString(contentResolver, name, newValue)
+              AndroidSettingsType.SECURE -> Settings.Secure.putString(contentResolver, name, newValue)
+              AndroidSettingsType.SYSTEM -> Settings.System.putString(contentResolver, name, newValue)
+            }
+            else -> throw AssertionError("Unsupported type ${newValue::class}")
+          }
         }
       return newValue
     }
@@ -55,118 +95,3 @@ class AndroidSettingModule<T : S, S>(
 }
 
 enum class AndroidSettingsType { GLOBAL, SECURE, SYSTEM }
-
-interface AndroidSettingAdapter<T> {
-  fun get(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    defaultValue: T
-  ): T
-
-  fun set(contentResolver: ContentResolver, name: String, type: AndroidSettingsType, value: T)
-}
-
-@Provide object FloatAndroidSettingAdapter : AndroidSettingAdapter<Float> {
-  override fun get(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    defaultValue: Float
-  ) = when (type) {
-    AndroidSettingsType.GLOBAL -> Settings.Global.getFloat(contentResolver, name, defaultValue)
-    AndroidSettingsType.SECURE -> Settings.Secure.getFloat(contentResolver, name, defaultValue)
-    AndroidSettingsType.SYSTEM -> Settings.System.getFloat(contentResolver, name, defaultValue)
-  }
-
-  override fun set(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    value: Float
-  ) {
-    when (type) {
-      AndroidSettingsType.GLOBAL -> Settings.Global.putFloat(contentResolver, name, value)
-      AndroidSettingsType.SECURE -> Settings.Secure.putFloat(contentResolver, name, value)
-      AndroidSettingsType.SYSTEM -> Settings.System.putFloat(contentResolver, name, value)
-    }
-  }
-}
-
-@Provide object IntAndroidSettingAdapter : AndroidSettingAdapter<Int> {
-  override fun get(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    defaultValue: Int
-  ) = when (type) {
-    AndroidSettingsType.GLOBAL -> Settings.Global.getInt(contentResolver, name, defaultValue)
-    AndroidSettingsType.SECURE -> Settings.Secure.getInt(contentResolver, name, defaultValue)
-    AndroidSettingsType.SYSTEM -> Settings.System.getInt(contentResolver, name, defaultValue)
-  }
-
-  override fun set(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    value: Int
-  ) {
-    when (type) {
-      AndroidSettingsType.GLOBAL -> Settings.Global.putInt(contentResolver, name, value)
-      AndroidSettingsType.SECURE -> Settings.Secure.putInt(contentResolver, name, value)
-      AndroidSettingsType.SYSTEM -> Settings.System.putInt(contentResolver, name, value)
-    }
-  }
-}
-
-@Provide object LongAndroidSettingAdapter : AndroidSettingAdapter<Long> {
-  override fun get(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    defaultValue: Long
-  ) = when (type) {
-    AndroidSettingsType.GLOBAL -> Settings.Global.getLong(contentResolver, name, defaultValue)
-    AndroidSettingsType.SECURE -> Settings.Secure.getLong(contentResolver, name, defaultValue)
-    AndroidSettingsType.SYSTEM -> Settings.System.getLong(contentResolver, name, defaultValue)
-  }
-
-  override fun set(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    value: Long
-  ) {
-    when (type) {
-      AndroidSettingsType.GLOBAL -> Settings.Global.putLong(contentResolver, name, value)
-      AndroidSettingsType.SECURE -> Settings.Secure.putLong(contentResolver, name, value)
-      AndroidSettingsType.SYSTEM -> Settings.System.putLong(contentResolver, name, value)
-    }
-  }
-}
-
-@Provide object StringAndroidSettingAdapter : AndroidSettingAdapter<String> {
-  override fun get(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    defaultValue: String
-  ) = when (type) {
-    AndroidSettingsType.GLOBAL -> Settings.Global.getString(contentResolver, name)
-    AndroidSettingsType.SECURE -> Settings.Secure.getString(contentResolver, name)
-    AndroidSettingsType.SYSTEM -> Settings.System.getString(contentResolver, name)
-  } ?: defaultValue
-
-  override fun set(
-    contentResolver: ContentResolver,
-    name: String,
-    type: AndroidSettingsType,
-    value: String
-  ) {
-    when (type) {
-      AndroidSettingsType.GLOBAL -> Settings.Global.putString(contentResolver, name, value)
-      AndroidSettingsType.SECURE -> Settings.Secure.putString(contentResolver, name, value)
-      AndroidSettingsType.SYSTEM -> Settings.System.putString(contentResolver, name, value)
-    }
-  }
-}
