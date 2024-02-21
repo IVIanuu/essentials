cc7a0b697a7fdb2a8b133eb063269965d9d2ec65
manu <ivianuu@gmail.com>
2024-02-21 21:57:00 +0100

    ...

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/HomeScreen.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/ui/HomeScreen.kt
@@ -23,88 +23,90 @@ import com.ivianuu.essentials.util.*
 import com.ivianuu.essentials.xposed.*
 import com.ivianuu.injekt.*
 
-@Provide class HomeScreen : RootScreen
+@Provide class HomeScreen : RootScreen {
+  @Provide companion object {
+    @Provide fun ui(
+      isXposedRunning: IsXposedRunning,
+      itemsFactory: () -> List<HomeItem>,
+      navigator: Navigator,
+      toaster: Toaster
+    ) = Ui<HomeScreen> {
+      val finalItems = remember { itemsFactory().sortedBy { it.title } }
+      ScreenScaffold(
+        topBar = {
+          AppBar(
+            title = { Text("Home") },
+            actions = {
+              DropdownMenuButton {
+                listOf("Option 1", "Option 2", "Option 3").forEach { title ->
+                  DropdownMenuItem(onClick = { toaster("Selected $title") }) {
+                    Text(title)
+                  }
+                }
+              }
+            }
+          )
+        }
+      ) {
+        VerticalList {
+          items(finalItems) { item ->
+            val color = rememberSaveable(item) {
+              ColorPickerPalette.entries
+                .filter { it != ColorPickerPalette.BLACK && it != ColorPickerPalette.WHITE }
+                .shuffled()
+                .first()
+                .front
+            }
 
-@Provide fun homeUi(
-  isXposedRunning: IsXposedRunning,
-  itemsFactory: () -> List<HomeItem>,
-  navigator: Navigator,
-  toaster: Toaster
-) = Ui<HomeScreen> {
-  val finalItems = remember { itemsFactory().sortedBy { it.title } }
-  ScreenScaffold(
-    topBar = {
-      AppBar(
-        title = { Text("Home") },
-        actions = {
+            HomeItem(
+              item = item,
+              color = color,
+              onClick = action { navigator.push(item.screenFactory(color)) }
+            )
+          }
+
+          item {
+            ListItem(
+              title = {
+                Text(
+                  if (isXposedRunning.value) "Xposed is running"
+                  else "Xposed is NOT running"
+                )
+              }
+            )
+          }
+        }
+      }
+    }
+
+    @Composable private fun HomeItem(color: Color, onClick: () -> Unit, item: HomeItem) {
+      ListItem(
+        onClick = onClick,
+        title = {
+          Text(
+            item.title,
+            style = MaterialTheme.typography.subtitle1
+          )
+        },
+        leading = {
+          Box(
+            modifier = Modifier
+              .size(40.dp)
+              .background(color, CircleShape)
+          )
+        },
+        trailing = {
           DropdownMenuButton {
-            listOf("Option 1", "Option 2", "Option 3").forEach { title ->
-              DropdownMenuItem(onClick = { toaster("Selected $title") }) {
-                Text(title)
+            (0..100).forEach { index ->
+              DropdownMenuItem(onClick = {}) {
+                Text(index.toString())
               }
             }
           }
         }
       )
     }
-  ) {
-    VerticalList {
-      items(finalItems) { item ->
-        val color = rememberSaveable(item) {
-          ColorPickerPalette.entries
-            .filter { it != ColorPickerPalette.BLACK && it != ColorPickerPalette.WHITE }
-            .shuffled()
-            .first()
-            .front
-        }
-
-        HomeItem(
-          item = item,
-          color = color,
-          onClick = action { navigator.push(item.screenFactory(color)) }
-        )
-      }
-
-      item {
-        ListItem(
-          title = {
-            Text(
-              if (isXposedRunning.value) "Xposed is running"
-              else "Xposed is NOT running"
-            )
-          }
-        )
-      }
-    }
   }
-}
-
-@Composable private fun HomeItem(color: Color, onClick: () -> Unit, item: HomeItem) {
-  ListItem(
-    onClick = onClick,
-    title = {
-      Text(
-        item.title,
-        style = MaterialTheme.typography.subtitle1
-      )
-    },
-    leading = {
-      Box(
-        modifier = Modifier
-          .size(40.dp)
-          .background(color, CircleShape)
-      )
-    },
-    trailing = {
-      DropdownMenuButton {
-        (0..100).forEach { index ->
-          DropdownMenuItem(onClick = {}) {
-            Text(index.toString())
-          }
-        }
-      }
-    }
-  )
 }
 
 data class HomeItem(val title: String, val screenFactory: (Color) -> Screen<*>)
