304985709e6d5fff67b869cf839a474a18a7d499
Manu <ivianuu@gmail.com>
2024-07-26 10:57:01 +0200

    Safer lerp apis

--- core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
+++ core/src/commonMain/kotlin/com/ivianuu/essentials/Lerp.kt
@@ -5,9 +5,11 @@
 package com.ivianuu.essentials
 
 import com.ivianuu.injekt.*
+import kotlinx.coroutines.flow.*
 import kotlin.math.*
 import kotlin.time.*
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.nanoseconds
 
 interface Lerper<T> {
   fun lerp(start: T, stop: T, fraction: Float): T
@@ -21,58 +23,61 @@ interface Lerper<T> {
       override fun lerp(start: T, stop: T, fraction: Float): T = lerp.invoke(start, stop, fraction)
       override fun unlerp(start: T, stop: T, value: T): Float = unlerp.invoke(start, stop, value)
     }
-
-    @Provide val float = Lerper<Float>(
-      lerp = { start, stop, fraction ->
-        start * (1f - fraction) + stop * fraction
-      },
-      unlerp = { start, stop, value ->
-        (if (stop - start == 0f) 0f else (value - start) / (stop - start)).coerceIn(0f, 1f)
-      }
-    )
-
-    @Provide val double = Lerper<Double>(
-      lerp = { start, stop, fraction ->
-        start * (1.0 - fraction) + stop * fraction
-      },
-      unlerp = { start, stop, value ->
-        (if (stop - start == 0.0) 0.0 else (value - start) / (stop - start)).coerceIn(0.0, 1.0)
-          .toFloat()
-      }
-    )
-
-    @Provide val duration = Lerper<Duration>(
-      lerp = { start, stop, fraction ->
-        lerp(start.inWholeMilliseconds, stop.inWholeMilliseconds, fraction)
-          .milliseconds
-      },
-      unlerp = { start, stop, value ->
-        unlerp(start.inWholeMilliseconds, stop.inWholeMilliseconds, value.inWholeMilliseconds)
-      }
-    )
-
-    @Provide val int = Lerper<Int>(
-      lerp = { start, stop, fraction ->
-        (start * (1f - fraction) + stop * fraction).roundToInt()
-      },
-      unlerp = { start, stop, value ->
-        unlerp(start.toFloat(), stop.toFloat(), value.toFloat())
-      }
-    )
-
-    @Provide val long = Lerper<Long>(
-      lerp = { start, stop, fraction ->
-        (start * (1f - fraction) + stop * fraction).roundToLong()
-      },
-      unlerp = { start, stop, value ->
-        unlerp(start.toDouble(), stop.toDouble(), value.toDouble())
-      }
-    )
   }
 }
 
-fun <T> lerp(start: T, stop: T, fraction: Float, lerper: Lerper<T> = inject): T =
-  lerper.lerp(start, stop, fraction)
+fun lerp(start: Float, stop: Float, fraction: Float): Float =
+  start * (1f - fraction) + stop * fraction
 
-fun <T> unlerp(start: T, stop: T, value: T, lerper: Lerper<T> = inject): Float =
-  lerper.unlerp(start, stop, value)
+fun unlerp(start: Float, stop: Float, value: Float): Float =
+  (if (stop - start == 0f) 0f else (value - start) / (stop - start)).coerceIn(0f, 1f)
+
+@Provide val FloatLerper = Lerper<Float>(
+  lerp = { start, stop, fraction -> lerp(start, stop, fraction) },
+  unlerp = { start, stop, value -> unlerp(start, stop, value) }
+)
+
+fun lerp(start: Double, stop: Double, fraction: Float): Double =
+  start * (1.0 - fraction) + stop * fraction
+
+fun unlerp(start: Double, stop: Double, value: Double): Float =
+  (if (stop - start == 0.0) 0.0 else (value - start) / (stop - start)).coerceIn(0.0, 1.0)
+    .toFloat()
+
+@Provide val DoubleLerper = Lerper<Double>(
+  lerp = { start, stop, fraction -> lerp(start, stop, fraction) },
+  unlerp = { start, stop, value -> unlerp(start, stop, value) }
+)
+
+fun lerp(start: Int, stop: Int, fraction: Float): Int =
+  (start * (1f - fraction) + stop * fraction).roundToInt()
+
+fun unlerp(start: Int, stop: Int, value: Int): Float =
+  unlerp(start.toFloat(), stop.toFloat(), value.toFloat())
+
+@Provide val IntLerper = Lerper<Int>(
+  lerp = { start, stop, fraction -> lerp(start, stop, fraction) },
+  unlerp = { start, stop, value -> unlerp(start, stop, value) }
+)
+
+fun lerp(start: Long, stop: Long, fraction: Float): Long =
+  (start * (1f - fraction) + stop * fraction).roundToLong()
+
+fun unlerp(start: Long, stop: Long, value: Long): Float =
+  unlerp(start.toDouble(), stop.toDouble(), value.toDouble())
+
+@Provide val LongLerper = Lerper<Long>(
+  lerp = { start, stop, fraction -> lerp(start, stop, fraction) },
+  unlerp = { start, stop, value -> unlerp(start, stop, value) }
+)
+
+fun lerp(start: Duration, stop: Duration, fraction: Float): Duration =
+  lerp(start.inWholeNanoseconds, stop.inWholeNanoseconds, fraction).nanoseconds
+
+fun unlerp(start: Duration, stop: Duration, value: Duration): Float =
+  unlerp(start.inWholeNanoseconds, stop.inWholeNanoseconds, value.inWholeNanoseconds)
+
+@Provide val DurationLerper = Lerper<Duration>(
+  lerp = { start, stop, fraction -> lerp(start, stop, fraction) },
+  unlerp = { start, stop, value -> unlerp(start, stop, value) }
+)
