dacee2e3174a6131e1c06ff76c5fdfa65dd61c15
Manu <ivianuu@gmail.com>
2024-11-04 12:15:36 +0100

    ...

--- android/src/main/kotlin/com/ivianuu/essentials/util/WakeLockManager.kt
+++ android/src/main/kotlin/com/ivianuu/essentials/util/WakeLockManager.kt
@@ -2,24 +2,33 @@ package com.ivianuu.essentials.util
 
 import android.os.*
 import androidx.compose.runtime.*
+import arrow.fx.coroutines.*
 import com.ivianuu.essentials.*
 import com.ivianuu.essentials.coroutines.*
 import com.ivianuu.essentials.logging.*
 import com.ivianuu.injekt.*
 import com.ivianuu.injekt.common.*
+import kotlinx.coroutines.*
 
 @Provide class WakeLockManager(
+  private val coroutineContexts: CoroutineContexts,
   private val logger: Logger,
   private val powerManager: @SystemService PowerManager
 ) {
   @Composable fun WakeLock(id: SourceKey = inject) {
-    DisposableEffect(id) {
-      logger.d { "${id.value} acquire wake lock" }
-      val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, id.value)
-        .also { it.acquire() }
-      onDispose {
-        logger.d { "${id.value} release wake lock" }
-        wakeLock.release()
+    LaunchedEffect(id) {
+      withContext(coroutineContexts.io) {
+        bracketCase(
+          acquire = { powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, id.value) },
+          use = {
+            logger.d { "${id.value} acquire wake lock" }
+            it.acquire()
+          },
+          release = { wakeLock, _ ->
+            logger.d { "${id.value} release wake lock" }
+            wakeLock.release()
+          }
+        )
       }
     }
   }
