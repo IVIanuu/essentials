9ee838ac8c823d8080baa981d3538a314985d1fa
Manu <ivianuu@gmail.com>
2024-11-04 12:15:22 +0100

    ...

--- sample/src/main/kotlin/com/ivianuu/essentials/sample/xposed/SampleHooks.kt
+++ sample/src/main/kotlin/com/ivianuu/essentials/sample/xposed/SampleHooks.kt
@@ -4,26 +4,20 @@
 
 package com.ivianuu.essentials.sample.xposed
 
-import android.bluetooth.*
+import android.media.AudioTrack
 import com.ivianuu.essentials.logging.*
 import com.ivianuu.essentials.xposed.*
 import com.ivianuu.injekt.*
 
 @Provide fun sampleHooks(logger: Logger) = Hooks { config ->
   logger.d { "hello ${config.packageName}" }
-  if (config.packageName != "uk.co.minirig.app") return@Hooks
+  if (config.packageName != "com.spotify.music") return@Hooks
 
-  logger.d { "minirig minirig minirig" }
+  logger.d { "spot spot spot" }
 
-  hookAllMethods(BluetoothGatt::class, "connect") {
+  hookAllMethods(AudioTrack::class, "write") {
     before {
-      logger.d { "connect ${args.contentDeepToString()}" }
-    }
-  }
-
-  hookAllMethods(BluetoothGatt::class, "writeCharacteristic") {
-    before {
-      logger.d { "minirig message ${args.contentDeepToString()}" }
+      logger.d { "write ${args.contentDeepToString()}" }
     }
   }
 }
