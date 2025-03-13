574652cb99f13f4955b6f974ec2e8930da47e39b
Manu <ivianuu@gmail.com>
2025-03-13 23:12:52 +0100

    ...

---
+++ android/src/main/kotlin/essentials/util/SensorManager.kt
@@ -0,0 +1,57 @@
+package essentials.util
+
+import android.hardware.*
+import androidx.compose.runtime.Composable
+import essentials.*
+import injekt.*
+import kotlin.time.*
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
+import androidx.compose.runtime.DisposableEffect
+
+@Provide class SensorManager(
+  private val sensorManager: @SystemService SensorManager,
+  private val wakeLockManager: WakeLockManager
+) {
+  fun sensor(type: Int): Sensor? = sensorManager.getDefaultSensor(type)
+
+  @Composable fun TriggerEventHandler(sensor: Sensor, onEvent: (TriggerEvent) -> Unit) {
+    if (!sensor.isWakeUpSensor)
+      wakeLockManager.WakeLock("sensors")
+
+    DisposableEffect(sensor) {
+      val listener = object : TriggerEventListener() {
+        override fun onTrigger(event: TriggerEvent) {
+          onEvent(event)
+          sensorManager.requestTriggerSensor(this, sensor)
+        }
+      }
+
+      sensorManager.requestTriggerSensor(listener, sensor)
+      onDispose { sensorManager.cancelTriggerSensor(listener, sensor) }
+    }
+  }
+
+  @Composable fun SensorEventHandler(
+    sensor: Sensor,
+    samplingRate: Duration,
+    onEvent: (SensorEvent) -> Unit
+  ) {
+    if (!sensor.isWakeUpSensor)
+      wakeLockManager.WakeLock("sensors")
+
+    DisposableEffect(sensor) {
+      val listener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+          onEvent(event)
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+      }
+
+      sensorManager.registerListener(listener, sensor, samplingRate.inWholeNanoseconds.toInt())
+      onDispose { sensorManager.unregisterListener(listener) }
+    }
+  }
+}
