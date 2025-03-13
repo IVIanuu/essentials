e601bdb3a6e6abe4d903c9d6b881a786539f8c48
Manu <ivianuu@gmail.com>
2025-03-13 16:58:21 +0100

    Notification api prototype

--- notification-listener/src/main/kotlin/essentials/notificationlistener/NotificationRepository.kt
+++ notification-listener/src/main/kotlin/essentials/notificationlistener/NotificationRepository.kt
@@ -9,23 +9,29 @@ import android.service.notification.*
 import androidx.compose.runtime.*
 import essentials.*
 import injekt.*
-import kotlinx.coroutines.flow.*
 
-@Stable @Provide class NotificationRepository(private val appScope: Scope<AppScope>) {
-  val notifications: List<StatusBarNotification>
-    get() = appScope.scopeOfOrNull<NotificationScope>()
-      ?.notificationListenerService?.notifications ?: emptyList()
+sealed interface NotificationApi {
+  data object Unavailable : NotificationApi
+  data object Empty : NotificationApi
+  data class Notifications(
+    val notifications: List<StatusBarNotification>,
+    val openNotification: suspend (Notification) -> Boolean,
+    val dismissNotification: suspend (String) -> Unit,
+    val dismissAllNotifications: suspend () -> Unit
+  ) : NotificationApi
+}
 
-  suspend fun openNotification(notification: Notification) =
-    catch { notification.contentIntent.send() }
-
-  suspend fun dismissNotification(key: String) = catch {
-    appScope.scopeOf<NotificationScope>().first()
-      .notificationListenerService.cancelNotification(key)
-  }
-
-  suspend fun dismissAllNotifications() = catch {
-    appScope.scopeOf<NotificationScope>().first()
-      .notificationListenerService.cancelAllNotifications()
-  }
+@Provide @Composable fun notificationApi(
+  appScope: Scope<AppScope>
+): @ComposeIn<AppScope> NotificationApi {
+  val notificationListenerService = appScope.scopeOfOrNull<NotificationScope>()
+    ?.notificationListenerService
+  return if (notificationListenerService == null) NotificationApi.Unavailable
+  else if (notificationListenerService.notifications.isEmpty()) NotificationApi.Empty
+  else NotificationApi.Notifications(
+    notifications = notificationListenerService.notifications,
+    openNotification = { catch { it.contentIntent.send() }.fold({ false }, { true }) },
+    dismissNotification = notificationListenerService::cancelNotification,
+    dismissAllNotifications = notificationListenerService::cancelAllNotifications
+  )
 }
---
+++ sample/src/main/kotlin/essentials/sample/ui/NotificationsScreen.kt
@@ -0,0 +1,84 @@
+package essentials.sample.ui
+
+import android.app.Notification
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.ClearAll
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import essentials.compose.action
+import essentials.notificationlistener.NotificationApi
+import essentials.permission.PermissionManager
+import essentials.ui.common.EsLazyColumn
+import essentials.ui.material.EsAppBar
+import essentials.ui.material.EsListItem
+import essentials.ui.material.EsScaffold
+import essentials.ui.navigation.Screen
+import essentials.ui.navigation.Ui
+import injekt.Provide
+
+@Provide val notificationsHomeItem = HomeItem("Notifications") {
+  NotificationsScreen()
+}
+
+class NotificationsScreen : Screen<Unit>
+
+@Provide @Composable fun NotificationsUi(
+  api: NotificationApi,
+  permissionManager: PermissionManager
+): Ui<NotificationsScreen> {
+  EsScaffold(
+    topBar = { EsAppBar { Text("Notifications") } },
+    floatingActionButton = {
+      AnimatedVisibility(api is NotificationApi.Notifications) {
+        if (api is NotificationApi.Notifications)
+          FloatingActionButton(onClick = action { api.dismissAllNotifications() }) {
+            Icon(Icons.Default.ClearAll, null)
+        }
+      }
+    }
+  ) {
+    when (api) {
+      is NotificationApi.Unavailable -> {
+        LaunchedEffect(true) {
+          permissionManager.ensurePermissions(listOf(SampleNotificationListenerPermission::class))
+        }
+
+        Text("Unavailable")
+      }
+      is NotificationApi.Empty -> Text("No notifications")
+      is NotificationApi.Notifications -> EsLazyColumn {
+        items(api.notifications) { notification ->
+          EsListItem(
+            onClick = action { api.openNotification(notification.notification) },
+            headlineContent = {
+              Text(
+                notification.notification.extras.getString(Notification.EXTRA_TITLE)
+                  ?: ""
+              )
+            },
+            supportingContent = {
+              Text(
+                notification.notification.extras.getString(Notification.EXTRA_TEXT)
+                  ?: ""
+              )
+            },
+            trailingContent = {
+              IconButton(
+                onClick = action { api.dismissNotification(notification.key) }
+              ) {
+                Icon(Icons.Default.Clear, null)
+              }
+            }
+          )
+        }
+      }
+    }
+  }
+}
